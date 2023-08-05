d24e5cdf8ff74023a803bdfdc66fc666c24fe926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:27 +0000

    Add package egttools/0.1.11.2/egttools-0.1.11.2-cp38-cp38-macosx_10_15_universal2.whl

---
+++ packages/egttools/egttools-0.1.11.2-cp38-cp38-macosx_10_15_universal2.whl/egttools/analytical/__init__.py
@@ -0,0 +1,8 @@
+"""
+API reference documentation for `sed_analytical` submodule.
+"""
+
+from .sed_analytical import replicator_equation
+from .sed_analytical import StochDynamics
+
+__all__ = ['replicator_equation', 'StochDynamics']
