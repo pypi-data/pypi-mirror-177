98721263d6b9fa8813db8ea98412ccd7ee0d16b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:50 +0000

    Add package testpypackage/0.0.1/testpypackage-0.0.1.tar.gz

---
+++ packages/testpypackage/testpypackage-0.0.1.tar.gz/testpypackage-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My package'
+
+# Setting up
+setup(
+    name="testpypackage",
+    version=VERSION,
+    author="Vishal",
+    author_email="vishal126sw@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python','python3'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
