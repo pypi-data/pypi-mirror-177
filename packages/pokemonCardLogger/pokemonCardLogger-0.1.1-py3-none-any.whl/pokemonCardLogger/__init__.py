b0ae15beac06a081864391741ef57dcd268a3e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package pokemonCardLogger/0.1.1/pokemonCardLogger-0.1.1-py3-none-any.whl

---
+++ packages/pokemonCardLogger/pokemonCardLogger-0.1.1-py3-none-any.whl/pokemonCardLogger/__init__.py
@@ -0,0 +1,9 @@
+"""
+Description:
+    A Module to log and store information about a Pokémon tcg collection
+Usage:
+    To run as a program "python3 main.py"
+    To use as a library: "from pokemonCardLogger import clss as pcl"
+    To get just the packs and their ids, run "python3 packRef.py"
+"""
+from pokemonCardLogger import *
