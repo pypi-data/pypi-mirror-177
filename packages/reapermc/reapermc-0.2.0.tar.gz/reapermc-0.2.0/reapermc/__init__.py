6726e39d03ede820a490d0aba4198beceb1d8a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:14 +0000

    Add package reapermc/0.2.0/reapermc-0.2.0.tar.gz

---
+++ packages/reapermc/reapermc-0.2.0.tar.gz/reapermc-0.2.0/reapermc/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.2.0"
+
+
+from beet import Context
+from beet.contrib.load import load
+from bolt import bolt
+
+
+def beet_default(ctx: Context):
+    ctx.require(
+        load(
+            data_pack={"data/reapermc/modules": "@reapermc/modules"},
+        ),
+        bolt(entrypoint=["*:main", "reapermc:reaper_cleanup"]),
+        "bolt_expressions.plugin",
+    )
