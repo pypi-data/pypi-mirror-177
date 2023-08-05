6ce8b34db450b5a989c7254082f48386455089d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:14 +0000

    Add package openmsistream/1.1.4/openmsistream-1.1.4-py3-none-any.whl

---
+++ packages/openmsistream/openmsistream-1.1.4-py3-none-any.whl/openmsistream/utilities/config.py
@@ -0,0 +1,16 @@
+"""Constants used in running different controlled processes in general"""
+
+#imports
+import pathlib
+
+class RunConstants :
+    """
+    Constants used for running controlled processes, reading config files, etc.
+    """
+
+    CONFIG_FILE_EXT = '.config'
+    CONFIG_FILE_DIR = (pathlib.Path(__file__).parent.parent / 'kafka_wrapper' / 'config_files').resolve()
+    DEFAULT_N_THREADS = 2
+    DEFAULT_UPDATE_SECONDS = 300
+
+RUN_CONST = RunConstants()
