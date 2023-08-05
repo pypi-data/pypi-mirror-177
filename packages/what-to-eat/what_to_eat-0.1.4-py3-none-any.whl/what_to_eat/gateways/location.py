8aca57a0343de4a59b97754f5dd3505e5d2bf942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:22 +0000

    Add package what-to-eat/0.1.4/what_to_eat-0.1.4-py3-none-any.whl

---
+++ packages/what-to-eat/what_to_eat-0.1.4-py3-none-any.whl/what_to_eat/gateways/location.py
@@ -0,0 +1,20 @@
+import urllib.parse
+
+import httpx
+
+from what_to_eat.models.location import Location
+
+
+class LocationError(Exception):
+    def __init__(self):
+        super().__init__("Error when trying to get location")
+
+
+def get(address: str) -> Location:
+    address = urllib.parse.quote(address)
+    response = httpx.get(f"https://nominatim.openstreetmap.org/search/{address}?format=json")
+
+    if not response.is_success:
+        raise LocationError()
+
+    return Location.parse_obj(response.json()[0])
