25ca27daf532958e3c1cfac7e286f8a7e9073f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:28 +0000

    Add package egttools/0.1.11.2/egttools-0.1.11.2-cp39-cp39-win_amd64.whl

---
+++ packages/egttools/egttools-0.1.11.2-cp39-cp39-win_amd64.whl/egttools/distributions/__init__.py
@@ -0,0 +1,5 @@
+"""Helpful implementations of stochastic distributions."""
+
+from egttools.numerical.numerical.distributions import (TimingUncertainty, multivariate_hypergeometric_pdf, )
+
+__all__ = ['TimingUncertainty', 'multivariate_hypergeometric_pdf']
