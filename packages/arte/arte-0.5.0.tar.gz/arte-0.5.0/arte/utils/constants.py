6fea7585a2e373c7bbeb7e010fd3ca945f4af687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:45 +0000

    Add package arte/0.5.0/arte-0.5.0.tar.gz

---
+++ packages/arte/arte-0.5.0.tar.gz/arte-0.5.0/arte/utils/constants.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class Constants(object):
+    '''Class holding global constants'''
+
+    RAD2ARCSEC = 180 * 3600 / np.pi
+    ARCSEC2RAD = np.pi / (180 * 3600)
+    RAD2DEG = 180 / np.pi
+    DEG2RAD = np.pi / 180
