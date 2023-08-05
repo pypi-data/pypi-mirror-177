94ec5a56221058fb7e3cca12110ea0fd5de675d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package bread-mail/1.2/bread-mail-1.2.tar.gz

---
+++ packages/bread-mail/bread-mail-1.2.tar.gz/bread-mail-1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.2'
+DESCRIPTION = 'Email application'
+LONG_DESCRIPTION = 'It is used for sending emails to multiple receivers at once.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="bread-mail",
+    version=VERSION,
+    author="leox",
+    author_email="leox9611@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    zip_safe=False,
+    # add any additional packages that
+    install_requires=[],
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'email'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
