630d4ba71c02d063882acdd2df7f8f88a45e1536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:19 +0000

    Add package lnschema_wetlab/0.9.0/lnschema_wetlab-0.9.0.tar.gz

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.9.0.tar.gz/lnschema_wetlab-0.9.0/lnschema_wetlab/__init__.py
@@ -0,0 +1,46 @@
+"""Schema module for a generic wetlab (`vvhc`).
+
+Import the package::
+
+   import lnschema_wetlab
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   Biometa
+   Biosample
+   Techsample
+   BiosampleTechsample
+   Readout
+   Treatment
+   ProjectExperiment
+   Experiment
+   ExperimentType
+
+Development tools:
+
+.. autosummary::
+   :toctree: .
+
+   dev
+
+"""
+# This is lnschema-module vvhc.
+_schema_id = "vvhc"
+_name = "wetlab"
+_migration = "bfda12fc80a8"
+__version__ = "0.9.0"
+
+from ._biosample import Biosample
+from ._core import (  # noqa
+    Biometa,
+    Experiment,
+    ExperimentType,
+    ProjectExperiment,
+    Readout,
+    Treatment,
+    dobject_biometa,
+)
+from ._techsample import BiosampleTechsample, Techsample
