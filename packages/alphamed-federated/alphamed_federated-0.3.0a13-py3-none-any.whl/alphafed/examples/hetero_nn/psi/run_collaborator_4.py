a639c4f3bc922b97e69c57c883a8e0d18ca5e088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:30 +0000

    Add package alphamed-federated/0.3.0a13/alphamed_federated-0.3.0a13-py3-none-any.whl

---
+++ packages/alphamed-federated/alphamed_federated-0.3.0a13-py3-none-any.whl/alphafed/examples/hetero_nn/psi/run_collaborator_4.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PYTHONPATH = os.path.join(CURRENT_DIR, os.pardir, os.pardir, os.pardir, os.pardir)
+sys.path.insert(0, PYTHONPATH)
+
+
+if True:
+    from alphafed import logger
+    from alphafed.examples import DATA_OWNER_4_ID
+    from alphafed.examples.hetero_nn.psi.demos import get_ids, get_task_id
+    from alphafed.hetero_nn.psi import RSAPSICollaboratorScheduler
+
+
+ids = get_ids()
+logger.info(f'local ids: {ids}')
+initiator_scheduler = RSAPSICollaboratorScheduler(task_id=get_task_id(),
+                                                  collaborator_id=DATA_OWNER_4_ID,
+                                                  ids=ids)
+initiator_scheduler._data_channel._ports = [i for i in range(21000, 21010)]
+intersection = initiator_scheduler.collaborate_intersection()
+logger.info(f'intersection ids: {intersection}')
