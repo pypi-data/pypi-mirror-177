4314ccd50b18d5d1083fb2874efaecf8f2d52d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:04 +0000

    Add package st-textnav-components/0.0.1/st_textnav_components-0.0.1.tar.gz

---
+++ packages/st-textnav-components/st_textnav_components-0.0.1.tar.gz/st_textnav_components-0.0.1/st_textnav_components/frontend/build/precache-manifest.e80c861d8cce26162e6362a910c1ae2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e21c346abd702b9d10e475ed2563b81",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57d2af5ddf490373e568",
+    "url": "./static/js/2.c12c44ef.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.c12c44ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e870f46fa426833f7f66",
+    "url": "./static/js/main.c0c60160.chunk.js"
+  },
+  {
+    "revision": "788696aa9cc71262340a",
+    "url": "./static/js/runtime-main.5875292e.js"
+  }
+]);
