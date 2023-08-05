0bfe08ef6078306c405a4e4b9a4add5804f5ed44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:20 +0000

    Add package naveeen/0.0.1/naveeen-0.0.1-py3-none-any.whl

---
+++ packages/naveeen/naveeen-0.0.1-py3-none-any.whl/naveeen/naveeen.py
@@ -0,0 +1,20 @@
+import concurrent.futures
+from time import perf_counter
+
+if __name__ == '__main__':
+    print(__name__)
+else:
+    def e(func,items,whizz=True):
+        t1 = perf_counter()
+        if whizz == True:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.map(func, items)
+        elif whizz == 2:
+            return f"{e(func,items,whizz=True)} {e(func,items,whizz=False)}"
+        else:
+            for item in items:
+                func(item)
+            t2 = perf_counter()
+            return f"\nCompleted in {t2-t1:.3f} seconds without naveeen mod."
+        t2 = perf_counter()
+        return f"\nCompleted in {t2-t1:.3f} seconds using naveeen mod."
