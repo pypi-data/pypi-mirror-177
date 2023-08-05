1fd650642d0586e8c0cc27a2d62bdac6b620d4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:22 +0000

    Add package rikai-sklearn/0.1.1/rikai_sklearn-0.1.1-py3-none-any.whl

---
+++ packages/rikai-sklearn/rikai_sklearn-0.1.1-py3-none-any.whl/rikai_sklearn/models/cluster.py
@@ -0,0 +1,15 @@
+from typing import List
+from rikai_sklearn.models import SklearnModelType
+
+
+class Clustering(SklearnModelType):
+    """Classification model type"""
+
+    def schema(self) -> str:
+        return "int"
+
+    def predict(self, x, *args, **kwargs) -> List[int]:
+        assert self.model is not None
+        return self.model.predict(x).tolist()
+
+MODEL_TYPE=Clustering()
