221755fa4df8e18200b36c74392c031c6026d1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:14 +0000

    Add package bilix/0.10.4/bilix-0.10.4-py3-none-any.whl

---
+++ packages/bilix/bilix-0.10.4-py3-none-any.whl/bilix/assign.py
@@ -0,0 +1,24 @@
+from bilix.log import logger
+
+
+class Handler:
+    registered = {}
+
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, func):
+        if self.name in self.registered:
+            raise KeyError(f"Handler {self.name} all ready exists")
+        self.registered[self.name] = func
+        return func
+
+
+def assign(**kwargs):
+    bili_handler = Handler.registered.pop('bilibili')
+    for name, handle in Handler.registered.items():
+        if res := handle(**kwargs):
+            logger.debug(f"Assign to {name}")
+            return res
+    # since bilix is originally designed for bilibili, finally use bilibili handler
+    return bili_handler(**kwargs)
---
+++ packages/bilix/bilix-0.10.4-py3-none-any.whl/bilix/download/downloader_jable.py
@@ -0,0 +1,43 @@
+import asyncio
+import re
+
+import httpx
+import bilix.api.jable as api
+from bilix.assign import Handler
+from bilix.download.base_downloader_m3u8 import BaseDownloaderM3u8
+
+
+class DownloaderJable(BaseDownloaderM3u8):
+    def __init__(self, videos_dir: str = "videos", video_concurrency: int = 3, part_concurrency: int = 10):
+        client = httpx.AsyncClient(
+            headers={'user-agent': 'PostmanRuntime/7.29.0', "referer": "https://jable.tv"}, http2=False)
+        super(DownloaderJable, self).__init__(client, videos_dir, video_concurrency, part_concurrency)
+
+    async def get_video(self, url: str, image=True, hierarchy=True):
+        video_info = await api.get_video_info(self.client, url)
+        if hierarchy:
+            hierarchy = self._make_hierarchy_dir(hierarchy, f"{video_info.avid} {video_info.model_name}")
+        cors = [self.get_m3u8_video(m3u8_url=video_info.m3u8_url, name=video_info.title,
+                                    hierarchy=hierarchy if hierarchy else '')]
+        if image:
+            cors.append(self._get_static(video_info.img_url, name=video_info.title,
+                                         hierarchy=hierarchy if hierarchy else ''))
+        await asyncio.gather(*cors)
+
+
+@Handler('jable')
+def handle(**kwargs):
+    method = kwargs['method']
+    key = kwargs['key']
+    videos_dir = kwargs['videos_dir']
+    video_concurrency = kwargs['video_concurrency']
+    part_concurrency = kwargs['part_concurrency']
+    hierarchy = kwargs['hierarchy']
+    image = kwargs['image']
+    if 'jable' in key or re.match(r"[A-Za-z]+-\d+", key):
+        d = DownloaderJable(videos_dir=videos_dir, video_concurrency=video_concurrency,
+                            part_concurrency=part_concurrency)
+        if method == 'get_video' or method == 'v':
+            cor = d.get_video(key, image=image, hierarchy=hierarchy)
+            return d, cor
+        raise ValueError(f'For {d.__class__.__name__} "{method}" is not available')
