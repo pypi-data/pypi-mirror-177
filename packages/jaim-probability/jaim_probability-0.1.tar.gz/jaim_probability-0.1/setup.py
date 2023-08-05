137abdc1860e534dc4d06df3adede12760f196d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:57 +0000

    Add package jaim-probability/0.1/jaim_probability-0.1.tar.gz

---
+++ packages/jaim-probability/jaim_probability-0.1.tar.gz/jaim_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jaim_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jaim_probability'],
+      author = 'José Imedio',
+      author_email = 'sofake@email.com',
+    zip_safe=False)
