a67f51e6dc2667fc8fe73b80256018f5db5fe501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:46 +0000

    Add package jupancon/0.3.2/jupancon-0.3.2-py2.py3-none-any.whl

---
+++ packages/jupancon/jupancon-0.3.2-py2.py3-none-any.whl/jupancon/core.py
@@ -0,0 +1,137 @@
+"""
+Core functional interface, querying the DB and helper functions
+"""
+import warnings
+
+import pandas as pd
+from sqlalchemy import text
+
+from .config import JPConfig
+from .defaults import REDSHIFT_CHUNKSIZE
+
+
+jpc = JPConfig()
+
+
+def change(name, configfile=None):
+    """Change database to `name`"""
+    jpc.change(name)
+
+
+def query_raw(query_string):
+    """
+    Raw SQL, careful, you can break things here
+    """
+    with jpc.connect() as conn:
+        return conn.execute(f"{query_string};")
+
+    jpc.close_tunnel()
+
+
+def query(query_str, chunksize=None):
+    """
+    Query the warehouse with a SELECT or WITH statement.
+
+    returns:
+        pandas.DataFrame
+    """
+    with jpc.connect() as conn:
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            if chunksize:
+                ret = pd.DataFrame()
+                for chunk in pd.read_sql(text(query_str), conn, chunksize=chunksize):
+                    ret = pd.concat([chunk, ret])
+            else:
+                ret = pd.read_sql(query_str, conn)
+
+    jpc.close_tunnel()
+    return ret
+
+
+def list_schemas():
+    """
+    List all schemas
+
+    returns:
+        pandas.DataFrame
+    """
+    if jpc.dbtype == "redshift":
+        return query(
+            """select s.nspname as table_schema,
+                      s.oid as schema_id,
+                      u.usename as owner
+               from pg_catalog.pg_namespace s
+               join pg_catalog.pg_user u on u.usesysid = s.nspowner
+               order by table_schema"""
+        )
+    elif jpc.dbtype == "bigquery":
+        return query("select schema_name FROM INFORMATION_SCHEMA.SCHEMATA")
+
+    elif jpc.dbtype == "databricks":
+        return query("show schemas")
+    else:
+        raise NotImplementedError
+
+
+def list_tables(schema):
+    """
+    List all tables in a schema
+
+    returns:
+        pandas.DataFrame
+    """
+    if jpc.dbtype == "redshift":
+        return query(
+            f"""
+            select table_name, table_type
+            from information_schema.tables t
+            where t.table_schema = '{schema}'
+            order by t.table_name"""
+        )
+
+    elif jpc.dbtype == "bigquery":
+        return query(f"SELECT * FROM {schema}.INFORMATION_SCHEMA.TABLES")
+
+    elif jpc.dbtype == "databricks":
+        return query(f"show tables from {schema}")
+
+    else:
+        raise NotImplementedError
+
+
+def df_to_table(dataframe, schema, table, chunksize=REDSHIFT_CHUNKSIZE):
+    """
+    Write pandas dataframe to the warehouse.
+    """
+    with jpc.connect() as conn:
+        dataframe.to_sql(
+            table,
+            schema=schema,
+            con=conn,
+            index=False,
+            if_exists="replace",
+            chunksize=chunksize,
+        )
+
+    jpc.close_tunnel()
+
+
+def peek(table, limit=3):
+    """
+    Look at the first few lines of your dataset
+    transposed, so all table columns fit the screen
+    nicely.
+    """
+    return query(f"select * from {table} limit {limit}").T
+
+
+def peek_schema(schema, limit=3):
+    """
+    Explore a whole schema by doing peek(table) in every table.
+    """
+    for table in list_tables(schema).table_name:
+        data = peek(f"{schema}.{table}", limit)
+        if not data.empty:
+            print(table)
+            print(f"-------------{data}\n")
