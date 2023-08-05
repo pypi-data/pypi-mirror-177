3ee4d1ae541efcb2882b0995afb4c441f19ecbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:37 +0000

    Add package tiktokapipy/0.1.0/tiktokapipy-0.1.0.tar.gz

---
+++ packages/tiktokapipy/tiktokapipy-0.1.0.tar.gz/tiktokapipy-0.1.0/src/tiktokapipy/api.py
@@ -0,0 +1,347 @@
+import json
+import time
+import warnings
+from typing import List, Literal, Tuple, Type, TypeVar, Union
+
+import requests
+from playwright.sync_api import Page, Request, Route, TimeoutError, sync_playwright
+from tiktokapipy.models.challenge import Challenge, challenge_link
+from tiktokapipy.models.raw_data import (
+    APIResponse,
+    ChallengeResponse,
+    MobileChallengeResponse,
+    MobileResponseMixin,
+    MobileUserResponse,
+    MobileVideoResponse,
+    PrimaryResponseType,
+    UserResponse,
+    VideoResponse,
+)
+from tiktokapipy.models.user import LightUser, User, user_link
+from tiktokapipy.models.video import LightVideo, Video, video_link
+
+DataModelT = TypeVar("DataModelT", bound=PrimaryResponseType)
+
+
+class TikTokAPIError(Exception):
+    pass
+
+
+class LightVideosIter:
+    def __init__(self, videos: List[LightVideo], api: "TikTokAPI"):
+        self._videos = videos
+        self._api = api
+
+    def fetch_video(self) -> Video:
+        video = self._api.video(video_link(self._videos[self.next_up].id))
+        self.next_up += 1
+        return video
+
+    def __iter__(self) -> "LightVideosIter":
+        self.next_up = 0
+        return self
+
+    def __next__(self) -> Video:
+        if self.next_up == len(self._videos):
+            raise StopIteration
+        return self.fetch_video()
+
+
+class LightUserGetter:
+    def __init__(self, user: str, api: "TikTokAPI"):
+        self._user = LightUser(unique_id=user)
+        self._api = api
+
+    def __call__(self) -> User:
+        return self._api.user(self._user.unique_id)
+
+
+class TikTokAPI:
+    def __init__(
+        self,
+        wait_until: Literal[
+            "domcontentloaded", "load", "networkidle", "commit"
+        ] = "networkidle",
+        scroll_down_time: float = 0,
+        headless: bool = None,
+        data_dump_file: str = None,
+        emulate_mobile: bool = False,
+        navigation_timeout: float = 30000,
+        navigation_retries: int = 0,
+        **context_kwargs,
+    ):
+        if scroll_down_time > 0 and headless:
+            raise ValueError("Cannot scroll down with a headless browser")
+        self.wait_until = wait_until
+        self.scroll_down_time = scroll_down_time
+        if headless is None:
+            self.headless = scroll_down_time == 0
+        else:
+            self.headless = headless
+        self.data_dump_file = data_dump_file
+        self.emulate_mobile = emulate_mobile
+        self.context_kwargs = context_kwargs
+        self.navigation_timeout = navigation_timeout
+        self.navigation_retries = navigation_retries
+
+    def __enter__(self):
+        self._playwright = sync_playwright().start()
+
+        self._browser = self.playwright.chromium.launch(headless=self.headless)
+
+        context_kwargs = self.context_kwargs
+        if self.emulate_mobile:
+            context_kwargs.update(self.playwright.devices["iPhone 12"])
+        self._context = self.browser.new_context(**context_kwargs)
+        self.context.set_default_navigation_timeout(self.navigation_timeout)
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.context.close()
+        self.browser.close()
+        self.playwright.stop()
+
+    @property
+    def playwright(self):
+        if not hasattr(self, "_playwright"):
+            raise TikTokAPIError("TikTokAPI must be used as a context manager")
+        return self._playwright
+
+    @property
+    def browser(self):
+        if not hasattr(self, "_browser"):
+            raise TikTokAPIError("TikTokAPI must be used as a context manager")
+        return self._browser
+
+    @property
+    def context(self):
+        if not hasattr(self, "_context"):
+            raise TikTokAPIError("TikTokAPI must be used as a context manager")
+        return self._context
+
+    @property
+    def _light_videos_iter_type(self):
+        return LightVideosIter
+
+    @property
+    def _light_user_getter_type(self):
+        return LightUserGetter
+
+    @property
+    def _challenge_response_type(self):
+        if self.emulate_mobile:
+            return MobileChallengeResponse
+        return ChallengeResponse
+
+    @property
+    def _user_response_type(self):
+        if self.emulate_mobile:
+            return MobileUserResponse
+        return UserResponse
+
+    @property
+    def _video_response_type(self):
+        if self.emulate_mobile:
+            return MobileVideoResponse
+        return VideoResponse
+
+    def challenge(self, challenge_name: str, video_limit: int = 25) -> Challenge:
+        link = challenge_link(challenge_name)
+        response, api_extras = self._scrape_data(link, self._challenge_response_type)
+        return self._extract_challenge_from_response(response, api_extras, video_limit)
+
+    def user(self, user: Union[int, str], video_limit: int = 25) -> User:
+        link = user_link(user)
+        response, api_extras = self._scrape_data(link, self._user_response_type)
+        return self._extract_user_from_response(response, api_extras, video_limit)
+
+    def video(self, link: str) -> Video:
+        response, api_extras = self._scrape_data(link, self._video_response_type)
+        return self._extract_video_from_response(response, api_extras)
+
+    def _scrape_data(
+        self, link: str, data_model: Type[DataModelT]
+    ) -> Tuple[DataModelT, List[APIResponse]]:
+        api_extras: List[APIResponse] = []
+        extras_json: List[dict] = []
+
+        def capture_api_extras(route: Route, request: Request):
+            r = requests.get(
+                request.url,
+                headers=request.headers,
+                cookies={
+                    cookie["name"]: cookie["value"] for cookie in self.context.cookies()
+                },
+            )
+            if len(r.content) > 2:
+                extras_json.append(json.loads(r.content.decode("utf-8")))
+                api_response = APIResponse.parse_raw(r.content)
+                api_extras.append(api_response)
+            route.continue_()
+
+        page = self.context.new_page()
+        page.route("**/api/challenge/item_list/*", capture_api_extras)
+        page.route("**/api/comment/list/*", capture_api_extras)
+        page.route("**/api/post/item_list/*", capture_api_extras)
+
+        for _ in range(self.navigation_retries + 1):
+            try:
+                page.goto(link, wait_until=self.wait_until)
+            except TimeoutError:
+                continue
+            break
+        else:
+            raise TimeoutError(
+                f"Data scraping unable to complete in {self.navigation_timeout / 1000}s "
+                f"(retries: {self.navigation_retries})"
+            )
+
+        if self.scroll_down_time > 0:
+            self._scroll_page_down(page, self.scroll_down_time)
+
+        content = page.content()
+
+        page.close()
+
+        return self._extract_and_dump_data(content, extras_json, data_model), api_extras
+
+    def _extract_and_dump_data(
+        self, page_content: str, extras_json: List[dict], data_model: Type[DataModelT]
+    ):
+        data = page_content.split('<script id="SIGI_STATE" type="application/json">')[
+            1
+        ].split("</script>")[0]
+
+        if self.data_dump_file:
+            with open(
+                f"{self.data_dump_file}.{data_model.__name__}.json",
+                "w+",
+                encoding="utf-8",
+            ) as f:
+                j = json.loads(data)
+                j["extras"] = extras_json
+                json.dump(j, f, indent=2)
+
+        parsed = data_model.parse_raw(data)
+        if isinstance(parsed, MobileResponseMixin):
+            parsed = parsed.to_desktop()
+        return parsed
+
+    def _extract_challenge_from_response(
+        self,
+        response: Union[ChallengeResponse, MobileChallengeResponse],
+        api_extras: List[APIResponse],
+        video_limit: int = 25,
+    ):
+        if response.challenge_page.status_code:
+            raise TikTokAPIError(
+                f"Error in challenge extraction: status code {response.challenge_page.status_code}"
+            )
+        challenge = response.challenge_page.challenge_info.challenge
+        stats = response.challenge_page.challenge_info.stats
+        challenge.stats = stats
+        challenge.videos = self._create_videos_iter(response, api_extras, video_limit)
+
+        return challenge
+
+    def _extract_user_from_response(
+        self,
+        response: Union[UserResponse, MobileUserResponse],
+        api_extras: List[APIResponse],
+        video_limit: int = 25,
+    ):
+        if response.user_page.status_code:
+            raise TikTokAPIError(
+                f"Error in user extraction: status code {response.user_page.status_code}"
+            )
+        name, user = list(response.user_module.users.items())[0]
+        user.stats = response.user_module.stats[name]
+        user.videos = self._create_videos_iter(response, api_extras, video_limit)
+
+        return user
+
+    def _create_videos_iter(
+        self,
+        response: Union[ChallengeResponse, UserResponse],
+        api_extras: List[APIResponse],
+        video_limit: int = 25,
+    ):
+        videos = list(response.item_module.values())
+        if api_extras:
+            for extra in api_extras:
+                if extra.item_list:
+                    videos += extra.item_list
+        if video_limit > 0:
+            videos = videos[:video_limit]
+        return self._light_videos_iter_type(videos, self)
+
+    def _extract_video_from_response(
+        self,
+        response: Union[VideoResponse, MobileVideoResponse],
+        api_extras: List[APIResponse],
+    ):
+        if response.video_page.status_code:
+            if response.video_page.status_code == 10239:
+                raise TikTokAPIError(
+                    "Slideshows can't be extracted without mobile emulation."
+                )
+            raise TikTokAPIError(
+                f"Error in video extraction: status code {response.video_page.status_code}"
+            )
+        video = list(response.item_module.values())[0]
+
+        comments = list(response.comment_item.values()) if response.comment_item else []
+        if api_extras:
+            for extra in api_extras:
+                if extra.comments:
+                    comments += extra.comments
+        for comment in comments:
+            if isinstance(comment.user, User):
+                comment.creator = self._light_user_getter_type(
+                    comment.user.unique_id, self
+                )
+            else:
+                comment.creator = self._light_user_getter_type(comment.user, self)
+
+        video.comments = comments
+        if not video.comments:
+            warnings.warn(
+                "Was unable to collect comments.\nA second attempt might work."
+            )
+        if isinstance(video.author, User):
+            video.creator = self._light_user_getter_type(video.author.unique_id, self)
+        else:
+            video.creator = self._light_user_getter_type(video.author, self)
+
+        return video
+
+    def _scroll_page_down(self, page: Page, scroll_down_time: float):
+        page.evaluate(
+            """
+            var intervalID = setInterval(function () {
+                var scrollingElement = (document.scrollingElement || document.body);
+                scrollingElement.scrollTop = scrollingElement.scrollHeight;
+            }, 200);
+            """
+        )
+        # prev_height = None
+        # done = False
+        # its = 0
+        # while not done:
+        #     curr_height = await page.evaluate('(window.innerHeight + window.scrollY)')
+        #     if not prev_height:
+        #         prev_height = curr_height
+        #         await asyncio.sleep(2)
+        #     elif prev_height == curr_height:
+        #         done = True
+        #     else:
+        #         prev_height = curr_height
+        #         await asyncio.sleep(2)
+        #
+        #     its += 1
+        #     if its >= time:
+        #         done = True
+        time.sleep(scroll_down_time)
+
+        page.evaluate("clearInterval(intervalID)")
---
+++ packages/tiktokapipy/tiktokapipy-0.1.0.tar.gz/tiktokapipy-0.1.0/src/tiktokapipy/models/video.py
@@ -0,0 +1,168 @@
+from typing import Awaitable, Callable, List, Optional, Union
+
+from tiktokapipy.models import CamelCaseModel, TitleCaseModel
+
+
+class VideoStats(CamelCaseModel):
+    digg_count: int
+    share_count: int
+    comment_count: int
+    play_count: int
+
+
+class SubtitleData(TitleCaseModel):
+    language_id: int
+    language_code_name: str
+    url: str
+    url_expire: int
+    format: str
+    version: int
+    source: str
+    # video_subtitle_ID: int
+    size: int
+
+
+class VideoData(CamelCaseModel):
+    id: int
+    height: int
+    width: int
+    duration: int
+    ratio: str
+    format: Optional[str]
+    bitrate: Optional[int]
+    # encoded_type: str
+    # video_quality: str
+    # encode_user_tag: str
+    # codec_type: str
+    # definition: str
+    # subtitle_infos: Optional[List[SubtitleData]]
+
+    ################
+    # Video stills #
+    ################
+    cover: str
+    origin_cover: str
+    dynamic_cover: str
+    share_cover: List[str]
+    reflow_cover: str
+
+    ###############
+    # Video links #
+    ###############
+    play_addr: str
+    download_addr: str
+
+
+class MusicData(CamelCaseModel):
+    id: int
+    title: str
+    play_url: str
+    author_name: str
+    duration: int
+    original: bool
+    album: str
+
+    cover_large: str
+    cover_medium: str
+    cover_thumb: str
+
+    # schedule_search_time: int
+
+
+class ImageUrlList(CamelCaseModel):
+    url_list: List[str]
+
+
+class ImageData(CamelCaseModel):
+    image_u_r_l: ImageUrlList
+    image_width: int
+    image_height: int
+
+
+class ImagePost(CamelCaseModel):
+    images: List[ImageData]
+    cover: ImageData
+    share_cover: ImageData
+    title: str
+
+
+class LightVideo(CamelCaseModel):
+    id: int
+
+
+class Video(LightVideo):
+    #####################
+    # Content and stats #
+    #####################
+    desc: str
+    stats: VideoStats
+    diversification_labels: Optional[List[str]]  # video categories/tags
+    challenges: "Optional[List[Challenge]]"  # hashtags
+    video: VideoData  # contains video file information, subtitle info, and download links
+    music: MusicData  # contains sound/bg music information and download links
+    # digged: bool                               # liked by you
+    # item_comment_status: int
+    location_created: Optional[str]
+    image_post: Optional[ImagePost]
+
+    ######################
+    # Author information #
+    ######################
+    author: "Union[User, str]"
+    nickname: Optional[str]
+    author_id: Optional[int]
+    # author_sec_id: Optional[str]
+    # avatar_thumb: Optional[Union[str, dict]]
+    author_stats: "UserStats"
+
+    ##########################
+    # Duet/stitching/sharing #
+    ##########################
+    # stitch_enabled: bool
+    # duet_enabled: bool
+    # share_enabled: bool
+    # private_item: bool
+    # duet_info: dict
+    # duet_display: int         # display format of duet I think
+    # stitch_display: int       # display format of stitch
+    # mix_info: Optional[dict]
+
+    ##########################################################
+    # Ad and Security info (not sure what most of these are) #
+    ##########################################################
+    is_ad: bool
+    # ad_authorization: bool
+    # ad_label_version: int
+    # original_item: bool
+    # offical_item: bool                # this is a typo
+    # is_activity_item: Optional[bool]
+    # secret: bool
+    # index_enabled: Optional[bool]
+    # show_not_pass: bool
+
+    #################################################
+    # Misc fields (not sure what most of these are) #
+    #################################################
+    # create_time: int
+    # schedule_time: Optional[int]
+    # take_down: Optional[int]
+    # item_mute: bool
+    # text_extra: Optional[list]
+    # effect_stickers: Optional[list]
+    # stickers_on_item: Optional[list]
+    # for_friend: bool
+    # vl1: bool
+
+    comments: "Optional[List[Comment]]"
+    creator: "Optional[Callable[[], Union[User, Awaitable[User]]]]"
+
+
+from tiktokapipy.models.challenge import Challenge  # noqa E402
+from tiktokapipy.models.comment import Comment  # noqa E402
+from tiktokapipy.models.user import User, UserStats  # noqa E402
+
+Video.update_forward_refs()
+
+
+def video_link(_id: int):
+    return f"https://m.tiktok.com/v/{_id}"
