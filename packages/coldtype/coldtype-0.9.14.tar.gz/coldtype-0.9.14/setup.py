e7783f84f244f2a6cbff3fb17fc1c9b7a757ed8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package coldtype/0.9.14/coldtype-0.9.14.tar.gz

---
+++ packages/coldtype/coldtype-0.9.14.tar.gz/coldtype-0.9.14/setup.py
@@ -0,0 +1,125 @@
+import setuptools
+
+long_description = """
+# Coldtype
+
+### Programmatic display typography
+
+More info available at: [coldtype.goodhertz.com](https://coldtype.goodhertz.com)
+"""
+
+setuptools.setup(
+    name="coldtype",
+    version="0.9.14",
+    author="Rob Stenson / Goodhertz",
+    author_email="rob@goodhertz.com",
+    description="Functions for manual vectorized typesetting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goodhertz/coldtype",
+    #package_dir={"": "coldtype"},
+    packages=[
+        "coldtype",
+        "coldtype.fx",
+        "coldtype.img",
+        "coldtype.time",
+        "coldtype.midi",
+        "coldtype.pens",
+        "coldtype.text",
+        "coldtype.grid",
+        "coldtype.color",
+        "coldtype.runon",
+        "coldtype.physics",
+        "coldtype.capture",
+        "coldtype.blender",
+        "coldtype.geometry",
+        "coldtype.time.nle",
+        "coldtype.renderer",
+        "coldtype.text.colr",
+        "coldtype.renderable",
+        "coldtype.fontgoggles",
+        "coldtype.interpolation",
+        "coldtype.runon.mixins",
+        "coldtype.renderer.winman",
+        "coldtype.fontgoggles.font",
+        "coldtype.fontgoggles.misc",
+        "coldtype.fontgoggles.compile",
+    ],
+    include_package_data=True,
+    package_data={
+        "": [
+            "demo/transparency_blocks.png",
+            "demo/RecMono-CasualItalic.ttf",
+            "demo/ColdtypeObviously-VF.ttf",
+            "demo/MutatorSans.ttf",
+            "demo/demo.py",
+            "demo/glyphs.py",
+            "demo/midi.py",
+            "demo/blank.py",
+            "demo/boiler.py",
+            "demo/vf.py",
+            "renderer/.coldtype.py"
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'coldtype = coldtype.renderer:main'
+        ],
+    },
+    extras_require={
+        "skia": [
+            "skia-python>=86.0",
+        ],
+        "viewer": [
+            "glfw",
+            "PyOpenGL",
+            "skia-python>=86.0",
+            "skia-pathops", # can this be taken from skia-python?
+            "ufo2ft",
+            #"ufoLib2",
+            "numpy",
+            "potracer",
+        ],
+        "experimental": [
+            "pynput",
+            "rtmidi",
+            "noise",
+        ],
+        "c": [
+            #"srt",
+            "noise",
+        ],
+        "unicode": [
+            "unicodedata2"
+        ],
+        "blender": [
+            "skia-pathops",
+            "ufo2ft",
+            "ufoLib2", # temporary for a bug in ufo2ft
+        ],
+        "notebook": [
+            "skia-pathops",
+            "skia-python",
+            "potracer",
+        ],
+        "audio": [
+            "pyaudio",
+            "soundfile",
+        ]
+    },
+    install_requires=[
+        "fonttools[ufo]",
+        "blackrenderer>=0.6.0",
+        "fontPens",
+        "easing-functions",
+        "mido",
+        "defcon",
+        "freetype-py",
+        "uharfbuzz>=0.14.0",
+        "python-bidi",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
