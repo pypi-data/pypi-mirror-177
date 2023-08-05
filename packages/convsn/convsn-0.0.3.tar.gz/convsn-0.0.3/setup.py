0e419ad647ece70bc9d7e68368593d8d34fda61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:54 +0000

    Add package convsn/0.0.3/convsn-0.0.3.tar.gz

---
+++ packages/convsn/convsn-0.0.3.tar.gz/convsn-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A conversion package'
+LONG_DESCRIPTION = 'A package that makes it easy to convert values between several units of measurement'
+
+setup(
+    name="convsn",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="oluyemi",
+    author_email="yemiwebby@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='conversion',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
