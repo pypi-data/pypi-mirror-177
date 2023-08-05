af0aa41768c252cc2cdcffad23963cc3adfe47b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:53 +0000

    Add package torcheval-nightly/2022.11.17/torcheval_nightly-2022.11.17-py3-none-any.whl

---
+++ packages/torcheval-nightly/torcheval_nightly-2022.11.17-py3-none-any.whl/torcheval/metrics/functional/classification/__init__.py
@@ -0,0 +1,74 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torcheval.metrics.functional.classification.accuracy import (
+    binary_accuracy,
+    multiclass_accuracy,
+    multilabel_accuracy,
+    topk_multilabel_accuracy,
+)
+
+from torcheval.metrics.functional.classification.auroc import (
+    binary_auroc,
+    multiclass_auroc,
+)
+from torcheval.metrics.functional.classification.binary_normalized_entropy import (
+    binary_normalized_entropy,
+)
+from torcheval.metrics.functional.classification.binned_auroc import (
+    binary_binned_auroc,
+    multiclass_binned_auroc,
+)
+from torcheval.metrics.functional.classification.binned_precision_recall_curve import (
+    binary_binned_precision_recall_curve,
+    multiclass_binned_precision_recall_curve,
+)
+from torcheval.metrics.functional.classification.confusion_matrix import (
+    binary_confusion_matrix,
+    multiclass_confusion_matrix,
+)
+from torcheval.metrics.functional.classification.f1_score import (
+    binary_f1_score,
+    multiclass_f1_score,
+)
+from torcheval.metrics.functional.classification.precision import (
+    binary_precision,
+    multiclass_precision,
+)
+from torcheval.metrics.functional.classification.precision_recall_curve import (
+    binary_precision_recall_curve,
+    multiclass_precision_recall_curve,
+    multilabel_precision_recall_curve,
+)
+from torcheval.metrics.functional.classification.recall import (
+    binary_recall,
+    multiclass_recall,
+)
+
+__all__ = [
+    "binary_auroc",
+    "binary_accuracy",
+    "binary_confusion_matrix",
+    "binary_normalized_entropy",
+    "binary_f1_score",
+    "binary_precision",
+    "binary_precision_recall_curve",
+    "binary_binned_auroc",
+    "binary_binned_precision_recall_curve",
+    "binary_recall",
+    "multiclass_accuracy",
+    "multiclass_auroc",
+    "multiclass_binned_auroc",
+    "multiclass_binned_precision_recall_curve",
+    "multiclass_confusion_matrix",
+    "multiclass_f1_score",
+    "multiclass_precision",
+    "multiclass_precision_recall_curve",
+    "multiclass_recall",
+    "multilabel_accuracy",
+    "multilabel_precision_recall_curve",
+    "topk_multilabel_accuracy",
+]
