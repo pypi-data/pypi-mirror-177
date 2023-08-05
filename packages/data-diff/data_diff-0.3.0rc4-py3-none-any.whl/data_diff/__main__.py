487719b0528ebf8355ec18e9cd273ba6af8b1cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:56 +0000

    Add package data-diff/0.3.0rc4/data_diff-0.3.0rc4-py3-none-any.whl

---
+++ packages/data-diff/data_diff-0.3.0rc4-py3-none-any.whl/data_diff/__main__.py
@@ -0,0 +1,448 @@
+from copy import deepcopy
+import sys
+import time
+import json
+import logging
+from itertools import islice
+from typing import Optional
+
+import rich
+import click
+
+from .utils import eval_name_template, remove_password_from_url, safezip, match_like
+from .diff_tables import Algorithm
+from .hashdiff_tables import HashDiffer, DEFAULT_BISECTION_THRESHOLD, DEFAULT_BISECTION_FACTOR
+from .joindiff_tables import TABLE_WRITE_LIMIT, JoinDiffer
+from .table_segment import TableSegment
+from .sqeleton.databases import create_schema
+from .databases import connect
+from .parse_time import parse_time_before_now, UNITS_STR, ParseError
+from .config import apply_config_from_file
+from .tracking import disable_tracking
+
+
+LOG_FORMAT = "[%(asctime)s] %(levelname)s - %(message)s"
+DATE_FORMAT = "%H:%M:%S"
+
+COLOR_SCHEME = {
+    "+": "green",
+    "-": "red",
+}
+
+
+def _remove_passwords_in_dict(d: dict):
+    for k, v in d.items():
+        if k == "password":
+            d[k] = "*" * len(v)
+        elif isinstance(v, dict):
+            _remove_passwords_in_dict(v)
+        elif k.startswith("database"):
+            d[k] = remove_password_from_url(v)
+
+
+def _get_schema(pair):
+    db, table_path = pair
+    return db.query_table_schema(table_path)
+
+
+def diff_schemas(schema1, schema2, columns):
+    logging.info("Diffing schemas...")
+    attrs = "name", "type", "datetime_precision", "numeric_precision", "numeric_scale"
+    for c in columns:
+        if c is None:  # Skip for convenience
+            continue
+        diffs = []
+        for attr, v1, v2 in safezip(attrs, schema1[c], schema2[c]):
+            if v1 != v2:
+                diffs.append(f"{attr}:({v1} != {v2})")
+        if diffs:
+            logging.warning(f"Schema mismatch in column '{c}': {', '.join(diffs)}")
+
+
+class MyHelpFormatter(click.HelpFormatter):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.indent_increment = 6
+
+    def write_usage(self, prog: str, args: str = "", prefix: Optional[str] = None) -> None:
+        self.write("data-diff - efficiently diff rows across database tables.\n\n")
+        self.write("Usage:\n")
+        self.write(f"  * In-db diff:    {prog} <database1> <table1> <table2> [OPTIONS]\n")
+        self.write(f"  * Cross-db diff: {prog} <database1> <table1> <database2> <table2> [OPTIONS]\n")
+        self.write(f"  * Using config:  {prog} --conf PATH [--run NAME] [OPTIONS]\n")
+
+
+click.Context.formatter_class = MyHelpFormatter
+
+
+@click.command(no_args_is_help=True)
+@click.argument("database1", required=False)
+@click.argument("table1", required=False)
+@click.argument("database2", required=False)
+@click.argument("table2", required=False)
+@click.option(
+    "-k", "--key-columns", default=[], multiple=True, help="Names of primary key columns. Default='id'.", metavar="NAME"
+)
+@click.option("-t", "--update-column", default=None, help="Name of updated_at/last_updated column", metavar="NAME")
+@click.option(
+    "-c",
+    "--columns",
+    default=[],
+    multiple=True,
+    help="Names of extra columns to compare."
+    "Can be used more than once in the same command. "
+    "Accepts a name or a pattern like in SQL. Example: -c col% -c another_col",
+    metavar="NAME",
+)
+@click.option("-l", "--limit", default=None, help="Maximum number of differences to find", metavar="NUM")
+@click.option(
+    "--bisection-factor",
+    default=None,
+    help=f"Segments per iteration. Default={DEFAULT_BISECTION_FACTOR}.",
+    metavar="NUM",
+)
+@click.option(
+    "--bisection-threshold",
+    default=None,
+    help=f"Minimal bisection threshold. Below it, data-diff will download the data and compare it locally. Default={DEFAULT_BISECTION_THRESHOLD}.",
+    metavar="NUM",
+)
+@click.option(
+    "-m",
+    "--materialize",
+    default=None,
+    metavar="TABLE_NAME",
+    help="(joindiff only) Materialize the diff results into a new table in the database. If a table exists by that name, it will be replaced.",
+)
+@click.option(
+    "--min-age",
+    default=None,
+    help="Considers only rows older than specified. Useful for specifying replication lag."
+    "Example: --min-age=5min ignores rows from the last 5 minutes. "
+    f"\nValid units: {UNITS_STR}",
+    metavar="AGE",
+)
+@click.option(
+    "--max-age", default=None, help="Considers only rows younger than specified. See --min-age.", metavar="AGE"
+)
+@click.option("-s", "--stats", is_flag=True, help="Print stats instead of a detailed diff")
+@click.option("-d", "--debug", is_flag=True, help="Print debug info")
+@click.option("--json", "json_output", is_flag=True, help="Print JSONL output for machine readability")
+@click.option("-v", "--verbose", is_flag=True, help="Print extra info")
+@click.option("-i", "--interactive", is_flag=True, help="Confirm queries, implies --debug")
+@click.option("--no-tracking", is_flag=True, help="data-diff sends home anonymous usage data. Use this to disable it.")
+@click.option(
+    "--case-sensitive",
+    is_flag=True,
+    help="Column names are treated as case-sensitive. Otherwise, data-diff corrects their case according to schema.",
+)
+@click.option(
+    "--assume-unique-key",
+    is_flag=True,
+    help="Skip validating the uniqueness of the key column during joindiff, which is costly in non-cloud dbs.",
+)
+@click.option(
+    "--sample-exclusive-rows",
+    is_flag=True,
+    help="Sample several rows that only appear in one of the tables, but not the other. (joindiff only)",
+)
+@click.option(
+    "--materialize-all-rows",
+    is_flag=True,
+    help="Materialize every row, even if they are the same, instead of just the differing rows. (joindiff only)",
+)
+@click.option(
+    "--table-write-limit",
+    default=TABLE_WRITE_LIMIT,
+    help=f"Maximum number of rows to write when creating materialized or sample tables, per thread. Default={TABLE_WRITE_LIMIT}",
+    metavar="COUNT",
+)
+@click.option(
+    "-j",
+    "--threads",
+    default=None,
+    help="Number of worker threads to use per database. Default=1. "
+    "A higher number will increase performance, but take more capacity from your database. "
+    "'serial' guarantees a single-threaded execution of the algorithm (useful for debugging).",
+    metavar="COUNT",
+)
+@click.option(
+    "-w", "--where", default=None, help="An additional 'where' expression to restrict the search space.", metavar="EXPR"
+)
+@click.option("-a", "--algorithm", default=Algorithm.AUTO.value, type=click.Choice([i.value for i in Algorithm]))
+@click.option(
+    "--conf",
+    default=None,
+    help="Path to a configuration.toml file, to provide a default configuration, and a list of possible runs.",
+    metavar="PATH",
+)
+@click.option(
+    "--run",
+    default=None,
+    help="Name of run-configuration to run. If used, CLI arguments for database and table must be omitted.",
+    metavar="NAME",
+)
+def main(conf, run, **kw):
+    indb_syntax = False
+    if kw["table2"] is None and kw["database2"]:
+        # Use the "database table table" form
+        kw["table2"] = kw["database2"]
+        kw["database2"] = kw["database1"]
+        indb_syntax = True
+
+    if conf:
+        kw = apply_config_from_file(conf, run, kw)
+
+    kw["algorithm"] = Algorithm(kw["algorithm"])
+    if kw["algorithm"] == Algorithm.AUTO:
+        kw["algorithm"] = Algorithm.JOINDIFF if indb_syntax else Algorithm.HASHDIFF
+
+    return _main(**kw)
+
+
+def _main(
+    database1,
+    table1,
+    database2,
+    table2,
+    key_columns,
+    update_column,
+    columns,
+    limit,
+    algorithm,
+    bisection_factor,
+    bisection_threshold,
+    min_age,
+    max_age,
+    stats,
+    debug,
+    verbose,
+    interactive,
+    no_tracking,
+    threads,
+    case_sensitive,
+    json_output,
+    where,
+    assume_unique_key,
+    sample_exclusive_rows,
+    materialize_all_rows,
+    table_write_limit,
+    materialize,
+    threads1=None,
+    threads2=None,
+    __conf__=None,
+):
+
+    if no_tracking:
+        disable_tracking()
+
+    if interactive:
+        debug = True
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+        if __conf__:
+            __conf__ = deepcopy(__conf__)
+            _remove_passwords_in_dict(__conf__)
+            logging.debug(f"Applied run configuration: {__conf__}")
+    elif verbose:
+        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    else:
+        logging.basicConfig(level=logging.WARNING, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+    if limit and stats:
+        logging.error("Cannot specify a limit when using the -s/--stats switch")
+        return
+
+    key_columns = key_columns or ("id",)
+    bisection_factor = DEFAULT_BISECTION_FACTOR if bisection_factor is None else int(bisection_factor)
+    bisection_threshold = DEFAULT_BISECTION_THRESHOLD if bisection_threshold is None else int(bisection_threshold)
+
+    threaded = True
+    if threads is None:
+        threads = 1
+    elif isinstance(threads, str) and threads.lower() == "serial":
+        assert not (threads1 or threads2)
+        threaded = False
+        threads = 1
+    else:
+        try:
+            threads = int(threads)
+        except ValueError:
+            logging.error("Error: threads must be a number, or 'serial'.")
+            return
+        if threads < 1:
+            logging.error("Error: threads must be >= 1")
+            return
+
+    start = time.monotonic()
+
+    try:
+        options = dict(
+            min_update=max_age and parse_time_before_now(max_age),
+            max_update=min_age and parse_time_before_now(min_age),
+            case_sensitive=case_sensitive,
+            where=where,
+        )
+    except ParseError as e:
+        logging.error(f"Error while parsing age expression: {e}")
+        return
+
+    if database1 is None or database2 is None:
+        logging.error(
+            f"Error: Databases not specified. Got {database1} and {database2}. Use --help for more information."
+        )
+        return
+
+    try:
+        db1 = connect(database1, threads1 or threads)
+        if database1 == database2:
+            db2 = db1
+        else:
+            db2 = connect(database2, threads2 or threads)
+    except Exception as e:
+        logging.error(e)
+        return
+
+    dbs = db1, db2
+
+    if interactive:
+        for db in dbs:
+            db.enable_interactive()
+
+    if algorithm == Algorithm.JOINDIFF:
+        differ = JoinDiffer(
+            threaded=threaded,
+            max_threadpool_size=threads and threads * 2,
+            validate_unique_key=not assume_unique_key,
+            sample_exclusive_rows=sample_exclusive_rows,
+            materialize_all_rows=materialize_all_rows,
+            table_write_limit=table_write_limit,
+            materialize_to_table=materialize and db1.parse_table_name(eval_name_template(materialize)),
+        )
+    else:
+        assert algorithm == Algorithm.HASHDIFF
+        differ = HashDiffer(
+            bisection_factor=bisection_factor,
+            bisection_threshold=bisection_threshold,
+            threaded=threaded,
+            max_threadpool_size=threads and threads * 2,
+        )
+
+    table_names = table1, table2
+    table_paths = [db.parse_table_name(t) for db, t in safezip(dbs, table_names)]
+
+    schemas = list(differ._thread_map(_get_schema, safezip(dbs, table_paths)))
+    schema1, schema2 = schemas = [
+        create_schema(db, table_path, schema, case_sensitive)
+        for db, table_path, schema in safezip(dbs, table_paths, schemas)
+    ]
+
+    mutual = schema1.keys() & schema2.keys()  # Case-aware, according to case_sensitive
+    logging.debug(f"Available mutual columns: {mutual}")
+
+    expanded_columns = set()
+    for c in columns:
+        cc = c if case_sensitive else c.lower()
+        match = set(match_like(cc, mutual))
+        if not match:
+            m1 = None if any(match_like(cc, schema1.keys())) else f"{db1}/{table1}"
+            m2 = None if any(match_like(cc, schema2.keys())) else f"{db2}/{table2}"
+            not_matched = ", ".join(m for m in [m1, m2] if m)
+            raise ValueError(f"Column '{c}' not found in: {not_matched}")
+
+        expanded_columns |= match
+
+    columns = tuple(expanded_columns - {*key_columns, update_column})
+
+    if db1 is db2:
+        diff_schemas(
+            schema1,
+            schema2,
+            (
+                *key_columns,
+                update_column,
+                *columns,
+            ),
+        )
+
+    logging.info(f"Diffing using columns: key={key_columns} update={update_column} extra={columns}")
+
+    segments = [
+        TableSegment(db, table_path, key_columns, update_column, columns, **options)._with_raw_schema(raw_schema)
+        for db, table_path, raw_schema in safezip(dbs, table_paths, schemas)
+    ]
+
+    diff_iter = differ.diff_tables(*segments)
+    info = diff_iter.info_tree.info
+
+    if limit:
+        diff_iter = islice(diff_iter, int(limit))
+
+    if stats:
+        diff = list(diff_iter)
+        key_columns_len = len(key_columns)
+
+        diff_by_key = {}
+        for sign, values in diff:
+            k = values[:key_columns_len]
+            if k in diff_by_key:
+                assert sign != diff_by_key[k]
+                diff_by_key[k] = "!"
+            else:
+                diff_by_key[k] = sign
+
+        diff_by_sign = {k: 0 for k in "+-!"}
+        for sign in diff_by_key.values():
+            diff_by_sign[sign] += 1
+
+        table1_count = info.rowcounts[1]
+        table2_count = info.rowcounts[2]
+        unchanged = table1_count - diff_by_sign["-"] - diff_by_sign["!"]
+        diff_percent = 1 - unchanged / max(table1_count, table2_count)
+
+        if json_output:
+            json_output = {
+                "rows_A": table1_count,
+                "rows_B": table2_count,
+                "exclusive_A": diff_by_sign["-"],
+                "exclusive_B": diff_by_sign["+"],
+                "updated": diff_by_sign["!"],
+                "unchanged": unchanged,
+                "total": sum(diff_by_sign.values()),
+                "stats": differ.stats,
+            }
+            rich.print_json(json.dumps(json_output))
+        else:
+            rich.print(f"{table1_count} rows in table A")
+            rich.print(f"{table2_count} rows in table B")
+            rich.print(f"{diff_by_sign['-']} rows exclusive to table A (not present in B)")
+            rich.print(f"{diff_by_sign['+']} rows exclusive to table B (not present in A)")
+            rich.print(f"{diff_by_sign['!']} rows updated")
+            rich.print(f"{unchanged} rows unchanged")
+            rich.print(f"{100*diff_percent:.2f}% difference score")
+
+            if differ.stats:
+                print("\nExtra-Info:")
+                for k, v in sorted(differ.stats.items()):
+                    rich.print(f"  {k} = {v}")
+    else:
+        for op, values in diff_iter:
+            color = COLOR_SCHEME[op]
+
+            if json_output:
+                jsonl = json.dumps([op, list(values)])
+                rich.print(f"[{color}]{jsonl}[/{color}]")
+            else:
+                text = f"{op} {', '.join(map(str, values))}"
+                rich.print(f"[{color}]{text}[/{color}]")
+
+            sys.stdout.flush()
+
+    end = time.monotonic()
+
+    logging.info(f"Duration: {end-start:.2f} seconds.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/data-diff/data_diff-0.3.0rc4-py3-none-any.whl/data_diff/diff_tables.py
@@ -0,0 +1,253 @@
+"""Provides classes for performing a table diff
+"""
+
+import re
+import time
+from abc import ABC, abstractmethod
+from enum import Enum
+from contextlib import contextmanager
+from operator import methodcaller
+from typing import Iterable, Tuple, Iterator, Optional
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from runtype import dataclass
+
+from data_diff.info_tree import InfoTree, SegmentInfo
+
+from .utils import run_as_daemon, safezip, getLogger
+from .thread_utils import ThreadedYielder
+from .table_segment import TableSegment
+from .tracking import create_end_event_json, create_start_event_json, send_event_json, is_tracking_enabled
+from .sqeleton.databases import IKey
+
+logger = getLogger(__name__)
+
+
+class Algorithm(Enum):
+    AUTO = "auto"
+    JOINDIFF = "joindiff"
+    HASHDIFF = "hashdiff"
+
+
+DiffResult = Iterator[Tuple[str, tuple]]  # Iterator[Tuple[Literal["+", "-"], tuple]]
+
+
+def truncate_error(error: str):
+    first_line = error.split("\n", 1)[0]
+    return re.sub("'(.*?)'", "'***'", first_line)
+
+
+@dataclass
+class ThreadBase:
+    "Provides utility methods for optional threading"
+
+    threaded: bool = True
+    max_threadpool_size: Optional[int] = 1
+
+    def _thread_map(self, func, iterable):
+        if not self.threaded:
+            return map(func, iterable)
+
+        with ThreadPoolExecutor(max_workers=self.max_threadpool_size) as task_pool:
+            return task_pool.map(func, iterable)
+
+    def _threaded_call(self, func, iterable):
+        "Calls a method for each object in iterable."
+        return list(self._thread_map(methodcaller(func), iterable))
+
+    def _thread_as_completed(self, func, iterable):
+        if not self.threaded:
+            yield from map(func, iterable)
+            return
+
+        with ThreadPoolExecutor(max_workers=self.max_threadpool_size) as task_pool:
+            futures = [task_pool.submit(func, item) for item in iterable]
+            for future in as_completed(futures):
+                yield future.result()
+
+    def _threaded_call_as_completed(self, func, iterable):
+        "Calls a method for each object in iterable. Returned in order of completion."
+        return self._thread_as_completed(methodcaller(func), iterable)
+
+    @contextmanager
+    def _run_in_background(self, *funcs):
+        with ThreadPoolExecutor(max_workers=self.max_threadpool_size) as task_pool:
+            futures = [task_pool.submit(f) for f in funcs if f is not None]
+            yield futures
+            for f in futures:
+                f.result()
+
+
+@dataclass
+class DiffResultWrapper:
+    diff: iter  # DiffResult
+    info_tree: InfoTree
+
+    def __iter__(self):
+        return iter(self.diff)
+
+
+class TableDiffer(ThreadBase, ABC):
+    bisection_factor = 32
+    stats: dict = {}
+
+    def diff_tables(self, table1: TableSegment, table2: TableSegment, info_tree: InfoTree = None) -> DiffResultWrapper:
+        """Diff the given tables.
+
+        Parameters:
+            table1 (TableSegment): The "before" table to compare. Or: source table
+            table2 (TableSegment): The "after" table to compare. Or: target table
+
+        Returns:
+            An iterator that yield pair-tuples, representing the diff. Items can be either -
+            ('-', row) for items in table1 but not in table2.
+            ('+', row) for items in table2 but not in table1.
+            Where `row` is a tuple of values, corresponding to the diffed columns.
+        """
+        if info_tree is None:
+            info_tree = InfoTree(SegmentInfo([table1, table2]))
+        return DiffResultWrapper(self._diff_tables_wrapper(table1, table2, info_tree), info_tree)
+
+    def _diff_tables_wrapper(self, table1: TableSegment, table2: TableSegment, info_tree: InfoTree) -> DiffResult:
+        if is_tracking_enabled():
+            options = dict(self)
+            options["differ_name"] = type(self).__name__
+            event_json = create_start_event_json(options)
+            run_as_daemon(send_event_json, event_json)
+
+        start = time.monotonic()
+        error = None
+        try:
+
+            # Query and validate schema
+            table1, table2 = self._threaded_call("with_schema", [table1, table2])
+            self._validate_and_adjust_columns(table1, table2)
+
+            yield from self._diff_tables_root(table1, table2, info_tree)
+
+        except BaseException as e:  # Catch KeyboardInterrupt too
+            error = e
+        finally:
+            info_tree.aggregate_info()
+
+            if is_tracking_enabled():
+                runtime = time.monotonic() - start
+                table1_count = info_tree.info.rowcounts[1]
+                table2_count = info_tree.info.rowcounts[2]
+                diff_count = info_tree.info.diff_count
+                err_message = truncate_error(repr(error))
+                event_json = create_end_event_json(
+                    error is None,
+                    runtime,
+                    table1.database.name,
+                    table2.database.name,
+                    table1_count,
+                    table2_count,
+                    diff_count,
+                    err_message,
+                )
+                send_event_json(event_json)
+
+            if error:
+                raise error
+
+    def _validate_and_adjust_columns(self, table1: TableSegment, table2: TableSegment) -> DiffResult:
+        pass
+
+    def _diff_tables_root(self, table1: TableSegment, table2: TableSegment, info_tree: InfoTree) -> DiffResult:
+        return self._bisect_and_diff_tables(table1, table2, info_tree)
+
+    @abstractmethod
+    def _diff_segments(
+        self,
+        ti: ThreadedYielder,
+        table1: TableSegment,
+        table2: TableSegment,
+        info_tree: InfoTree,
+        max_rows: int,
+        level=0,
+        segment_index=None,
+        segment_count=None,
+    ):
+        ...
+
+    def _bisect_and_diff_tables(self, table1, table2, info_tree):
+        if len(table1.key_columns) > 1:
+            raise NotImplementedError("Composite key not supported yet!")
+        if len(table2.key_columns) > 1:
+            raise NotImplementedError("Composite key not supported yet!")
+        (key1,) = table1.key_columns
+        (key2,) = table2.key_columns
+
+        key_type = table1._schema[key1]
+        key_type2 = table2._schema[key2]
+        if not isinstance(key_type, IKey):
+            raise NotImplementedError(f"Cannot use column of type {key_type} as a key")
+        if not isinstance(key_type2, IKey):
+            raise NotImplementedError(f"Cannot use column of type {key_type2} as a key")
+        assert key_type.python_type is key_type2.python_type
+
+        # Query min/max values
+        key_ranges = self._threaded_call_as_completed("query_key_range", [table1, table2])
+
+        # Start with the first completed value, so we don't waste time waiting
+        min_key1, max_key1 = self._parse_key_range_result(key_type, next(key_ranges))
+
+        table1, table2 = [t.new(min_key=min_key1, max_key=max_key1) for t in (table1, table2)]
+
+        logger.info(
+            f"Diffing segments at key-range: {table1.min_key}..{table2.max_key}. "
+            f"size: table1 <= {table1.approximate_size()}, table2 <= {table2.approximate_size()}"
+        )
+
+        ti = ThreadedYielder(self.max_threadpool_size)
+        # Bisect (split) the table into segments, and diff them recursively.
+        ti.submit(self._bisect_and_diff_segments, ti, table1, table2, info_tree)
+
+        # Now we check for the second min-max, to diff the portions we "missed".
+        min_key2, max_key2 = self._parse_key_range_result(key_type, next(key_ranges))
+
+        if min_key2 < min_key1:
+            pre_tables = [t.new(min_key=min_key2, max_key=min_key1) for t in (table1, table2)]
+            ti.submit(self._bisect_and_diff_segments, ti, *pre_tables, info_tree)
+
+        if max_key2 > max_key1:
+            post_tables = [t.new(min_key=max_key1, max_key=max_key2) for t in (table1, table2)]
+            ti.submit(self._bisect_and_diff_segments, ti, *post_tables, info_tree)
+
+        return ti
+
+    def _parse_key_range_result(self, key_type, key_range):
+        mn, mx = key_range
+        cls = key_type.make_value
+        # We add 1 because our ranges are exclusive of the end (like in Python)
+        try:
+            return cls(mn), cls(mx) + 1
+        except (TypeError, ValueError) as e:
+            raise type(e)(f"Cannot apply {key_type} to '{mn}', '{mx}'.") from e
+
+    def _bisect_and_diff_segments(
+        self,
+        ti: ThreadedYielder,
+        table1: TableSegment,
+        table2: TableSegment,
+        info_tree: InfoTree,
+        level=0,
+        max_rows=None,
+    ):
+        assert table1.is_bounded and table2.is_bounded
+
+        # Choose evenly spaced checkpoints (according to min_key and max_key)
+        biggest_table = max(table1, table2, key=methodcaller("approximate_size"))
+        checkpoints = biggest_table.choose_checkpoints(self.bisection_factor - 1)
+
+        # Create new instances of TableSegment between each checkpoint
+        segmented1 = table1.segment_by_checkpoints(checkpoints)
+        segmented2 = table2.segment_by_checkpoints(checkpoints)
+
+        # Recursively compare each pair of corresponding segments between table1 and table2
+        for i, (t1, t2) in enumerate(safezip(segmented1, segmented2)):
+            info_node = info_tree.add_node(t1, t2, max_rows=max_rows)
+            ti.submit(
+                self._diff_segments, ti, t1, t2, info_node, max_rows, level + 1, i + 1, len(segmented1), priority=level
+            )
