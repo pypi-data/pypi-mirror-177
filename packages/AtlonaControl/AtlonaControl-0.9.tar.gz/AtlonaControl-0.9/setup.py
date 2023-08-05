eadfad3cf5dfaabf7480738045d57a7ca1f01edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:49 +0000

    Add package AtlonaControl/0.9/AtlonaControl-0.9.tar.gz

---
+++ packages/AtlonaControl/AtlonaControl-0.9.tar.gz/AtlonaControl-0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='AtlonaControl',
+    version='0.9',
+    license='MIT',
+    author="sdoDevelop",
+    author_email='sdoconnell92@gmail.com',
+    packages=find_packages('AtlonaControl'),
+    package_dir={'': 'AtlonaControl'},
+    url='https://github.com/sdoDevelop/AtlonaControl',
+    keywords='atlona',
+    install_requires=[],
+)
