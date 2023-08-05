bbf54609dc248aa07b53e24de60f6a6b7675a63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:18 +0000

    Add package MySEAS-OLD/1.0.3/MySEAS_OLD-1.0.3.tar.gz

---
+++ packages/MySEAS-OLD/MySEAS_OLD-1.0.3.tar.gz/MySEAS_OLD-1.0.3/SEAS/Engine/Components/renderPolyComp.py
@@ -0,0 +1,12 @@
+from SEAS.Engine.Setup import *
+from SEAS.Engine.Core import *
+
+
+class RenderPoly:
+    def start(self):
+        self.transformComp = SEAS.getScene().getComponent('TransformPoly')
+
+    def update(self):
+        objectColor = SEAS.getMaterial()
+        points = self.transformComp.points
+        pygame.draw.polygon(SEAS.coreModules['Screen'].wn, objectColor, points)
