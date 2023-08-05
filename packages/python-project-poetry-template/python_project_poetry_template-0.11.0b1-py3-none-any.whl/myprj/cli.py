edd239a15c56c38ce5c160d4017c9ee1c3491d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:20 +0000

    Add package python-project-poetry-template/0.11.0b1/python_project_poetry_template-0.11.0b1-py3-none-any.whl

---
+++ packages/python-project-poetry-template/python_project_poetry_template-0.11.0b1-py3-none-any.whl/myprj/cli.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Project main CLI interface
+"""
+
+from myprj.__version__ import __version__
+
+
+def cli():
+    "Main cli Interface"
+    print(f"Hello CLI 2! : {__version__}")
