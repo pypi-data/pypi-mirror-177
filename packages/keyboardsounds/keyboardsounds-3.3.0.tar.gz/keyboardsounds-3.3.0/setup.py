e5910c0646acfa940edec253e1a2916d74ed7e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package keyboardsounds/3.3.0/keyboardsounds-3.3.0.tar.gz

---
+++ packages/keyboardsounds/keyboardsounds-3.3.0.tar.gz/keyboardsounds-3.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+   name='keyboardsounds',
+   version='3.3.0',
+   description='Adds the ability to play sounds while typing on any system.',
+   author='Nathan Fiscaletti',
+   author_email='nate.fiscaletti@gmail.com',
+   packages=['keyboardsounds'],
+   install_requires=['pygame', 'pynput', 'psutil', 'imageio-ffmpeg'],
+   package_data={
+    'keyboardsounds': [
+      'profiles/ios/*',
+      'profiles/mx-speed-silver/*',
+      'profiles/opera-gx/*',
+      'profiles/osu-nagatoro/*',
+      'profiles/telios-v2/*',
+      'profiles/typewriter/*',
+    ],
+   },
+   entry_points={
+         'console_scripts': [
+                'keyboardsounds = keyboardsounds.main:main',
+         ],
+   },
+)
