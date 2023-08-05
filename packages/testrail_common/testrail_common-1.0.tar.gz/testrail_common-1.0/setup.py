501c34a6e5041badbe1bb3966b205e0af0cffdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:14 +0000

    Add package testrail_common/1.0/testrail_common-1.0.tar.gz

---
+++ packages/testrail_common/testrail_common-1.0.tar.gz/testrail_common-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name="testrail_common",
+    packages=['testrail_common'],
+    version="1.0",
+    description='Common TestRail APIs that can be used for all Dexcom VnV groups',
+    author='Shervin Haghgouy',
+    author_email='shervin.haghgouy@dexcom.com',
+    url='https://github.com/dexcom-inc/VnV-Common-TestRail',
+    download_url='https://github.com/dexcom-inc/VnV-Common-TestRail/archive/refs/tags/1.0.tar.gz',
+    classifiers=[]
+)
