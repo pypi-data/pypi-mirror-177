89350ce1eaad87ca6533425376bb8d2b56c0d6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:10 +0000

    Add package AutoMonkey/0.1.4/AutoMonkey-0.1.4-py2.py3-none-any.whl

---
+++ packages/AutoMonkey/AutoMonkey-0.1.4-py2.py3-none-any.whl/automonkey/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for AutoMonkey."""
+
+__author__ = """Mihail-Cosmin Munteanu"""
+__email__ = 'munteanumihailcosmin@gmail.com'
+__version__ = '0.1.4'
+
+from .automonkey import *
