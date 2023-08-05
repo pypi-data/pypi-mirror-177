04ead172e56429bed30bee71d87ff4b64476904e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:28 +0000

    Add package daiasataraiabauataiaoanasa/0.1/daiasataraiabauataiaoanasa-0.1.tar.gz

---
+++ packages/daiasataraiabauataiaoanasa/daiasataraiabauataiaoanasa-0.1.tar.gz/daiasataraiabauataiaoanasa-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='daiasataraiabauataiaoanasa',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['daiasataraiabauataiaoanasa'],
+      zip_safe=False)
