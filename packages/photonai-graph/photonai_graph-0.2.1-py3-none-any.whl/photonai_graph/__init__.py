110850c727c2c4f14463b3da61850f0368dd6125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:08 +0000

    Add package photonai-graph/0.2.1/photonai_graph-0.2.1-py3-none-any.whl

---
+++ packages/photonai-graph/photonai_graph-0.2.1-py3-none-any.whl/photonai_graph/__init__.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+from photonai.photonlogger import logger
+from photonai_graph.version import __version__
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+registered_file = os.path.join(current_path, "registered")
+logger.info("Checking Graph Module Registration")
+if not os.path.isfile(registered_file):  # pragma: no cover
+    logger.info("Registering Graph Module")
+    from photonai.base import PhotonRegistry
+    reg = PhotonRegistry()
+    reg.add_module(os.path.join(current_path, "photonai_graph.json"))
+    with open(os.path.join(registered_file), "w") as f:
+        f.write(str(datetime.now()))
