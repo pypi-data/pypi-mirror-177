4e1fe85afbc950d18ef3a619f47616996194a00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:55 +0000

    Add package ASRscsvmv/0.0.1/ASRscsvmv-0.0.1.tar.gz

---
+++ packages/ASRscsvmv/ASRscsvmv-0.0.1.tar.gz/ASRscsvmv-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="ASRscsvmv",
+version="0.0.1",
+description="This package reconizes and translates your words",
+author="SCSVMV 22-23 PROJECT 17",
+packages=['ASRscsvmv'],
+install_requires=['vosk','PySimpleGUI','argostranslate','streamlit'])
