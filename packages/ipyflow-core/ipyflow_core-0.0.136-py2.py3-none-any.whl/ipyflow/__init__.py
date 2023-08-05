3420557e68b4b36c900159d62a227dba670ad14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:45 +0000

    Add package ipyflow-core/0.0.136/ipyflow_core-0.0.136-py2.py3-none-any.whl

---
+++ packages/ipyflow-core/ipyflow_core-0.0.136-py2.py3-none-any.whl/ipyflow/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from ipyflow.api import *
+
+
+# Jupyter Extension points
+def _jupyter_nbextension_paths():
+    return [
+        dict(
+            section="notebook",
+            # the path is relative to the `my_fancy_module` directory
+            src="resources/nbextension",
+            # directory in the `nbextension/` namespace
+            dest="ipyflow",
+            # _also_ in the `nbextension/` namespace
+            require="ipyflow/index",
+        )
+    ]
+
+
+def load_jupyter_server_extension(nbapp):
+    pass
+
+
+from . import _version
+__version__ = _version.get_versions()['version']
