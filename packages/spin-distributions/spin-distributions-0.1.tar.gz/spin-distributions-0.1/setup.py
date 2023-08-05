9f06219d3e632ea794d563963ebc9bbbfa4cdc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:29 +0000

    Add package spin-distributions/0.1/spin-distributions-0.1.tar.gz

---
+++ packages/spin-distributions/spin-distributions-0.1.tar.gz/spin-distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spin-distributions",
+    version="0.1",
+    description="Gaussian & Binomial Distributions",
+    packages=["spin-distributions"],
+    author='Adedayo Agboola',
+    author_email='maxkreativity@gmail.com',
+    zip_safe=False,
+)
