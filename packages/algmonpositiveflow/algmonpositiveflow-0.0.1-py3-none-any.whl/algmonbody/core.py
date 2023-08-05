b2727c7c2fe13c1c2624e1f1f8065828b533c2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package algmonpositiveflow/0.0.1/algmonpositiveflow-0.0.1-py3-none-any.whl

---
+++ packages/algmonpositiveflow/algmonpositiveflow-0.0.1-py3-none-any.whl/algmonbody/core.py
@@ -0,0 +1,24 @@
+"""core body code
+"""
+
+class Body:
+	"""Body class
+
+	The primary purpose of the 'Body' class is
+
+	Arguments
+	---------
+
+	Example
+	-------
+	>>> from XXXXX import XXXXX
+	>>> class SimpleBody(Body):
+	...	def 
+	...	def
+	>>> model = 
+	>>> body = 
+	>>> body.fit( ... )
+	"""
+
+	def __init__(self,):
+		print("the Algmon Body Init ...")
