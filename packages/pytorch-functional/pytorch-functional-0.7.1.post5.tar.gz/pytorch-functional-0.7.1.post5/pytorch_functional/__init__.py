27a80de47987763c3bdd4fb4bf8e262466a1e1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:11 +0000

    Add package pytorch-functional/0.7.1.post5/pytorch-functional-0.7.1.post5.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.7.1.post5.tar.gz/pytorch-functional-0.7.1.post5/pytorch_functional/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2022 Szymon Mikler
+
+import logging
+
+logging.warning("Pytorch Functional was renamed to Pytorch Symbolic!")
+logging.warning("Please import pytorch_symbolic instead!")
+
+from pytorch_symbolic import *
+
