0fddf56f168a0c33071330a8b14743df83a4ca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package crtmlib/0.0.6/crtmlib-0.0.6.tar.gz

---
+++ packages/crtmlib/crtmlib-0.0.6.tar.gz/crtmlib-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.6'
+PACKAGE_NAME = 'crtmlib' 
+AUTHOR = 'Juan Pedro Hurtado Masero'
+AUTHOR_EMAIL = 'juanpepitt@gmail.com'
+URL = 'https://github.com/Juanpepitt/crtmfuncs.git'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'obtiene funciones contenidas en un .py ubicado en un bucket de AWS S3' 
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-16')
+LONG_DESC_TYPE = "text/markdown"
+
+#INSTALL_REQUIRES = 'requirements.txt'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
