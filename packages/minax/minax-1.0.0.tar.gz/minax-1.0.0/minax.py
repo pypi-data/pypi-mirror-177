98621069d95e5e53039f807cafdfb973f4da956b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:08 +0000

    Add package minax/1.0.0/minax-1.0.0.tar.gz

---
+++ packages/minax/minax-1.0.0.tar.gz/minax-1.0.0/minax.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+def min_max(num_list):
+    num_min=min(num_list)
+    num_max=max(num_list)
+    print('最小值为：',num_min,'  最大值为：',num_max)
---
+++ packages/minax/minax-1.0.0.tar.gz/minax-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name='minax',
+      version='1.0.0',
+      py_modules=['minax'],
+      author='zhaojxing',
+      author_email='759112039@qq.com',
+      url='https://gitee.com/zjxperfect/projects?sort=&scope=personal&state=',
+      description='A simple printer of minax',
+     )
