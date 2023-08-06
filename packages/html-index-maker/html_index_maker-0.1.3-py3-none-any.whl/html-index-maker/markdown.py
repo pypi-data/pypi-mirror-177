8d36db07e57a48720ddb9e7d6b8e3807c9654f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:45 +0000

    Add package html-index-maker/0.1.3/html_index_maker-0.1.3-py3-none-any.whl

---
+++ packages/html-index-maker/html_index_maker-0.1.3-py3-none-any.whl/html-index-maker/markdown.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import re
+
+
+def read_markdown(filepath: str) -> str:
+    pattern = "(#+.*)"
+    with open(Path(filepath).resolve(), "r") as file:
+        data = re.findall(pattern, file.read())
+        print(data)
+
+
+def make_markdown(data: list) -> str:
+    result = "# Index\n"
+    for file in data:
+        index = [0, 0, 0]
+        file_url = Path(file["file"]).with_suffix(".md")
+        result += f"## [{Path(file['file']).stem.capitalize()}]({file_url})\n\n"
+        for header in file["headers"]:
+            text = header["text"]
+            level = "*" * (int(header["tag"].replace("h", "")) - 1)
+            # calculate index
+            index = [*index[: len(level)], *[1 for _ in range(len(index) - len(level))]]
+            index[len(level) - 1] += 1
+            num = "".join(f"{i}." for i in index[: len(level)])
+            # get link text
+            link = f'[{"&nbsp;"*(len(level)-1)*2} {num} {text}]({file_url}#{header["id"]})\n\n'
+            result += f"{link}"
+    return result
