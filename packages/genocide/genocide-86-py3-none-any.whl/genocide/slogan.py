6019b93e3da09e6d74062be4a2159813e5a37fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:19 +0000

    Add package genocide/86/genocide-86-py3-none-any.whl

---
+++ packages/genocide/genocide-86-py3-none-any.whl/genocide/slogan.py
@@ -0,0 +1,12 @@
+# This file is placed in the Public Domain
+# pylint: disable=C0114,C0115,C0116
+
+
+"slogan"
+
+
+TXT = "Assembly. Court. Prosecutor. Reconsider OTP-CR-!17/19."
+
+
+def slogan(event):
+    event.reply(TXT)
