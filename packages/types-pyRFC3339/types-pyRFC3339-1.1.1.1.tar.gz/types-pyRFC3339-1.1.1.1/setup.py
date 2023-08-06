f87f9d7d00883efb8176d2a0c4c0f3d001a0ae31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:56 +0000

    Add package types-pyRFC3339/1.1.1.1/types-pyRFC3339-1.1.1.1.tar.gz

---
+++ packages/types-pyRFC3339/types-pyRFC3339-1.1.1.1.tar.gz/types-pyRFC3339-1.1.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-pyRFC3339"
+description = "Typing stubs for pyRFC3339"
+long_description = '''
+## Typing stubs for pyRFC3339
+
+This is a PEP 561 type stub package for the `pyRFC3339` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `pyRFC3339`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/pyRFC3339. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `8a46e34bc37e312484a0b34ef2446d3155e9a3e8`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.1.1.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/pyRFC3339.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['pyrfc3339-stubs'],
+      package_data={'pyrfc3339-stubs': ['__init__.pyi', 'generator.pyi', 'parser.pyi', 'utils.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
