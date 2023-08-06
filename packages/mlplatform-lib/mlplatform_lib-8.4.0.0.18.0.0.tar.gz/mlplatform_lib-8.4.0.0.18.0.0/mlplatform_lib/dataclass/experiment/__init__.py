2805fead106b78f6269205908ed95e44b2eb2826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:08 +0000

    Add package mlplatform-lib/8.4.0.0.18.0.0/mlplatform_lib-8.4.0.0.18.0.0.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.18.0.0.tar.gz/mlplatform_lib-8.4.0.0.18.0.0/mlplatform_lib/dataclass/experiment/__init__.py
@@ -0,0 +1,5 @@
+from .experiment_predefinedai_dto import ExperimentPredefinedaiDto
+from .experiment_automl_dto import ExperimentAutomlDto
+
+
+__all__ = ["ExperimentPredefinedaiDto", "ExperimentAutomlDto"]
