f978cde6e5c9f12481d3102f804443583eedbbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:52:29 +0000

    Add package qasem/0.1.5/qasem-0.1.5.tar.gz

---
+++ packages/qasem/qasem-0.1.5.tar.gz/qasem-0.1.5/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("qasem/version.txt", "r") as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="qasem",
+    version=version,
+    author="Ayal Klein, Ruben Wolhandler, Ron Eliav",
+    author_email="ayal.s.klein@gmail.com",
+    description="package for QA-based Semantics - representing textual information via question-answer pairs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kleinay/QASem",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'transformers==4.15.0',
+        'spacy>=3.0.0',
+        'qanom',
+        'roleqgen'
+    ],
+    package_data={
+        "": ["qasem/data/connectives_small_set.txt"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
