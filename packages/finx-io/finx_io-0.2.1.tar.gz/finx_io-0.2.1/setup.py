b8c6f28e5e4d6346c00932864b0ce4f8632f023c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:39 +0000

    Add package finx-io/0.2.1/finx_io-0.2.1.tar.gz

---
+++ packages/finx-io/finx_io-0.2.1.tar.gz/finx_io-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(install_requires=[
+    "aiohttp==3.8.1",
+    "setuptools>=61.0",
+    "lru-dict==1.1.7",
+    "nest-asyncio==1.5.5",
+    "numpy==1.22.3",
+    "pandas==1.4.2",
+    "plotly==5.8.0",
+    "requests==2.27.1",
+    "scipy==1.8.0",
+    "websocket-client>=0.58.0",
+    "websockets==10.3",
+])
