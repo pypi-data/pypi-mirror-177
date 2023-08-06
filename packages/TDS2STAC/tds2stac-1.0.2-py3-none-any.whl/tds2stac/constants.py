b85dfa9cb776121de3abca60ca1b2afb6d5a62e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:03 +0000

    Add package TDS2STAC/1.0.2/tds2stac-1.0.2-py3-none-any.whl

---
+++ packages/TDS2STAC/tds2stac-1.0.2-py3-none-any.whl/tds2stac/constants.py
@@ -0,0 +1,5 @@
+unidata = "http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0"
+w3 = "http://www.w3.org/1999/xlink"
+iso = "http://www.isotc211.org/2005/gmd"
+global_bounding_box = [-360,-90,0,90]
+No_inf = "No information"
