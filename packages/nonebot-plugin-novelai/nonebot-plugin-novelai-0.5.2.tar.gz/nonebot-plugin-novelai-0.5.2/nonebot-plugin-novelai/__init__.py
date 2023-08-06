78c404c197e071896c16b3e83f068e006b3e15fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:40 +0000

    Add package nonebot-plugin-novelai/0.5.2/nonebot-plugin-novelai-0.5.2.tar.gz

---
+++ packages/nonebot-plugin-novelai/nonebot-plugin-novelai-0.5.2.tar.gz/nonebot-plugin-novelai-0.5.2/nonebot-plugin-novelai/__init__.py
@@ -0,0 +1,8 @@
+from . import aidraw,config,manage
+from nonebot.plugin import PluginMetadata
+from .extension.deepdanbooru import deepdanbooru
+__plugin_meta__ = PluginMetadata(
+    name="AI绘图",
+    description="调用novelai进行二次元AI绘图",
+    usage=f"基础用法:\n.aidraw[指令] [空格] loli,[参数]\n示例:.aidraw loli,cute,kawaii,\n项目地址:https://github.com/Mutsukibot/tree/nonebot-plugin-novelai\n说明书：https://sena-nana.github.io/MutsukiDocs/",
+)
