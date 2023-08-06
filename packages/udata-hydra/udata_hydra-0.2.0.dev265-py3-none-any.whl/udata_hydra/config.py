fce69c478b2eb9d8c10f2cee93663febc1eb4d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:58 +0000

    Add package udata-hydra/0.2.0.dev265/udata_hydra-0.2.0.dev265-py3-none-any.whl

---
+++ packages/udata-hydra/udata_hydra-0.2.0.dev265-py3-none-any.whl/udata_hydra/config.py
@@ -0,0 +1,32 @@
+# -- crawler settings -- #
+
+# sql LIKE syntax
+EXCLUDED_PATTERNS = [
+    'http%geo.data.gouv.fr%',
+    # opendatasoft shp
+    '%?format=shp%',
+]
+# no backoff for those domains
+NO_BACKOFF_DOMAINS = [
+    'static.data.gouv.fr',
+    'www.data.gouv.fr',
+    # dead domain, no need to backoff
+    'inspire.data.gouv.fr',
+]
+# max number of _completed_ requests per domain per period
+BACKOFF_NB_REQ = 180
+BACKOFF_PERIOD = 360  # in seconds
+# crawl batch size, beware of open file limits
+BATCH_SIZE = 100
+# crawl url if last check is older than
+SINCE = '1w'
+# seconds to wait for between batches
+SLEEP_BETWEEN_BATCHES = 60
+
+# -- Webhook integration config -- #
+ENABLE_WEBHOOK = True
+UDATA_URI = 'http://dev.local:7000/api/2'
+UDATA_URI_API_KEY = ''
+
+# Save resource to minio or not
+SAVE_TO_MINIO = False
