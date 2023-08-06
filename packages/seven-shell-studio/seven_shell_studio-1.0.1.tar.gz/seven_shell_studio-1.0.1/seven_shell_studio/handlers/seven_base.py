32d327b2b251c66b233d5bea17b4cc623ba23c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package seven-shell-studio/1.0.1/seven_shell_studio-1.0.1.tar.gz

---
+++ packages/seven-shell-studio/seven_shell_studio-1.0.1.tar.gz/seven_shell_studio-1.0.1/seven_shell_studio/handlers/seven_base.py
@@ -0,0 +1,11 @@
+
+# -*- coding: utf-8 -*-
+
+from seven_framework.web_tornado.base_handler.base_api_handler import *
+
+
+class SevenBaseHandler(BaseHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
