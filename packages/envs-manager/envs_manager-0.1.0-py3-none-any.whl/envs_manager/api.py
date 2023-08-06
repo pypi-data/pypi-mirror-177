4ee413c3b64771527814169ea4ca0bc8d0550984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:52 +0000

    Add package envs-manager/0.1.0/envs_manager-0.1.0-py3-none-any.whl

---
+++ packages/envs-manager/envs_manager-0.1.0-py3-none-any.whl/envs_manager/api.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2022-present Spyder Development Team and envs-manager contributors
+#
+# SPDX-License-Identifier: MIT
+
+
+class EnvManagerInstance:
+    ID = ""
+
+    def __init__(self, environment_path, external_executable=None):
+        self.environment_path = environment_path
+        self.external_executable = external_executable
+        self.executable_variant = None
+        assert self.validate(), f"{self.ID} backend unavailable!"
+
+    def validate(self):
+        pass
+
+    def create_environment(self, packages=None, channels=None):
+        raise NotImplementedError()
+
+    def delete_environment(self):
+        raise NotImplementedError()
+
+    def activate_environment(self):
+        raise NotImplementedError()
+
+    def deactivate_environment(self):
+        raise NotImplementedError()
+
+    def export_environment(self, export_file_path=None):
+        raise NotImplementedError()
+
+    def import_environment(self, import_file_path):
+        raise NotImplementedError()
+
+    def install_packages(
+        self,
+        packages,
+        channels=None,
+        force=False,
+        capture_output=False,
+    ):
+        raise NotImplementedError()
+
+    def uninstall_packages(self, packages, force=False, capture_output=False):
+        raise NotImplementedError()
+
+    def update_packages(self, packages, force=False, capture_output=False):
+        raise NotImplementedError()
+
+    def list_packages(self):
+        raise NotImplementedError()
