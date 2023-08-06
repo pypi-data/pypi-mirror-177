d79bf90f784af3e8af63a02a42a546726e5318b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:25 +0000

    Add package dbpystream/0.6/dbpystream-0.6.tar.gz

---
+++ packages/dbpystream/dbpystream-0.6.tar.gz/dbpystream-0.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+ 
+from setuptools import setup, find_packages
+def get_install_requires():
+    reqs = [
+            'pandas>=0.18.0',
+            'requests>=2.0.0',
+			'pickle',
+			'toml' ,
+			'uuid',
+			'json',
+			'itertools',
+			'random',
+			'threading',
+			'pyzstd >=0.15',
+			'numpy>=1.9.2'
+            ]
+    return reqs
+setup(
+	name = "dbpystream",
+	version = "0.6.1",
+    author ="songroom",
+    long_description_content_type="text/markdown",
+	url = 'https://github.com/songroom2016/dbpystream.git',
+	long_description = open('README.md',encoding="utf-8").read(),
+    python_requires=">=3.6",
+    install_requires=get_install_requires(),
+	packages = find_packages(),
+ 
+)
