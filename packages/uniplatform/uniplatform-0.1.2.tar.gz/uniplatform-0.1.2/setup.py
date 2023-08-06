efeef967ce516bbc57ee800322d62654b4acefe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:38 +0000

    Add package uniplatform/0.1.2/uniplatform-0.1.2.tar.gz

---
+++ packages/uniplatform/uniplatform-0.1.2.tar.gz/uniplatform-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='uniplatform',
+    version=open('VERSION').read(),
+    description='Make platform info in python more universal.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='HivertMoZara',
+    author_email='worldmozara@gmail.com',
+    url='https://github.com/NCBM/uniplatform',
+    scripts=['uniplatform.py'],
+    py_modules=["uniplatform"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    license='MIT',
+)
