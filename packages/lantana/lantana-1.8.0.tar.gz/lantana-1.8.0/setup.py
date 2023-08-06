05a05aa5d635aeb18e3671ab3e5bb2fb0fd4b3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:15 +0000

    Add package lantana/1.8.0/lantana-1.8.0.tar.gz

---
+++ packages/lantana/lantana-1.8.0.tar.gz/lantana-1.8.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.8.0'
+
+setup(
+    name="lantana",
+    version=VERSION,
+    url='http://wsoft-project.github.io/lantana/',
+    license='MIT',
+    description='Bootstrap theme for MkDocs',
+    author='WSOFT',
+    author_email='info@wsoft.ws',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['mkdocs>=1.1','mkdocs-material>=7.0','mkdocs-awesome-pages-plugin>=2.3','mkdocs-macros-plugin>=0.6.3','mkdocs-git-authors-plugin>=0.6.2'],
+    python_requires='>=3.5',
+    entry_points={
+        'mkdocs.themes': [
+            'lantana = lantana',
+        ]
+    },
+    zip_safe=False
+)
