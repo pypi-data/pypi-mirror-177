4a064906b1a25e8e9e7ccb54638dd761e9f254ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:42 +0000

    Add package viktor/12.3.0/viktor-12.3.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/viktor/viktor-12.3.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/viktor/external/grlweap.pyi
@@ -0,0 +1,9 @@
+from ..errors import ExecutionError as ExecutionError
+from .external_program import ExternalProgram as ExternalProgram
+from io import BytesIO
+from typing import Any, Optional
+
+class GRLWeapAnalysis(ExternalProgram):
+    input_file: Any = ...
+    def __init__(self, input_file: BytesIO) -> None: ...
+    def get_output_file(self) -> Optional[BytesIO]: ...
