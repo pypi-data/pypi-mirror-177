feca55fc5508c62c829ba0f0731a441a7cbf331b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:27 +0000

    Add package b4udata/0.0/b4udata-0.0.tar.gz

---
+++ packages/b4udata/b4udata-0.0.tar.gz/b4udata-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='b4udata',
+      version='0.0',
+      description='Retrieve and edit data',
+      packages=['b4udata'],
+      author_email='leonard@aps.holdings',
+      zip_safe=False)
