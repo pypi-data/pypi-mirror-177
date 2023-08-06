688eb3460149362aaea372807a9a8f18489a6c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:58 +0000

    Add package aiida-gaussian/2.0.0/aiida-gaussian-2.0.0.tar.gz

---
+++ packages/aiida-gaussian/aiida-gaussian-2.0.0.tar.gz/aiida-gaussian-2.0.0/aiida_gaussian/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+aiida_gaussian
+
+AiiDA plugin for the Gaussian quantum chemistry software
+"""
+
+from .version import __version__
+
+__all__ = [
+    "__version__",
+]
