398ef293e3e08367312dfa92aa987b6644b1026c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:06 +0000

    Add package types-slumber/0.7.3.1/types-slumber-0.7.3.1.tar.gz

---
+++ packages/types-slumber/types-slumber-0.7.3.1.tar.gz/types-slumber-0.7.3.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-slumber"
+description = "Typing stubs for slumber"
+long_description = '''
+## Typing stubs for slumber
+
+This is a PEP 561 type stub package for the `slumber` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `slumber`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/slumber. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `4588dd6c74f3e67b38421f806db7c84a3b27c7a8`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.7.3.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/slumber.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-requests'],
+      packages=['slumber-stubs'],
+      package_data={'slumber-stubs': ['__init__.pyi', 'exceptions.pyi', 'serialize.pyi', 'utils.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
