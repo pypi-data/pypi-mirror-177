2787d4b9d5d0a6d61730a98028ec828c22d23681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:22 +0000

    Add package jackattack/0.0.1/jackattack-0.0.1-py3-none-any.whl

---
+++ packages/jackattack/jackattack-0.0.1-py3-none-any.whl/jackattack.py
@@ -0,0 +1,32 @@
+
+
+
+
+class JackAttack(object):
+
+    """An attack class for Jack"""
+
+    def __init__(self):
+        """Instantiate a Jack"""
+        
+
+
+    def attack(self):
+        """
+        Initiate a Jack Attack
+        """
+        for i in range(3):
+            print("Jack attacks you.")
+
+        print("You die.")
+
+
+
+    def run(self):
+        """
+        Initiate Jack 'Run Mode'
+        """
+        print("You scare Jack.")
+        print("He runs away.")
+
+
