afc2c188be02c995827c45516f2fe130110faf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:21 +0000

    Add package eth-utils/2.1.0/eth-utils-2.1.0.tar.gz

---
+++ packages/eth-utils/eth-utils-2.1.0.tar.gz/eth-utils-2.1.0/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+extras_require = {
+    "test": [
+        "hypothesis>=4.43.0,<5.0.0",
+        "pytest>=6.2.5,<7",
+        "pytest-xdist",
+        "tox==3.14.6",
+        "types-setuptools",
+    ],
+    "lint": [
+        "black>=22",
+        "flake8==3.7.9",
+        "isort>=4.2.15,<5",
+        "mypy==0.910",
+        "pydocstyle>=5.0.0,<6",
+        "pytest>=6.2.5,<7",
+        "types-setuptools",
+    ],
+    "doc": [
+        "Sphinx>=1.6.5,<2",
+        "sphinx_rtd_theme>=0.1.9,<2",
+        "towncrier>=21,<22",
+        "jinja2>=3.0.0,<3.0.1",
+    ],
+    "dev": [
+        "bumpversion>=0.5.3,<1",
+        "pytest-watch>=4.1.0,<5",
+        "wheel>=0.30.0,<1.0.0",
+        "twine>=1.13,<2",
+        "ipython",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"]
+    + extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="eth-utils",
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version="2.1.0",
+    description=(
+        """eth-utils: Common utility functions for python code that interacts with Ethereum"""
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/eth-utils",
+    include_package_data=True,
+    install_requires=[
+        "cached-property>=1.5.2,<2;python_version<'3.8'",
+        "eth-hash>=0.3.1",
+        "eth-typing>=3.0.0",
+        "toolz>0.8.2;implementation_name=='pypy'",
+        "cytoolz>=0.10.1;implementation_name=='cpython'",
+    ],
+    python_requires=">=3.7,<4",
+    extras_require=extras_require,
+    py_modules=["eth_utils"],
+    license="MIT",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"eth_utils": ["py.typed"]},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
