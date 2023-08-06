2796bcf70e4538c9a1f3037029ae1f30af1080b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:52:01 +0000

    Add package pytups/0.86.2/pytups-0.86.2.tar.gz

---
+++ packages/pytups/pytups-0.86.2.tar.gz/pytups-0.86.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+packages = ["pytups"]
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+required = []
+
+kwargs = {
+    "name": "pytups",
+    "version": "0.86.2",
+    "packages": packages,
+    "description": "data wrangling for lists of tuples and dictionaries",
+    "long_description": long_description,
+    "long_description_content_type": "text/x-rst",
+    "author": "Franco Peschiera",
+    "maintainer": "Franco Peschiera",
+    "author_email": "pchtsp@gmail.com",
+    "maintainer_email": "pchtsp@gmail.com",
+    "install_requires": required,
+    "url": "https://github.com/pchtsp/pytups",
+    "download_url": "https://github.com/pchtsp/pytups/archive/master.zip",
+    "keywords": "super dict dictionary tuple list math pulp",
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+}
+
+setup(**kwargs)
