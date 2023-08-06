7ee2b40a132f98e33fd01f7593cb01eba0fa7136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:23 +0000

    Add package lino-cosi/22.11.0/lino-cosi-22.11.0.tar.gz

---
+++ packages/lino-cosi/lino-cosi-22.11.0.tar.gz/lino-cosi-22.11.0/lino_cosi/lib/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-wele
+# Copyright 2013-2019 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+"""
+
+Plugins
+=======
+
+.. autosummary::
+   :toctree:
+
+    cosi
+    contacts
+    products
+
+Not used
+========
+
+.. autosummary::
+   :toctree:
+
+    orders
+
+"""
+
---
+++ packages/lino-cosi/lino-cosi-22.11.0.tar.gz/lino-cosi-22.11.0/lino_cosi/lib/users/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2022 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+from lino.modlib.users import Plugin, _
+
+
+class Plugin(Plugin):
+    pass
