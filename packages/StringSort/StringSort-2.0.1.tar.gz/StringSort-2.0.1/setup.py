4353c8263981c38ae0de33f0e03900db36ec8ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:42 +0000

    Add package StringSort/2.0.1/StringSort-2.0.1.tar.gz

---
+++ packages/StringSort/StringSort-2.0.1.tar.gz/StringSort-2.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='StringSort',
+    packages=['StringSort'],
+    version='2.0.1',
+    license='MIT',
+    description='string sorting library',
+    author='Luka',
+    author_email='app6onpython@gmail.com',
+    keywords=['sort', 'string', 'StringSort'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
