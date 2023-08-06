b87dffd196f04e40c560f923e1344fb091d4a960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:44 +0000

    Add package lzr-near-api-py/0.1.8/lzr-near-api-py-0.1.8.tar.gz

---
+++ packages/lzr-near-api-py/lzr-near-api-py-0.1.8.tar.gz/lzr-near-api-py-0.1.8/tests/config.py
@@ -0,0 +1,16 @@
+# TODO: add here environment configuration for local testing.
+
+# mainnet
+# https://rpc.mainnet.near.org
+# testnet
+# https://rpc.testnet.near.org
+# betanet (may be unstable)
+# https://rpc.betanet.near.org
+# localnet
+# http://localhost: 3030
+
+# NODE_URL = "https://rpc.testnet.near.org"
+
+NODE_URL = "https://rpc.ci-testnet.near.org"
+TEST_ACCOUNT = "test.near"
+TEST_KEY_PAIR = "ed25519:2wyRcSwSuHtRVmkMCGjPwnzZmQLeXLzLLyED1NDMt4BjnKgQL6tF85yBx6Jr26D2dUNeC716RBoTxntVHsegogYw"
