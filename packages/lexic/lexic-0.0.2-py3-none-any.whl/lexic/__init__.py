34776eec283094af2828c1ea05fc94db69d66a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:29 +0000

    Add package lexic/0.0.2/lexic-0.0.2-py3-none-any.whl

---
+++ packages/lexic/lexic-0.0.2-py3-none-any.whl/lexic/__init__.py
@@ -0,0 +1,7 @@
+from lexic import ll1_parsing
+from lexic import reg_exp_1
+from lexic import reg_exp_2
+from lexic import reg_exp_3
+from lexic import single_multi_comment
+from lexic import count_words
+from lexic import first_follow_ll1
