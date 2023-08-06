4673eefb67bbb67e8a63dbd7a7be685b8ea5de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:02 +0000

    Add package farmworld/0.0.1/farmworld-0.0.1.tar.gz

---
+++ packages/farmworld/farmworld-0.0.1.tar.gz/farmworld-0.0.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['farmworld', 'farmworld.env', 'farmworld.geojson']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=22.10.0,<23.0.0',
+ 'gym==0.21.0',
+ 'polygenerator>=0.2.0,<0.3.0',
+ 'pygame>=2.1.2,<3.0.0',
+ 'stable-baselines3[extra]>=1.6.2,<2.0.0',
+ 'torch>=1.13.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'farmworld',
+    'version': '0.0.1',
+    'description': 'Reinforcement Learning for Agriculture',
+    'long_description': '# FarmWorld\n\nA reinforcement learning library for agriculture.',
+    'author': 'Tom Grek',
+    'author_email': 'tom.grek@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tomgrek/farmworld',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
