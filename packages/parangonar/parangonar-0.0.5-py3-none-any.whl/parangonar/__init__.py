d94edc106e42d07506f1efd49ffa08b2809fecf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:43 +0000

    Add package parangonar/0.0.5/parangonar-0.0.5-py3-none-any.whl

---
+++ packages/parangonar/parangonar-0.0.5-py3-none-any.whl/parangonar/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+The top level of the package contains functions to
+note-align symbolic music data.
+"""
+import pkg_resources
+EXAMPLE = pkg_resources.resource_filename("tests", "data/mozart_k265_var1.match")
+
+
+from .match import AnchorPointNoteMatcher, AutomaticNoteMatcher
+from .evaluate import fscore_alignments
+
+__all__ = [
+    "AnchorPointNoteMatcher",
+    "AutomaticNoteMatcher",
+    "fscore_alignments",
+]
