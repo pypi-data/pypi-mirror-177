58b711e11803ca9014b932cbaed6f49adab4946e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package ymmetric-secret-share/0.0.8/ymmetric_secret_share-0.0.8-py3-none-any.whl

---
+++ packages/ymmetric-secret-share/ymmetric_secret_share-0.0.8-py3-none-any.whl/sss_cli/helper.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+from dataclasses import dataclass
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class NoKeychainException(Exception):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+@dataclass(frozen=True)
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class File_Map:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Config_Manager:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
