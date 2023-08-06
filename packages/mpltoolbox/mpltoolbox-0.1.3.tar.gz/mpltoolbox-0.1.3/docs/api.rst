eefb146c700fa28bc791f7d9e13eef446f309434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:52:30 +0000

    Add package mpltoolbox/0.1.3/mpltoolbox-0.1.3.tar.gz

---
+++ packages/mpltoolbox/mpltoolbox-0.1.3.tar.gz/mpltoolbox-0.1.3/docs/api.rst
@@ -0,0 +1,14 @@
+.. currentmodule:: mpltoolbox
+
+*************
+API Reference
+*************
+
+.. autosummary::
+   :toctree: generated
+
+   Ellipses
+   Lines
+   Points
+   Polygons
+   Rectangles
---
+++ packages/mpltoolbox/mpltoolbox-0.1.3.tar.gz/mpltoolbox-0.1.3/src/mpltoolbox/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Mpltoolbox contributors (https://github.com/mpltoolbox)
+
+# flake8: noqa
+
+from .lines import Lines
+from .rectangles import Rectangles
+from .ellipses import Ellipses
+from .points import Points
+from .polygons import Polygons
