f51add284affe55cc29f1f3fff2b2cf1e1dfd45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:47 +0000

    Add package riemann-sum/0.1.0/riemann-sum-0.1.0.tar.gz

---
+++ packages/riemann-sum/riemann-sum-0.1.0.tar.gz/riemann-sum-0.1.0/riemann/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+from ._summation import LOWER, MIDPOINT, UPPER
+from ._summation import Dimension
+from ._summation import summation
+
+# Type aliases
+Dim = Dimension
+
+# Function aliases
+rsum = summation
+
+# Global variable aliases
+L, M, U = LOWER, MIDPOINT, UPPER
