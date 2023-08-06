4c482d18373b775a06d144c21e8dff882276c315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:25 +0000

    Add package mlbugdetection/0.0.8/mlbugdetection-0.0.8.tar.gz

---
+++ packages/mlbugdetection/mlbugdetection-0.0.8.tar.gz/mlbugdetection-0.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.8'
+DESCRIPTION = 'A package for identify Machine Learning bugs'
+
+# Setting up
+setup(
+    name="mlbugdetection",
+    version=VERSION,
+    author="João Gianfaldoni | Giovanni Cardoso | William Silva",
+    author_email="william.silva.ismart@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy", "matplotlib", "sklearn"],
+    keywords=['python'],
+    include_package_data=True,
+    package_data={'': ['data/*.csv', 'models/*.pkl']},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
