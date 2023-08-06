11b69ea9278dc14d54e82b414a618617688a34f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:57 +0000

    Add package pybpa/0.2.0/pybpa-0.2.0.tar.gz

---
+++ packages/pybpa/pybpa-0.2.0.tar.gz/pybpa-0.2.0/src/pybpa/lsd/nrtsc_lsd.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+# Name:         transient_stability: nrtsc_lsd
+# Author:       MilkyDesk
+# Date:         2021/7/23 11:13
+# Description:
+#   
+"""
+
+# ------------------------- params --------------------------
+
+
+# ------------------------- params --------------------------
