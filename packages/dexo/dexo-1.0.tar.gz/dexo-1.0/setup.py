524e10c0327e8b6a286b8ae7a049949df34b66d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:40 +0000

    Add package dexo/1.0/dexo-1.0.tar.gz

---
+++ packages/dexo/dexo-1.0.tar.gz/dexo-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dexo',
+    version='1.0',
+    author='Kavin Bharathi',
+    author_email='r.m.kavinbharathi@gmail.com',
+    description='A cli for a quick and easy web dev kit',
+    url = 'https://github.com/kavinbharathii/webdev',
+    packages = find_packages(),
+    install_requires=[
+        'setuptools',
+    ],
+    python_requires='>=3.5',
+    entry_points = '''
+        [console_scripts]
+        dexo=app:run
+    '''
+)
+
