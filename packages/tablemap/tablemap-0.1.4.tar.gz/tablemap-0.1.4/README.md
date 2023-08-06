b6fa84eeee3f7dbeb2c4d61a524ee297322cf1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:34 +0000

    Add package tablemap/0.1.4/tablemap-0.1.4.tar.gz

---
+++ packages/tablemap/tablemap-0.1.4.tar.gz/tablemap-0.1.4/README.md
@@ -0,0 +1,8 @@
+# tablemap
+
+# Install
+pip install tablemap
+
+# 
+[Documentation]
+(https://tablemap.readthedocs.io/en/latest/index.html)
---
+++ packages/tablemap/tablemap-0.1.4.tar.gz/tablemap-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tablemap',
+    version='0.1.4',
+    description='sql not knowing sql',
+    long_description=open('README.md').read(),
+
+    url='https://github.com/nalssee/tablemap.git',
+    author='nalssee',
+    author_email='jinisrolling@gmail.com',
+
+    license='MIT',
+      
+    )
