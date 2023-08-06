32a2e47de80d1ae0ad6f84ba81ceb097cd256e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:04 +0000

    Add package sbx-rl/0.3.0/sbx-rl-0.3.0.tar.gz

---
+++ packages/sbx-rl/sbx-rl-0.3.0.tar.gz/sbx-rl-0.3.0/sbx/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+from sbx.droq import DroQ
+from sbx.ppo import PPO
+from sbx.sac import SAC
+from sbx.tqc import TQC
+
+# Read version from file
+version_file = os.path.join(os.path.dirname(__file__), "version.txt")
+with open(version_file) as file_handler:
+    __version__ = file_handler.read().strip()
