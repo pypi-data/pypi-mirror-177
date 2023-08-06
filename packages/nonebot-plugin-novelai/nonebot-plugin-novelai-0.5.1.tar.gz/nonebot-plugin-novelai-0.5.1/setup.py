13011deeaf6293d3d7b12f259c998fb644c76d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:59 +0000

    Add package nonebot-plugin-novelai/0.5.1/nonebot-plugin-novelai-0.5.1.tar.gz

---
+++ packages/nonebot-plugin-novelai/nonebot-plugin-novelai-0.5.1.tar.gz/nonebot-plugin-novelai-0.5.1/setup.py
@@ -0,0 +1,40 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import codecs
+
+with codecs.open('README.md', encoding="utf-8") as fp:
+    long_description = fp.read()
+INSTALL_REQUIRES = [
+    'aiohttp>=3.8.3',
+    'nonebot-adapter-onebot>=2.1.3',
+    'nonebot2>=2.0.0b4',
+    'aiofiles>=22.1.0',
+    'pillow>=9.2.0',
+]
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-novelai',
+    'version': '0.5.1',
+    'description': '基于nonebot2的novelai绘图插件',
+    'long_description': long_description,
+    'license': 'MIT',
+    'author': '',
+    'author_email': 'sena-nana <851183156@qq.com>',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'packages': [
+        'nonebot-plugin-novelai',
+        'nonebot-plugin-novelai.utils',
+    ],
+    'package_data': {'': ['*']},
+    'long_description_content_type': 'text/markdown',
+    'install_requires': INSTALL_REQUIRES,
+    'python_requires': '>=3.8',
+
+}
+
+
+setup(**setup_kwargs)
