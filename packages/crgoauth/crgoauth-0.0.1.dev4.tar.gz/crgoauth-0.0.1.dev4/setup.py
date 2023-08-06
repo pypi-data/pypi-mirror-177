71d4cb807738d955f2016a83af96cb098a227eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:23 +0000

    Add package crgoauth/0.0.1.dev4/crgoauth-0.0.1.dev4.tar.gz

---
+++ packages/crgoauth/crgoauth-0.0.1.dev4.tar.gz/crgoauth-0.0.1.dev4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+from crgoauth import __version__
+
+setup (
+    name='crgoauth',
+    version=__version__,
+    description='Cloudreach implementation of Google SignIn OAuth',
+
+    url='https://github.com/nikolay-tonev/crgoauth',
+    author='Cloudreach',
+    author_email='nikolay.tonev@cloudreach.com',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'flask',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: MacOS X',
+    ],
+)
