68d1d897ee4a04d7a2023345bb4115d2924acb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package simple-calculator-pws/0.0.1/simple_calculator_pws-0.0.1-py3-none-any.whl

---
+++ packages/simple-calculator-pws/simple_calculator_pws-0.0.1-py3-none-any.whl/simple_calculator_pws/operations.py
@@ -0,0 +1,17 @@
+# Submodulo de operacoes aritmeticas 
+
+def soma (valor1, valor2):
+    resultado = valor1 + valor2
+    return f'A soma de {valor1:.2f} + {valor2:.2f} é : {resultado}'
+
+def subtracao(valor1, valor2):
+    resultado = valor1 - valor2
+    return f'A subtração de {valor1:.2f} + {valor2:.2f} é : {resultado}'
+
+def multiplicacao(valor1, valor2):
+    resultado = valor1 + valor2
+    return f'A Multiplicação de {valor1:.2f} * {valor2:.2f} é : {resultado}'
+
+def divisao(valor1, valor2):
+    resultado = valor1 + valor2
+    return f'A divisão de {valor1:.2f} / {valor2:.2f} é : {resultado}'
