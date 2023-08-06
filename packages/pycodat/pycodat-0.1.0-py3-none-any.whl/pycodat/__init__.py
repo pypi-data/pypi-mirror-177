74594a4574e18009ddbb12cb0cbbe7d368a0c85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:05 +0000

    Add package pycodat/0.1.0/pycodat-0.1.0-py3-none-any.whl

---
+++ packages/pycodat/pycodat-0.1.0-py3-none-any.whl/pycodat/__init__.py
@@ -0,0 +1,13 @@
+
+
+
+class Codat:
+
+    def __init__(self):
+        # self.api_key = api_key
+        # self.company_id = company_id
+        # self.base_url = base_url
+        self.hello = "hello"
+
+    def test(self):
+        print(self.hello)
