daa06b09b6b017fc93c78ce0bc1d0c538256fcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:30 +0000

    Add package bread-mail/1.7/bread_mail-1.7.tar.gz

---
+++ packages/bread-mail/bread_mail-1.7.tar.gz/bread_mail-1.7/bread_mail/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+self.txtSubject = PhotoImage(
+        #     file=relative_to_assets("entry_4.png"))
+        # entry_bg_4 = canvas.create_image(
+        #     593.0,
+        #     131.0,
+        #     image=entry_image_4
+        # )
+        # entry_4 = Entry(
+        #     bd=0,
+        #     bg="#FFFFFF",
+        #     highlightthickness=0
+        # )
+        # entry_4.place(
+        #     x=440.0,
+        #     y=116.0,
+        #     width=306.0,
+        #     height=28.0
+        # )
