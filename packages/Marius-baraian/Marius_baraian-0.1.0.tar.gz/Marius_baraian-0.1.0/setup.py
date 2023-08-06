5c2d9416a86e2d7c20519461e96a119757fca5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:22 +0000

    Add package Marius-baraian/0.1.0/Marius_baraian-0.1.0.tar.gz

---
+++ packages/Marius-baraian/Marius_baraian-0.1.0.tar.gz/Marius_baraian-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name= "Marius_baraian",
+    version="0.1.0",
+    author= "Baraian Marius",
+    author_email= "marius.baraian08@gmail.com",
+    packages= ["my_own_package"],
+    package_dir= {"":"src\\"},
+    include_package_data= True,
+    description= "This is my first package",
+)
