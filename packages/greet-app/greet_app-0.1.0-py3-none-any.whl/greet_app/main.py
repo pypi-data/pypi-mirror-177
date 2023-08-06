ce0a2c387205756bb686b58c3ce805766bf63a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:21 +0000

    Add package greet-app/0.1.0/greet_app-0.1.0-py3-none-any.whl

---
+++ packages/greet-app/greet_app-0.1.0-py3-none-any.whl/greet_app/main.py
@@ -0,0 +1,27 @@
+from importlib import metadata
+
+import typer
+
+app = typer.Typer()
+__version__ = metadata.version(__package__)
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        print(__version__)
+        raise typer.Exit()
+
+
+@app.callback()
+def callback(
+    _: bool = typer.Option(None, "--version", "-v", callback=version_callback)
+) -> None:
+    """My app description"""
+
+
+@app.command()
+def hello(
+    name: str = typer.Argument(default="World", help="The person to greet.")
+) -> None:
+    """Say hello to NAME"""
+    typer.echo(f"Hello {name}!")
