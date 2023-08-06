7663bec5d1aa2ed789e1bb75c7ddf48d1e87998f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package patent-client/3.0.5/patent_client-3.0.5.tar.gz

---
+++ packages/patent-client/patent_client-3.0.5.tar.gz/patent_client-3.0.5/src/patent_client/uspto/__init__.py
@@ -0,0 +1,17 @@
+from .assignment import Assignment
+from .fulltext import Patent
+from .fulltext import PublishedApplication
+from .peds import USApplication
+from .ptab import PtabDecision
+from .ptab import PtabDocument
+from .ptab import PtabProceeding
+
+__all__ = [
+    "Assignment",
+    "Patent",
+    "PublishedApplication",
+    "USApplication",
+    "PtabDecision",
+    "PtabDocument",
+    "PtabProceeding",
+]
---
+++ packages/patent-client/patent_client-3.0.5.tar.gz/patent_client-3.0.5/src/patent_client/uspto/fulltext/__init__.py
@@ -0,0 +1,9 @@
+from .patent.model import Patent
+from .published_application.model import PublishedApplication
+
+__api_name__ = "USPTO Fulltext Databases"
+
+__all__ = [
+    "Patent",
+    "PublishedApplication",
+]
