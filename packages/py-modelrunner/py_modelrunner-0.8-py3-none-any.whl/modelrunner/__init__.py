b57bd7643cfe0f5b35d00d0048dd81b92d50b4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:15 +0000

    Add package py-modelrunner/0.8/py_modelrunner-0.8-py3-none-any.whl

---
+++ packages/py-modelrunner/py_modelrunner-0.8-py3-none-any.whl/modelrunner/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+"""
+
+from .model import (
+    ModelBase,
+    make_model,
+    make_model_class,
+    run_function_with_cmd_args,
+    run_script,
+    set_default,
+)
+from .parameters import Parameter
+from .results import Result, ResultCollection
+from .run import submit_job, submit_jobs
+from .version import __version__
