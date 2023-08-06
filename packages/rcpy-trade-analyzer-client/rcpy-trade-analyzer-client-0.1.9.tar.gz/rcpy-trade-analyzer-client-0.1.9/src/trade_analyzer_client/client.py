c8e8a37efb6f502198f7ae320eff9cfe028e01e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:53 +0000

    Add package rcpy-trade-analyzer-client/0.1.9/rcpy-trade-analyzer-client-0.1.9.tar.gz

---
+++ packages/rcpy-trade-analyzer-client/rcpy-trade-analyzer-client-0.1.9.tar.gz/rcpy-trade-analyzer-client-0.1.9/src/trade_analyzer_client/client.py
@@ -0,0 +1,63 @@
+from requests import Response, get, post, delete, put
+
+from .models import *
+
+
+class TradeAnalyzerClient:
+
+    BASE_URL: str = 'http://127.0.0.1:8000'
+
+    __access_token: str = ''
+
+    @classmethod
+    def create_strategy(cls, id: str, description: str, type: StrategyType, commission_model: CommissionModel, platform: Platform, currency: str):
+        endpoint: str = '/api/v1/strategy'
+
+        json_data = {
+            'id': id,
+            'description': description,
+            'type': type.value,
+            'commission_model': commission_model.name,
+            'commission_pricing': commission_model.pricing,
+            'platform': platform.value,
+            'currency': currency
+        }
+        
+        response: Response = post(cls.BASE_URL + endpoint, json = json_data, headers = {
+            'Content-Type': 'application/json'
+        })
+        return response
+
+    @classmethod
+    def get_strategies(cls):
+        endpoint: str = '/api/v1/strategy'
+        response: Response = get(cls.BASE_URL + endpoint, headers = {
+            'Content-Type': 'application/json'
+        }) 
+        return response
+
+    @classmethod
+    def get_strategy(cls, strategy_id: str):
+        endpoint: str = '/api/v1/strategy'
+        json_data = {
+            'id': strategy_id
+        }
+
+        response: Response = get(cls.BASE_URL + endpoint, json = json_data, headers = {
+            'Content-Type': 'application/json'
+        }) 
+        return response
+
+    @classmethod
+    def delete_strategy(cls, strategy_id: str):
+        endpoint: str = '/api/v1/strategy'
+        json_data = {
+            'id': strategy_id
+        }
+
+        response: Response = delete(cls.BASE_URL + endpoint, json = json_data, headers = {
+            'Content-Type': 'application/json'
+        }) 
+        return response
+
+    
---
+++ packages/rcpy-trade-analyzer-client/rcpy-trade-analyzer-client-0.1.9.tar.gz/rcpy-trade-analyzer-client-0.1.9/src/trade_analyzer_client/models/__init__.py
@@ -0,0 +1,5 @@
+from .strategy_type import StrategyType
+from .platform import Platform
+from .commission_rule import CommissionRule
+from .commission_model import CommissionModel
+from .strategy import Strategy
