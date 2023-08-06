5f14839e0d1ca69354ff629f3b0fe7fa64d2dbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:17 +0000

    Add package github-dependents-info/0.7.0/github_dependents_info-0.7.0-py3-none-any.whl

---
+++ packages/github-dependents-info/github_dependents_info-0.7.0-py3-none-any.whl/github_dependents_info/__init__.py
@@ -0,0 +1,16 @@
+"""Collect information about dependencies between a github repo and other repositories. Results available in JSON, markdown and badges."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+from .gh_dependents_info import GithubDependentsInfo
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
