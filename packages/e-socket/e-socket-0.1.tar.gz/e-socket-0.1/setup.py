341bf18e613a9496ad44b76d278b34e5ce01868b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:58 +0000

    Add package e-socket/0.1/e-socket-0.1.tar.gz

---
+++ packages/e-socket/e-socket-0.1.tar.gz/e-socket-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='e-socket',
+    version='0.1',
+    packages=['e_socket'],
+    url='https://github.com/kucer0043/e_socket',
+    license='',
+    author='kucer0043',
+    author_email='kucer0043@gmail.com',
+    description='Easy Sockets'
+)
