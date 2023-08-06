8a6fba660b36919f9d46546fcef6613d68a46fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package modal-client/0.40.310/modal_client-0.40.310-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.40.310-py3-none-any.whl/client_test/webhook_test.py
@@ -0,0 +1,17 @@
+# Copyright Modal Labs 2022
+import pytest
+
+from modal.aio import AioStub
+
+stub = AioStub()
+
+
+@stub.webhook(method="POST")
+async def f(x):
+    return {"square": x**2}
+
+
+@pytest.mark.asyncio
+async def test_webhook(servicer, aio_client):
+    async with stub.run(client=aio_client):
+        assert f.web_url
