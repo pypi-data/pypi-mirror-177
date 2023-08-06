ed82fda06d44b26f0aa1c03feb4c96ad877b2296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:36 +0000

    Add package Bodea-Bogdan/0.1.0/Bodea_Bogdan-0.1.0.tar.gz

---
+++ packages/Bodea-Bogdan/Bodea_Bogdan-0.1.0.tar.gz/Bodea_Bogdan-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "Bodea_Bogdan",
+    version= "0.1.0",
+    author = "Bodea Bogdan",
+    autor_email = "bogdan.bodea2903@gmail.com",
+    packages = ["my_own_package"],
+    package_dir= {"":"src\\"},
+    include_package_data =True,
+    description = "This is my first package!" )
+
