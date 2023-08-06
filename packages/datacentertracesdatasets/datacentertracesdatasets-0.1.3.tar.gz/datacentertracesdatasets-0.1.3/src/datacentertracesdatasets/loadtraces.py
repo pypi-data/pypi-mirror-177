98cb26a80d796a9debb4123aaca7f24dfb51bcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:06 +0000

    Add package datacentertracesdatasets/0.1.3/datacentertracesdatasets-0.1.3.tar.gz

---
+++ packages/datacentertracesdatasets/datacentertracesdatasets-0.1.3.tar.gz/datacentertracesdatasets-0.1.3/src/datacentertracesdatasets/loadtraces.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from io import StringIO
+
+def get_alibaba_2018_trace(trace="machine_usage", day=None, stride_seconds=10, format='dataframe'):
+    try:
+        import importlib.resources as pkg_resources
+    except ImportError:
+        import importlib_resources as pkg_resources
+    from . import alibaba2018
+    assert trace in ["machine_usage"], 'only "machine_usage" trace is supported right now'
+    assert day is None or (day > 0 and day < 9), 'alibaba days range from 1 to 8'
+    assert stride_seconds in [10, 30, 300], 'only 10, 30 and 300 seconds are currently suported as stride_seconds'
+    assert format in ['dataframe', 'ndarray'], 'only "dataframe" and "ndarray" are supported right now'
+    if (day is None):
+        contents = pkg_resources.read_text(alibaba2018, f'{trace}_days_1_to_8_grouped_{stride_seconds}_seconds.csv')
+        res = pd.read_csv(StringIO(contents), index_col=False)
+    else:
+        contents = pkg_resources.read_text(alibaba2018, f'{trace}_day_{day}_grouped_{stride_seconds}_seconds.csv')
+        res = pd.read_csv(StringIO(contents), index_col=False)
+    if format == 'ndarray':
+        res = res.to_numpy()
+    return res
