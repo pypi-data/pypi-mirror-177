4c793de08567ecbcb180c43a60f11359696c770b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:48 +0000

    Add package ixel/0.1.0/ixel-0.1.0.tar.gz

---
+++ packages/ixel/ixel-0.1.0.tar.gz/ixel-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'ixel',
+    version = '0.1.0',
+    description = 'Yardımcı Modül',
+    author = 'ixelizm',
+    author_email = 'felekburak8@gmail.com',
+    keywords = ['TAG1', 'TAG2'],
+)
