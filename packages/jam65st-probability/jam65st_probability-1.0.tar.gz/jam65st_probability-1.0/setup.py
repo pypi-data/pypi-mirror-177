b433c525bd9300b600cbe94bf6315ed6199e52be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:28 +0000

    Add package jam65st-probability/1.0/jam65st_probability-1.0.tar.gz

---
+++ packages/jam65st-probability/jam65st_probability-1.0.tar.gz/jam65st_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jam65st_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      author='Jaime Mendez',
+      author_email='jam65st@gmail.com',
+      packages=['jam65st_probability'],
+      zip_safe=False)
