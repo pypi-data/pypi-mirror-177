8659d636f3079c4ad296bd16c1e7f61bd5bace21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:43 +0000

    Add package cron4py/1.0.3/cron4py-1.0.3.tar.gz

---
+++ packages/cron4py/cron4py-1.0.3.tar.gz/cron4py-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cron4py',
+      version='1.0.3',
+      description='Cron to list datetime',
+      packages=['cron4py'],
+      author_email='kivdev@yandex.ru',
+      zip_safe=False)
