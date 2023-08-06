45fca52a76a5abae54aea0ed66bfd69109d660a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:51:20 +0000

    Add package gdshoplib/0.5.1.3/gdshoplib-0.5.1.3-py3-none-any.whl

---
+++ packages/gdshoplib/gdshoplib-0.5.1.3-py3-none-any.whl/gdshoplib/core/cli.py
@@ -0,0 +1,40 @@
+import typer
+from rich import print
+
+from gdshoplib import Description, Media, NotionManager
+
+app = typer.Typer()
+
+
+@app.command()
+def update_descriptions(sku=None):
+    description = Description()
+    if not sku:
+        for product in description.notion_manager.get_products(as_generator=True):
+            description.update(product["sku"])
+            print(f"Обновлен продукт {product['sku']}")
+        return
+
+    description.update(sku)
+    print(f"Обновлен продукт {sku}")
+
+
+@app.command()
+def update_media(sku=None):
+    media = Media()
+    if sku:
+        media.product_update(sku)
+        print(f"Медия {sku} обновлены")
+
+    for product in NotionManager().get_products(format="model", as_generator=True):
+        media.save(product)
+        print(f"Медия {product.dict()['sku']} обновлены")
+
+
+@app.command()
+def update_sku():
+    NotionManager().set_sku()
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/gdshoplib/gdshoplib-0.5.1.3-py3-none-any.whl/gdshoplib/core/manager/basemanager.py
@@ -0,0 +1,97 @@
+import datetime
+import functools
+import logging
+
+import requests as r
+
+logger = logging.getLogger(__name__)
+
+
+class BaseManager:
+    CACHE = {}
+    DESCRIPTION_TEMPLATE = "basic.txt"
+    KEY = None
+
+    def __init__(self, cache=False, cache_period=20) -> None:
+        self.cache = cache
+        self.cache_period = cache_period
+        self.settings = self.SETTINGS()
+
+    @classmethod
+    def get_platform_keys(cls):
+        return [
+            manager.KEY.upper() if manager.KEY else None
+            for manager in cls.get_managers()
+        ]
+
+    @classmethod
+    def get_managers(cls):
+        return [manager for manager in cls.__subclasses__()]
+
+    @classmethod
+    def get_manager(cls, key):
+        for manager in cls.get_managers():
+            if (key is None and manager.KEY is key) or (
+                key and manager.KEY == key.upper()
+            ):
+                return manager
+        assert False, f"Менеджер по ключу {key} не найден"
+
+    def get_cache_key(self, path, **kwargs):
+        return f"{path}"
+
+    def check_cacheble(self, *args, **kwargs):
+        return kwargs.get("method").upper() == "GET"
+
+    def auth(self):
+        raise NotImplementedError
+
+    def pagination(self, url, *, params=None, **kwargs):
+        raise NotImplementedError
+
+    def pagination_next(self, response):
+        raise NotImplementedError
+
+    def cache_response(func):
+        @functools.wraps(func)
+        def wrap(self, *args, **kwargs):
+            if self.cache and self.check_cacheble(*args, **kwargs):
+                cache_key = self.get_cache_key(*args, **kwargs)
+                cached = self.CACHE.get(cache_key)
+
+                if cached and cached["time"] > datetime.datetime.now():
+                    cached["time"] += datetime.timedelta(seconds=self.cache_period)
+                    return cached["data"]
+
+                self.CACHE[cache_key] = {
+                    "data": func(self, *args, **kwargs),
+                    "time": datetime.datetime.now()
+                    + datetime.timedelta(seconds=self.cache_period),
+                }
+
+                return self.CACHE[cache_key]["data"]
+            return func(self, *args, **kwargs)
+
+        return wrap
+
+    @cache_response
+    def make_request(self, path, *, method, params=None):
+        _path = f"{self.BASE_URL}{path}"
+        _params = (
+            dict(params=params) or {}
+            if method.upper() == "GET"
+            else dict(json=params) or {}
+        )
+
+        _r = r.request(
+            method,
+            _path,
+            headers=self.get_headers(),
+            **_params,
+        )
+        if not _r.ok:
+            logger.warning(_r.json())
+            assert (
+                False
+            ), f"Запрос {method.upper()} {_path} прошел с ошибкой {_r.status_code}/n"
+        return _r
