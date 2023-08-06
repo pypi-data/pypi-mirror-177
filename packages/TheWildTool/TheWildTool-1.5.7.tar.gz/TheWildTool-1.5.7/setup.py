87ebe72f394188983362be673237246a9b8946a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:40 +0000

    Add package TheWildTool/1.5.7/TheWildTool-1.5.7.tar.gz

---
+++ packages/TheWildTool/TheWildTool-1.5.7.tar.gz/TheWildTool-1.5.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name = 'TheWildTool',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = ['TheWildTool'],
+    version = '1.5.7',
+    license = 'MIT',
+    description = 'TheWildTool is a tool developed with the main objective of saving time when working with audio datasets. Either to prepare them, to get them or to train a model with them. 🤖',
+    author = 'ElHaban3ro',
+    author_email = 'habanferd@gmail.com',
+    url = 'https://github.com/ElHaban3ro/ConvTool',
+    keywords = ['Data Analysis', 'IA', 'datasets', 'Data Engenering', 'DeepLearing'],
+    classifiers = ['Programming Language :: Python :: 3.10'],
+    install_requires=['moviepy==1.0.3', 'ipython', 'scipy', 'matplotlib', 'yt-dlp']
+)
