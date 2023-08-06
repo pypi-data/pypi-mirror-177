25f73aef39cede89c9c8b33a922300c119e6f90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:49 +0000

    Add package mielib/0.1.4/mielib-0.1.4.tar.gz

---
+++ packages/mielib/mielib-0.1.4.tar.gz/mielib-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mielib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['scipy>=1.9.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'mielib',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': '# MieLib\n\nLibrary which contains many Mie-ralated functions in optics and acoustics. In particular:\n- Scattering Mie coefficients for isotropic spheres:\n    -  Acoustic Mie coefficients based on Phys. Rev. Lett. 123, 183901 (2019) (see SM)\n    - Optics Mie coefficients based on Bohren Huffmann book.\n- Scalar spherical harmonics\n- Vector spherical harmonics (complex and real)\n\n\n## Credits\nIvan Toftul \n\n`toftul.ivan@gmail.com`\n',
+    'author': 'Ivan Toftul',
+    'author_email': 'toftul.ivan@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
