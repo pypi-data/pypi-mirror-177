7262b4a348ba84bbb40e3669d95fa6717535cf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:51 +0000

    Add package aipkg/0.2.5/aipkg-0.2.5.tar.gz

---
+++ packages/aipkg/aipkg-0.2.5.tar.gz/aipkg-0.2.5/src/aipkg/codepack.py
@@ -0,0 +1,75 @@
+import os
+
+def logic():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'logic.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def searches():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'searches.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def lppexamples():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'lppexamples.py')
+    with open(filename, encoding="utf-8") as hw:
+        print(hw.read())
+
+def constraint():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'constraint.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def optimization():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'optimization.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def tictactoe():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'tictactoe.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def searches_av():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'searches_av.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def theorytext():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'AI_notes_text.txt')
+    with open(filename) as hw:
+        print(hw.read())
+
+def knowledgeknights():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'knowledgenights.py')
+    with open(filename) as hw:
+        print(hw.read())
+
+def theory():
+    print("Removed to save size, use getdocpath() or theorytext() instead")
+#     dirname = os.path.dirname(__file__)
+#     imgsdir = os.path.join(dirname, 'AI_Notes_imgs')
+#     # print(imgsdir)
+#     img_names = []
+#     for img in os.listdir(imgsdir):
+#         img_names.append(img)
+#     # print(img_names)
+
+#     from IPython.display import Image, display
+
+#     for imageName in img_names:
+#         display(Image(filename=(os.path.join(imgsdir, imageName))))
+
+def getdocpath():
+    dirname = os.path.dirname(__file__)
+    docpath = os.path.join(dirname, 'AI_Notes.docx')
+    print(docpath)
