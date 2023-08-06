3a5e9ef322c61819f26f435a869a91cdcc7f361a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package imgtovar/1.1.4/imgtovar-1.1.4.tar.gz

---
+++ packages/imgtovar/imgtovar-1.1.4.tar.gz/imgtovar-1.1.4/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = "Extracting structured variables from image data"
+LONG_DESCRIPTION = "A package that allows researchers to analyse unstructured image data by extracting a range of features."
+
+# Setting up
+setup(
+    name="imgtovar",
+    version="1.1.4",
+    author="Dimitar Dimitrov",
+    author_email="dvdimitrov13@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    url="https://github.com/dvdimitrov13/ImgtoVar",
+    keywords=[
+        "python",
+        "research",
+        "image data",
+        "extract variables",
+        "object detection",
+        "background detection",
+        "face detection",
+        "facial attribute analysis",
+        "chart detection",
+        "graph detection",
+        "image color analysis",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7.0",
+    install_requires=[
+        "tensorflow==2.4.1",
+        "deepface>=0.0.75",
+        "yolov5>=6.1.5",
+        "PyMuPDF>=1.20.1",
+        "dlib>=19.20.0",
+        "mediapipe>=0.8.7.3"
+    ],
+)
