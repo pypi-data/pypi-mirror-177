21c23162b02105219f1688342e08aa588845819a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:38 +0000

    Add package deepdriver/0.0.25/deepdriver-0.0.25-py3-none-any.whl

---
+++ packages/deepdriver/deepdriver-0.0.25-py3-none-any.whl/deepdriver/sdk/setting.py
@@ -0,0 +1,8 @@
+from assertpy import assert_that
+from deepdriver.sdk.interface import interface
+# deepdriver 실험환경을 사용하기위한 로그인 과정
+# 서버의 login api를 호출하여 key를 서버로 전송하고 결과로서 jwt key를 받는다
+def setting(http_host, grpc_host):
+    interface.set_http_host(http_host)
+    interface.set_grpc_host(grpc_host)
+
