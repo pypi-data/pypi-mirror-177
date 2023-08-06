6e8535fd59fc1d8b47e4ea70ce03266d6d4dd77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package gpgi/0.7.2/gpgi-0.7.2.tar.gz

---
+++ packages/gpgi/gpgi-0.7.2.tar.gz/gpgi-0.7.2/setup.py
@@ -0,0 +1,26 @@
+import numpy
+from Cython.Build import cythonize
+from distutils.extension import Extension
+from setuptools import setup
+
+ext_modules = ["gpgi/clib/_indexing.pyx"]
+
+
+setup(
+    ext_modules=cythonize(
+        [
+            Extension(
+                "gpgi.clib._indexing",
+                ["gpgi/clib/_indexing.pyx"],
+                include_dirs=[numpy.get_include()],
+            ),
+            Extension(
+                "gpgi.clib._deposition_methods",
+                ["gpgi/clib/_deposition_methods.pyx"],
+                include_dirs=[numpy.get_include()],
+            ),
+        ],
+        compiler_directives={"language_level": 3},
+        annotate=True,
+    ),
+)
