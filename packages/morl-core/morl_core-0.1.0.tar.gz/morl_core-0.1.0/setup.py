1d64bf29ed99cddf5e25cb055f6a5b1f8beaf36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:34 +0000

    Add package morl-core/0.1.0/morl_core-0.1.0.tar.gz

---
+++ packages/morl-core/morl_core-0.1.0.tar.gz/morl_core-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['morl_core']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'morl-core',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Aditya Gudimella',
+    'author_email': 'aditya.gudimella@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
