db6d689f8b874a50cf3c9045536ff1c02d977e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:55 +0000

    Add package pxycrypto/0.1.0/pxycrypto-0.1.0-py3-none-any.whl

---
+++ packages/pxycrypto/pxycrypto-0.1.0-py3-none-any.whl/pxycrypto/types.py
@@ -0,0 +1,13 @@
+
+from typing import Union
+from io import BytesIO, BufferedRandom, BufferedReader
+
+
+InputType = Union[
+        bytearray,
+        bytes,
+        BytesIO,
+        BufferedRandom,
+        BufferedReader,
+        str
+]
