2cf110d89fbf4e4fd7f304771832f705b486260e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:50 +0000

    Add package rpamaker/0.0.4/rpamaker-0.0.4.tar.gz

---
+++ packages/rpamaker/rpamaker-0.0.4.tar.gz/rpamaker-0.0.4/README.md
@@ -0,0 +1,11 @@
+# Build library 
+
+'''
+python setup.py sdist bdist_wheel
+'''
+
+# Upload library
+
+'''
+twine upload dist/*
+'''
---
+++ packages/rpamaker/rpamaker-0.0.4.tar.gz/rpamaker-0.0.4/rpamaker/utils.py
@@ -0,0 +1,69 @@
+import datetime
+import logging
+import os
+import sys
+from datetime import datetime
+from subprocess import run
+import pathlib
+
+
+def get_base_path():
+    # file_path = sys.argv[0]
+    # file_location = os.path.dirname(file_path)
+    file_location = os.getcwd()
+    return file_location
+
+
+def call_robot(keyword, variables):
+    library_path = pathlib.Path(__file__).parent.resolve()
+    root_path = get_base_path()
+    base_path = os.path.join(root_path, f'{keyword}/')
+
+    logging.info(f'call_robot: {keyword} {variables} {base_path}')
+
+    output_path = os.path.join(base_path, 'output/')
+    robot_path = os.path.join(base_path, f'tasks.robot')
+
+    d = datetime.strftime(datetime.now(), '%y%m%d%H%M%S%f')
+    output_file = 'output-' + d + '.xml'
+    log_file = 'log-' + d + '.html'
+    report_file = 'report-' + d + '.html'
+
+    command = [
+        os.path.join(base_path, 'venv/Scripts/python.exe'),
+        '-m',
+        'robot',
+        '--pythonpath', base_path,
+        '--listener', 'rpamaker.listener.Listener',
+        *variables,
+        '--log', os.path.join(output_path, log_file),
+        '--output', os.path.join(output_path, output_file),
+        '--report', os.path.join(output_path, report_file),
+        robot_path,
+    ]
+
+    shell = run(
+        command,
+        shell=True,
+    )
+
+# def call_robot(keyword, variables):
+#     base_path = get_base_path()
+#     logging.info(f'call_robot: {keyword} {variables} {base_path}')
+#
+#     output_path = os.path.join(base_path, 'output/')
+#     robot_path = os.path.join(base_path, f'{keyword}.robot')
+#
+#     d = datetime.strftime(datetime.now(), '%y%m%d%H%M%S%f')
+#     output_file = 'output-' + d + '.xml'
+#     log_file = 'log-' + d + '.html'
+#     report_file = 'report-' + d + '.html'
+#     robot.run(
+#         robot_path,
+#         # loglevel='DEBUG',
+#         listener=Listener(),
+#         variable=variables,
+#         log=output_path + log_file,
+#         output=output_path + output_file,
+#         report=output_path + report_file
+#     )
