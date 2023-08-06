38d6dc3a801e0e437b1706ed8d6fa5e4991e2171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:42 +0000

    Add package mypy-boto3-stepfunctions/1.26.13.post13/mypy-boto3-stepfunctions-1.26.13.post13.tar.gz

---
+++ packages/mypy-boto3-stepfunctions/mypy-boto3-stepfunctions-1.26.13.post13.tar.gz/mypy-boto3-stepfunctions-1.26.13.post13/mypy_boto3_stepfunctions/__init__.py
@@ -0,0 +1,44 @@
+"""
+Main interface for stepfunctions service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_stepfunctions import (
+        Client,
+        GetExecutionHistoryPaginator,
+        ListActivitiesPaginator,
+        ListExecutionsPaginator,
+        ListStateMachinesPaginator,
+        SFNClient,
+    )
+
+    session = Session()
+    client: SFNClient = session.client("stepfunctions")
+
+    get_execution_history_paginator: GetExecutionHistoryPaginator = client.get_paginator("get_execution_history")
+    list_activities_paginator: ListActivitiesPaginator = client.get_paginator("list_activities")
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_state_machines_paginator: ListStateMachinesPaginator = client.get_paginator("list_state_machines")
+    ```
+"""
+from .client import SFNClient
+from .paginator import (
+    GetExecutionHistoryPaginator,
+    ListActivitiesPaginator,
+    ListExecutionsPaginator,
+    ListStateMachinesPaginator,
+)
+
+Client = SFNClient
+
+
+__all__ = (
+    "Client",
+    "GetExecutionHistoryPaginator",
+    "ListActivitiesPaginator",
+    "ListExecutionsPaginator",
+    "ListStateMachinesPaginator",
+    "SFNClient",
+)
