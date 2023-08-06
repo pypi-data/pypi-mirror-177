9418171dabbe6dae48c45b0caf8859a02a77a11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:25 +0000

    Add package pynotepad/1.3.4/pynotepad-1.3.4.tar.gz

---
+++ packages/pynotepad/pynotepad-1.3.4.tar.gz/pynotepad-1.3.4/setup.py
@@ -0,0 +1,35 @@
+import sys,os
+from setuptools import setup
+
+try:
+    os.chdir(os.path.split(__file__)[0])
+except OSError:pass
+
+try:
+    long_desc=open("README.rst").read()
+except OSError:
+    long_desc=notepad.__doc__
+
+try:os.rename("pynotepad.pyw","pynotepad.py")
+except OSError:pass
+import pynotepad
+
+setup(
+  name='pynotepad',
+  version=pynotepad.__version__,
+  description="A simple text editor made by tkinter.一款使用tkinter编写的文本编辑器程序。",
+  long_description=long_desc,
+  author=pynotepad.__author__,
+  author_email=pynotepad.__email__,
+  url="https://github.com/qfcy/Python/blob/main/pynotepad.pyw",
+  py_modules=['pynotepad'], #这里是代码所在的文件名称
+  keywords=["text","editor","notepad","tkinter","pynotepad","文本编辑器"],
+  classifiers=[
+      'Programming Language :: Python',
+      "Topic :: Text Editors",
+      "Topic :: Software Development :: User Interfaces",
+      "Natural Language :: Chinese (Simplified)"],
+  requires=["chardet","windnd"]
+)
+try:os.rename("pynotepad.py","pynotepad.pyw")
+except OSError:pass
