8beaac13fcd75b0e9718cd7084a936d64e2123ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package specifipy/0.1.4/specifipy-0.1.4-py3-none-any.whl

---
+++ packages/specifipy/specifipy-0.1.4-py3-none-any.whl/specifipy/parsers/results.py
@@ -0,0 +1,11 @@
+import dataclasses
+
+from specifipy.parsers.structure.code_structure_definitions import FunctionStructureDefinition, ClassStructureDefinition, \
+    FieldStructureDefinition
+
+
+@dataclasses.dataclass
+class ParsingResult:
+    classes: list[ClassStructureDefinition]
+    functions: list[FunctionStructureDefinition]
+    class_fields: list[FieldStructureDefinition]
