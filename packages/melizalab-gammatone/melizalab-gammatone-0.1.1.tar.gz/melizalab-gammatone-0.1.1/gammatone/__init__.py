4c9d48f38622f0c0d378aa5fc4fe26ddc47e233d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:55 +0000

    Add package melizalab-gammatone/0.1.1/melizalab-gammatone-0.1.1.tar.gz

---
+++ packages/melizalab-gammatone/melizalab-gammatone-0.1.1.tar.gz/melizalab-gammatone-0.1.1/gammatone/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""gammatone filterbank toolkit
+
+Copyright (C) 2013 Jason Heeris, <jason.heeris@gmail.com>
+Copyright (C) 2022 Dan Meliza, Jonah Weissmann, Tyler Robbins <dan@meliza.org>
+
+"""
+__version__ = "0.1.1"
