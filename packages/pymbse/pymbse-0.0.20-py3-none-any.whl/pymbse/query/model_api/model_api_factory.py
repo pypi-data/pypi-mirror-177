f7c63bfc91123d036f566b36d8a86cbef0bb8be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:46 +0000

    Add package pymbse/0.0.20/pymbse-0.0.20-py3-none-any.whl

---
+++ packages/pymbse/pymbse-0.0.20-py3-none-any.whl/pymbse/query/model_api/model_api_factory.py
@@ -0,0 +1,12 @@
+from pymbse.query.config.model_type import ModelType
+from pymbse.query.model_api.notebook_script_api import (
+    NotebookModelAPI,
+    ScriptModelAPI,
+    NotebookScriptModelAPI,
+)
+
+model_type_to_api = {
+    ModelType.NOTEBOOK: NotebookModelAPI,
+    ModelType.SCRIPT: ScriptModelAPI,
+    ModelType.NOTEBOOK_SCRIPT: NotebookScriptModelAPI,
+}
