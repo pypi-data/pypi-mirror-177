363b976f156707a15e1eb589bfeca0b987036e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:02 +0000

    Add package acclaimbadge-xblock/1.2.0/acclaimbadge-xblock-1.2.0.tar.gz

---
+++ packages/acclaimbadge-xblock/acclaimbadge-xblock-1.2.0.tar.gz/acclaimbadge-xblock-1.2.0/setup.py
@@ -0,0 +1,61 @@
+"""Setup for acclaimbadge XBlock."""
+
+import os
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+setup(
+    name='acclaimbadge-xblock',
+    version='1.2.0',
+    description='XBlock used to retrieve badges from acclaim',  
+    license='Apache License 2.0',
+    packages=[
+        'acclaimbadge',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    entry_points={
+        'xblock.v1': [
+            'acclaimbadge = acclaimbadge:AcclaimBadgeXBlock',
+        ]
+    },
+    package_data=package_data("acclaimbadge", ["static", "public"]),
+)
