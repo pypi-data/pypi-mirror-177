71dc9ea238fb4571d366d2cd1e8792ff6835b9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:20 +0000

    Add package vtk-addon/0.1.0/vtk_addon-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/vtk-addon/vtk_addon-0.1.0-cp310-cp310-win_amd64.whl/vtk_addon-0.1.0.data/data/lib/cmake/vtk/vtkAddon-targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "VTK::Addon" for configuration "Release"
+set_property(TARGET VTK::Addon APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(VTK::Addon PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/vtkAddon.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "VTK::CommonMath"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/vtkmodules/vtkAddon.dll"
+  )
+
+list(APPEND _cmake_import_check_targets VTK::Addon )
+list(APPEND _cmake_import_check_files_for_VTK::Addon "${_IMPORT_PREFIX}/lib/vtkAddon.lib" "${_IMPORT_PREFIX}/lib/vtkmodules/vtkAddon.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
