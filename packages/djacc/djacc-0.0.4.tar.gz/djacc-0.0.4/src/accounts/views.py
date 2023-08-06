45f055d3834793411d324d2248b29cad21761e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:49 +0000

    Add package djacc/0.0.4/djacc-0.0.4.tar.gz

---
+++ packages/djacc/djacc-0.0.4.tar.gz/djacc-0.0.4/src/accounts/views.py
@@ -0,0 +1,50 @@
+from .forms import *
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+
+
+# Create your views here.
+class Login(SuccessMessageMixin, LoginView):
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('home')
+    success_message = "Login successful"
+
+
+class Register(SuccessMessageMixin, CreateView):
+    form_class = UserRegisterForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('accounts:signin')
+    success_message = "Your profile was created successfully"
+
+
+class Profile(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    # form_class = UserChangeForm
+    form_class = UserProfile
+    template_name = 'accounts/profile.html'
+    success_url = reverse_lazy('home')
+    success_message = "Your profile updated successfully"
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordChange(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'accounts/change-password.html'
+    success_url = reverse_lazy('home')
+    success_message = "Your password was changed successfully"
+
+
+class PasswordReset(SuccessMessageMixin, PasswordResetView):
+    template_name = 'accounts/reset-password.html'
+    success_url = reverse_lazy('accounts:login')
+    success_message = "Your password reset request was sent"
+    form_class = EmailValidationCheckForm
+
+
+class PasswordResetConfirm(SuccessMessageMixin, PasswordResetConfirmView):
+    template_name = 'accounts/reset-password-confirm.html'
+    success_url = reverse_lazy('accounts:login')
+    success_message = "Your password reset was done successfully"
