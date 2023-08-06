419e38a678dc8a320f7754190d3c33fab8dbdc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:49 +0000

    Add package snowconn/3.10.0/snowconn-3.10.0.tar.gz

---
+++ packages/snowconn/snowconn-3.10.0.tar.gz/snowconn-3.10.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_directory, 'README.md')
+
+with open(readme_path, encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='snowconn',
+    version='3.10.0',
+    description='Python utilities for connection to the Snowflake data '
+                'warehouse',
+    url='https://github.com/Daltix/snowconn',
+    author='Daltix NV',
+    author_email='snowconn@daltix.com',
+    packages=['snowconn'],
+    install_requires=[
+        'six',
+        'snowflake-connector-python==2.7.9',
+        'snowflake-sqlalchemy>=1.3, <1.4',
+        'sqlalchemy<=1.4.41',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    extras_require={
+        "pandas": [
+            "snowflake-connector-python[pandas]==2.7.9",
+        ],
+        "storage": [
+            "snowflake-connector-python[secure-local-storage]==2.7.9",
+            'wheel',
+        ],
+        "all": [
+            "boto3",
+            "snowflake-connector-python[secure-local-storage,pandas]==2.7.9",
+        ]
+    }
+)
