a3284d71db77d90b9017c13d6918e4d3b67ecf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:17 +0000

    Add package configml/0.0.1/configml-0.0.1.tar.gz

---
+++ packages/configml/configml-0.0.1.tar.gz/configml-0.0.1/README.md
@@ -0,0 +1,5 @@
+﻿<div align="left">
+
+**configml: Configured Machine Learning ** </br>
+**Version 0.0.1 out now!** 
+  
---
+++ packages/configml/configml-0.0.1.tar.gz/configml-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @author: Wenting Tu
+# @email: wtugmail@163.com
+# @date: 2022/11
+
+from setuptools import setup, find_packages
+
+setup(
+    name="configml",
+    version="0.0.1",
+    description="Configured Machine Learning",
+    author=["Wenting Tu"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)
