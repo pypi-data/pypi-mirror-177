fc70c30b836d901756dc7a4ef5d63d2127f32e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:14 +0000

    Add package easyops/1.0.5/easyops-1.0.5-py3-none-any.whl

---
+++ packages/easyops/easyops-1.0.5-py3-none-any.whl/easyops/apps/easy_plugin.py
@@ -0,0 +1,21 @@
+from ._base import BaseAppPaths, Path, APP
+
+
+class PluginApis(BaseAppPaths):
+    execute = Path("/", method="POST", desc="execute plugin")
+
+
+class Plugin(APP):
+    paths = PluginApis("plugin")
+
+    def execute(self, name, options=None):
+        """
+        execute plugin
+        :param name: plugin name
+        :param options:
+        :return:
+        """
+        return self.client.call(self.paths.execute.method, self.paths.execute, json={
+            "name": name,
+            "inputs": options or {}
+        })
