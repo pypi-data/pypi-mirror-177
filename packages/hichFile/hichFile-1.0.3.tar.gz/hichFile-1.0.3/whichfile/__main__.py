628fa106c9251a9c1a94765d2c257ee689d96bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:13 +0000

    Add package hichFile/1.0.3/hichFile-1.0.3.tar.gz

---
+++ packages/hichFile/hichFile-1.0.3.tar.gz/hichFile-1.0.3/whichfile/__main__.py
@@ -0,0 +1,32 @@
+import webbrowser
+
+class CircularLink(EnvironmentError):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Alias(object):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Builtin(Alias):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+                              resolved classes.
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Function(Alias):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class ResolvedNames(object):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class ResolvedPath(object):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+        """ Print a correct representation of this class instance. """
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+    @classmethod
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class InvalidArg(ValueError):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
