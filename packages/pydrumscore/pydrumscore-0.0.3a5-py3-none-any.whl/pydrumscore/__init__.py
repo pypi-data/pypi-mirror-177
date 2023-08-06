fcf6a4f094f2772ab1a1ad4d11499b163253e78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:16 +0000

    Add package pydrumscore/0.0.3a5/pydrumscore-0.0.3a5-py3-none-any.whl

---
+++ packages/pydrumscore/pydrumscore-0.0.3a5-py3-none-any.whl/pydrumscore/__init__.py
@@ -0,0 +1,8 @@
+"""
+HELP TODO
+"""
+import logging
+
+# Init logger for all modules
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
---
+++ packages/pydrumscore/pydrumscore-0.0.3a5-py3-none-any.whl/pydrumscore/__main__.py
@@ -0,0 +1,7 @@
+"""
+Calls the exporter when running 'pydrumscore' from the command line
+"""
+from pydrumscore.core import export
+
+if __name__ == "__main__":
+    export.main()
