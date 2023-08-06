5bb29b8094807e9112785ea517af2b99089342f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:23 +0000

    Add package iman/0.0.31/iman-0.0.31.tar.gz

---
+++ packages/iman/iman-0.0.31.tar.gz/iman-0.0.31/iman/tsne.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
+import numpy as np
+
+def plot(fea , lab):
+  lab= np.array(lab)
+  tsne = TSNE(n_components=2, random_state=0)
+
+  X_2d = tsne.fit_transform(fea)
+
+
+  for i in range(max(lab)+1):
+    plt.scatter(X_2d[lab == i, 0], X_2d[lab == i,1])
+  plt.show()  
+
