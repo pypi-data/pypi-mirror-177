c975cb1ebffdde4fa139033c34920a2d30f2f294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package vidstreamv2/1.0/vidstreamv2-1.0-py3-none-any.whl

---
+++ packages/vidstreamv2/vidstreamv2-1.0-py3-none-any.whl/vidstream/__init__.py
@@ -0,0 +1,6 @@
+from vidstreamv2.streaming import StreamingServer
+from vidstreamv2.streaming import CameraClient
+from vidstreamv2.streaming import VideoClient
+from vidstreamv2.streaming import ScreenShareClient
+from vidstreamv2.audio import AudioSender
+from vidstreamv2.audio import AudioReceiver
