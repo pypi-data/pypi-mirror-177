29ae35d4508fe51fd747563aa8d1ecea459a83a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:12 +0000

    Add package superbuddy/0.0.1/superbuddy-0.0.1.tar.gz

---
+++ packages/superbuddy/superbuddy-0.0.1.tar.gz/superbuddy-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='superbuddy',
+    version='0.0.1',
+    author='Rahul Shome',
+    author_email='rahulshome8@gmail.com',
+    packages=find_packages(),
+    install_requires=['click'],
+    keywords='example',
+    entry_points="""
+    [console_scripts]
+    buddy=superbuddy.cli:main
+    """
+)
