bbc0aec431da6c94a68ee088d2287c74f3889539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:32 +0000

    Add package django-rest-framework-tricks/0.2.14/django_rest_framework_tricks-0.2.14-py2.py3-none-any.whl

---
+++ packages/django-rest-framework-tricks/django_rest_framework_tricks-0.2.14-py2.py3-none-any.whl/rest_framework_tricks/filters/__init__.py
@@ -0,0 +1,10 @@
+"""
+Filters.
+"""
+
+from .ordering import *
+
+__author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
+__copyright__ = "2017-2022 Artur Barseghyan"
+__license__ = "GPL-2.0-only OR LGPL-2.1-or-later"
+__all__ = ("OrderingFilter",)
