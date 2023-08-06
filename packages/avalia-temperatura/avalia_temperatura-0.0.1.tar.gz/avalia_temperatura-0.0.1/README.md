68a8e0e66c2f2a14d313270c8cc698ebe299102a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:14 +0000

    Add package avalia-temperatura/0.0.1/avalia_temperatura-0.0.1.tar.gz

---
+++ packages/avalia-temperatura/avalia_temperatura-0.0.1.tar.gz/avalia_temperatura-0.0.1/README.md
@@ -0,0 +1,24 @@
+# package_name
+
+Descricao. 
+O pacote avalia_temperatura contem um unico modulo simples, que apos o input da temperatura corporal do usuario retorna se ela esta normal, febril ou abaixo do normal, sendo este um desafio de Projeto de Bootcamp da DIO.
+## Installation
+
+Use o adm. de pacote [pip](https://pip.pypa.io/en/stable/) para instalar avalia_temperatura
+
+```bash
+pip install avalia_temperatura
+```
+
+## Usage
+
+```python
+from avalia_temperatura import termometro
+termometro.temperatura()
+```
+
+## Author
+Gabriel Dimas
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/avalia-temperatura/avalia_temperatura-0.0.1.tar.gz/avalia_temperatura-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="avalia_temperatura",
+    version="0.0.1",
+    author="Gabriel",
+    description="Este pacote é um exercício do Desafio de Projeto da DIO",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GabeGD/Bootcamp_Geracao_Tech_Unimed_BH_Ciencia_de_Dados/pacote_DIO",
+    packages=find_packages(),
+    python_requires='>=3.11',
+)
