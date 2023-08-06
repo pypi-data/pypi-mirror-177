b246c8e129838a487174f066b57de8d639524535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package prelude-cli/0.9.15/prelude-cli-0.9.15.tar.gz

---
+++ packages/prelude-cli/prelude-cli-0.9.15.tar.gz/prelude-cli-0.9.15/prelude_cli/cli.py
@@ -0,0 +1,23 @@
+import click
+from prelude_cli.views.iam import iam
+from prelude_cli.views.configure import configure
+from prelude_cli.views.build import build
+from prelude_cli.views.detect import detect
+from prelude_sdk.models.account import Account
+
+
+@click.group()
+@click.pass_context
+@click.option('--profile', default='default', help='detect keychain profile', show_default=True)
+def cli(ctx, profile):
+    ctx.obj = Account(profile=profile)
+
+
+cli.add_command(iam)
+cli.add_command(configure)
+cli.add_command(detect)
+cli.add_command(build)
+
+
+if __name__ == '__main__':
+    cli()
