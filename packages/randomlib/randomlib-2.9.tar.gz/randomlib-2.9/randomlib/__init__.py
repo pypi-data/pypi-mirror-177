8c884aa7d9b78298b10da7e4e9fba0cac1b27a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:56 +0000

    Add package randomlib/2.9/randomlib-2.9.tar.gz

---
+++ packages/randomlib/randomlib-2.9.tar.gz/randomlib-2.9/randomlib/__init__.py
@@ -0,0 +1,6 @@
+import logging
+import pandas as pd
+logging.disable(logging.INFO) # disable INFO 
+logging.disable(logging.DEBUG) # disable INFO 
+logging.disable(logging.WARNING) # disable INFO 
+pd.options.display.max_colwidth = None
---
+++ packages/randomlib/randomlib-2.9.tar.gz/randomlib-2.9/randomlib/modelRepo/mahaHate.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+import pandas as pd
+from ..config import paths
+
+class HateModel:
+    def __init__(self, modelName='mahahate-bert'):
+        self.modelName = modelName
+        self.modelRoute = paths[self.modelName]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.modelRoute)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.modelRoute)
+        self.classifier = pipeline('text-classification',
+                              model=self.model, tokenizer=self.tokenizer)
+
+    def getPolarityScore(self, text):
+        result = self.classifier(text)
+        df = pd.DataFrame.from_dict(result)
+        return df
+
+    def listModels(self):
+            for model in paths:
+                print(model, ": ", paths[model], "\n")
