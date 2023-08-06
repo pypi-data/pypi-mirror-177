423da1d74b07b9f32f8748d30291e276f24898b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:39 +0000

    Add package tmp-connection-psql/1.2.0/tmp_connection_psql-1.2.0.tar.gz

---
+++ packages/tmp-connection-psql/tmp_connection_psql-1.2.0.tar.gz/tmp_connection_psql-1.2.0/tmp_connection_psql/__init__.py
@@ -0,0 +1,6 @@
+"""Little project to create a temparory connection to a psql database."""
+
+__version__ = "1.2.0"
+
+# First party imports
+from tmp_connection_psql.tmp_connection_psql import tmp_connection  # noqa: F401
