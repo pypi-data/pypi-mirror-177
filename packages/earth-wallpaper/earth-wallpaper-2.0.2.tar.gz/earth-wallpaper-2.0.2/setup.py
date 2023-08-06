b33ec242bf88e04d5e08743f04cb8e009ad39030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:25 +0000

    Add package earth-wallpaper/2.0.2/earth-wallpaper-2.0.2.tar.gz

---
+++ packages/earth-wallpaper/earth-wallpaper-2.0.2.tar.gz/earth-wallpaper-2.0.2/setup.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+from setuptools import setup, find_packages
+from earth_wallpaper.about import get_version
+import platform
+
+
+requires_list=[
+    'Pillow',
+    'PySide6',
+    'requests',
+    'setuptools',
+]
+if platform.system()=="Windows":
+    requires_list.append('pywin32')
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='earth-wallpaper',
+    version=get_version(),
+    url='https://github.com/ambition-echo/earth_wallpaper',
+    description='Simple and easy to use multifunctional wallpaper software 简单好用的多功能壁纸软件',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GPLv3",
+    author='ambition-echo',
+    author_email='ambition_echo@outlook.com',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'earth_wallpaper': ['resource/earth-wallpaper.png','utils/xfce.sh']
+    },
+    entry_points={
+        'console_scripts': ['earth-wallpaper = earth_wallpaper.main:main']
+    },
+    requires=requires_list
+)
