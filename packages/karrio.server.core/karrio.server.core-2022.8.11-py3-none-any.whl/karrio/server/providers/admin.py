297df71cb4d7d88bb704be087ad0f7fb6aacd046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:15 +0000

    Add package karrio.server.core/2022.8.11/karrio.server.core-2022.8.11-py3-none-any.whl

---
+++ packages/karrio.server.core/karrio.server.core-2022.8.11-py3-none-any.whl/karrio/server/providers/admin.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib import admin
+from django import forms
+
+import karrio.server.providers.models as carriers
+
+
+def model_admin(model):
+    class _Admin(admin.ModelAdmin):
+        list_display = ("__str__", "test_mode", "active")
+        exclude = (
+            ["active_users", "metadata"]
+            if model.__name__ not in ["GenericSettings", "CanadaPostSettings"]
+            else ["active_users", "services"]
+        )
+        formfield_overrides = {
+            models.CharField: {
+                "widget": forms.TextInput(
+                    attrs={
+                        "type": "text",
+                        "readonly": "true",
+                        "class": "vTextField",
+                        "data - lpignore": "true",
+                        "autocomplete": "keep-off",
+                        "onfocus": "this.removeAttribute('readonly');",
+                    }
+                )
+            }
+        }
+
+        def get_queryset(self, request):
+            query = super().get_queryset(request)
+            return query.filter(created_by=None)
+
+    return type(f"{model.__class__.__name__}Admin", (_Admin,), {})
+
+
+for name, model in carriers.MODELS.items():
+    admin.site.register(model, model_admin(model))
