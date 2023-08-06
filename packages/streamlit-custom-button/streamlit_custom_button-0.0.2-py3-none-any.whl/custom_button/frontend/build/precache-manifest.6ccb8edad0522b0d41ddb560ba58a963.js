479bf517c74dd5686661d5ca67bde1698e9ef65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:11 +0000

    Add package streamlit-custom-button/0.0.2/streamlit_custom_button-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-custom-button/streamlit_custom_button-0.0.2-py3-none-any.whl/custom_button/frontend/build/precache-manifest.6ccb8edad0522b0d41ddb560ba58a963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da48bed6e94e9323e87988c98b4ce13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b4e3e285ef14d730b5b",
+    "url": "./static/css/main.b1c2beb0.chunk.css"
+  },
+  {
+    "revision": "71ee9b2c5edfe70a1777",
+    "url": "./static/js/2.33adaba6.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.33adaba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4e3e285ef14d730b5b",
+    "url": "./static/js/main.6361ea37.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
