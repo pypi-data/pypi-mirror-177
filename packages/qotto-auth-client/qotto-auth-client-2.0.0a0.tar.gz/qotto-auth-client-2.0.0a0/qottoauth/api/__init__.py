055dbcd6412f4ce744f46efe0b133ecee26fce4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:51 +0000

    Add package qotto-auth-client/2.0.0a0/qotto-auth-client-2.0.0a0.tar.gz

---
+++ packages/qotto-auth-client/qotto-auth-client-2.0.0a0.tar.gz/qotto-auth-client-2.0.0a0/qottoauth/api/__init__.py
@@ -0,0 +1,10 @@
+from qottoauth.api.base import QottoAuthApi, QottoAuthApiError
+from qottoauth.api.gql import QottoAuthGqlApi
+from qottoauth.api.test import QottoAuthTestApi
+
+__all__ = [
+    'QottoAuthApi',
+    'QottoAuthApiError',
+    'QottoAuthTestApi',
+    'QottoAuthGqlApi',
+]
