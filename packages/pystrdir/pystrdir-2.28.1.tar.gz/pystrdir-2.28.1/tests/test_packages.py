7bdabb7020c099f7523e7a9610f7e4519e0f39fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:37 +0000

    Add package pystrdir/2.28.1/pystrdir-2.28.1.tar.gz

---
+++ packages/pystrdir/pystrdir-2.28.1.tar.gz/pystrdir-2.28.1/tests/test_packages.py
@@ -0,0 +1,13 @@
+import pystrdir
+
+
+def test_can_access_urllib3_attribute():
+    pystrdir.packages.urllib3
+
+
+def test_can_access_idna_attribute():
+    pystrdir.packages.idna
+
+
+def test_can_access_chardet_attribute():
+    pystrdir.packages.chardet
