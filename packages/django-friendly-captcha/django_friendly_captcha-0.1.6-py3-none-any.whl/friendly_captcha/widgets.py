254914f02fd691089a69d8cbc201502a2d758093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:25 +0000

    Add package django-friendly-captcha/0.1.6/django_friendly_captcha-0.1.6-py3-none-any.whl

---
+++ packages/django-friendly-captcha/django_friendly_captcha-0.1.6-py3-none-any.whl/friendly_captcha/widgets.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.forms import Widget
+from django.utils import translation
+from django.utils.html import format_html
+from django.forms.utils import flatatt
+
+
+class FrcCaptchaWidget(Widget):
+
+    def render(self, name, value, attrs=None, renderer=None):
+        attrs['data-lang'] = translation.get_language()
+        attrs['data-sitekey'] = getattr(settings, 'FRC_CAPTCHA_SITE_KEY', None)
+        attrs['data-solution-field-name'] = name
+        attrs['class'] = 'frc-captcha'
+        final_attrs = self.build_attrs(attrs)
+        return format_html('<div {}></div>', flatatt(final_attrs))
