b6ff5fecca558791f480f8b2bc6accfec4764598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:30 +0000

    Add package setuptools-wrapper/0.2.3/setuptools_wrapper-0.2.3-py3-none-any.whl

---
+++ packages/setuptools-wrapper/setuptools_wrapper-0.2.3-py3-none-any.whl/setuptools_wrapper/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.0
+# hash=1535b7fcabeff521ed93aac68bf255cf
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A simple interface to setuptools's setup function."
+PKG_NAME = "setuptools-wrapper"
+VERSION = "0.2.3"
