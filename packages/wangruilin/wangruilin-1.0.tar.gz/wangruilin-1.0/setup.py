319f99c26b873b2e39972f568a03c030ab1ff649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:38 +0000

    Add package wangruilin/1.0/wangruilin-1.0.tar.gz

---
+++ packages/wangruilin/wangruilin-1.0.tar.gz/wangruilin-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wangruilin',
+    version='v1.0',
+    description='for say',
+    py_modules=['wangruilin'],
+    author='upuuuuuu',
+    author_email='2226548059@qq.com',
+    url='https://github.com/upuuuuuu',
+    license='MIT'
+)
