7390dea46732eaf429549780004f87f71cb0d877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:03 +0000

    Add package mysql-mimic/2.0.0/mysql-mimic-2.0.0.tar.gz

---
+++ packages/mysql-mimic/mysql-mimic-2.0.0.tar.gz/mysql-mimic-2.0.0/mysql_mimic/constants.py
@@ -0,0 +1,16 @@
+from mysql_mimic.types import Capabilities
+
+DEFAULT_SERVER_CAPABILITIES = (
+    Capabilities.CLIENT_PROTOCOL_41
+    | Capabilities.CLIENT_DEPRECATE_EOF
+    | Capabilities.CLIENT_CONNECT_WITH_DB
+    | Capabilities.CLIENT_QUERY_ATTRIBUTES
+    | Capabilities.CLIENT_CONNECT_ATTRS
+    | Capabilities.CLIENT_PLUGIN_AUTH
+    | Capabilities.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA
+    | Capabilities.CLIENT_SECURE_CONNECTION
+    | Capabilities.CLIENT_LONG_PASSWORD
+    | Capabilities.CLIENT_ODBC
+    | Capabilities.CLIENT_INTERACTIVE
+    | Capabilities.CLIENT_IGNORE_SPACE
+)
