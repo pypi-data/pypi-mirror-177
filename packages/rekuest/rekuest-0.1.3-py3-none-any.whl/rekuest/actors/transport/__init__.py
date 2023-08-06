3c7599d48e8683be8607bc2cc6d956d2b474082d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:36 +0000

    Add package rekuest/0.1.3/rekuest-0.1.3-py3-none-any.whl

---
+++ packages/rekuest/rekuest-0.1.3-py3-none-any.whl/rekuest/actors/transport/__init__.py
@@ -0,0 +1,18 @@
+from typing import Awaitable, Callable, List, Protocol, Optional
+from pydantic import BaseModel
+
+from rekuest.api.schema import AssignationStatus, LogLevelInput
+from rekuest.messages import Assignation
+
+
+class ActorTransport(BaseModel):
+
+    log_to_assignation: Callable[[str, LogLevelInput, str], Awaitable[None]]
+    change_assignation: Callable[[Assignation, AssignationStatus, str], Awaitable[None]]
+    list_assignations: Callable[[Optional[AssignationStatus]], List[Assignation]]
+
+
+class SharedTransport(ActorTransport):
+    log_to_assignation: Callable[[str, LogLevelInput, str], Awaitable[None]]
+    change_assignation: Callable[[Assignation, AssignationStatus, str], Awaitable[None]]
+    list_assignations: Callable[[Optional[AssignationStatus]], List[Assignation]]
