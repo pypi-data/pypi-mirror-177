c8de9342064d53b54a04e1377fa8c475b9b69cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:54 +0000

    Add package tryalgorumour/0.0.1/tryalgorumour-0.0.1.tar.gz

---
+++ packages/tryalgorumour/tryalgorumour-0.0.1.tar.gz/tryalgorumour-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'This package includes algorithms such as LinearSearch,BinarySearch, Two pointer , Sliding window. It will get updated every week!!!'
+
+# Setting up
+setup(
+    name="tryalgorumour",
+    version=VERSION,
+    author="Rumour(Sarthak Samantaray)",
+    author_email="<sarthak.samantaraycoding@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=[],
+    install_requires = [])
