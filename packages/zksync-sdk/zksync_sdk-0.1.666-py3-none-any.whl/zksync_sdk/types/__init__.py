037926eac4a128a0fb00b9dae5c16ba1bf094d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:16 +0000

    Add package zksync-sdk/0.1.666/zksync_sdk-0.1.666-py3-none-any.whl

---
+++ packages/zksync-sdk/zksync_sdk-0.1.666-py3-none-any.whl/zksync_sdk/types/__init__.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+from .responses import *
+from .signatures import *
+from .transactions import *
+from .auth_types import *
+
+
+class ChainId(IntEnum):
+    MAINNET = 1
+    RINKEBY = 4
+    ROPSTEN = 3
+    LOCALHOST = 9
