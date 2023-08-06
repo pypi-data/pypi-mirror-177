fbce8f569a699b98785c1b57bd499ccaebb457af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:32 +0000

    Add package seven-shell-studio/1.0.12/seven_shell_studio-1.0.12-py3-none-any.whl

---
+++ packages/seven-shell-studio/seven_shell_studio-1.0.12-py3-none-any.whl/seven_shell_studio/route.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Kangwenbin
+:Date: 2022-11-21 17:46:56
+:LastEditTime: 2022-11-22 16:45:25
+:LastEditors: Kangwenbin
+:Description: 
+"""
+import seven_shell_studio.handlers.api.integral as integral
+
+def seven_studio_route():
+    return [
+        (r"/api/integral/exchange_list", integral.ExchangeListHandler), # 兑换列表 
+        (r"/api/integral/goods_list", integral.GoodsListHandler), # 商品列表 
+        (r"/api/integral/goods_manage", integral.GoodsManageHandler), # 商品信息/商品添加/商品修改/商品删除/商品上下架 
+    ]
