71a98ef2d3c07550472fd7b8bdfa008554bc5294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:17 +0000

    Add package aligo/5.4.2/aligo-5.4.2-py3-none-any.whl

---
+++ packages/aligo/aligo-5.4.2-py3-none-any.whl/aligo/response/GetShareTokenResponse.py
@@ -0,0 +1,21 @@
+"""..."""
+from dataclasses import dataclass
+
+from aligo.types import DataClass
+
+
+@dataclass
+class GetShareTokenResponse(DataClass, str):
+    """..."""
+    share_token: str = None
+    expire_time: str = None
+    expires_in: int = None
+
+    def __new__(cls, *args, **kwargs):
+        share_token = kwargs.get('share_token')
+        if share_token is None:
+            share_token = args[0]
+        return super().__new__(cls, share_token)
+
+    def __str__(self):
+        return self.share_token
