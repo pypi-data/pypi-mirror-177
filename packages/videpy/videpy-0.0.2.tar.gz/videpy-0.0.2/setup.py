3d0a959a31e4fd78a351c4ddddaceb6abbe132a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:59 +0000

    Add package videpy/0.0.2/videpy-0.0.2.tar.gz

---
+++ packages/videpy/videpy-0.0.2.tar.gz/videpy-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('videpy/README.md', 'r') as arq:
+    readme = arq.read()
+
+setup(
+    name='videpy',
+    version='0.0.2',
+    license='MIT License',
+    author='Rodolpho Macedo dos Santos',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rodolpho-progmatica/videpy',
+    author_email='rodolpho.ime@gmail.com',
+    keywords=['videpy', 'vide', 'bayesian', 'stan', 'pystan', 'rethinking'],
+    description=u'Tools to handle samples posteriori from Stan (pystan)',
+    packages=['videpy'],
+    install_requiments=['numpy', 'pandas', 'matplotlib'],
+)
