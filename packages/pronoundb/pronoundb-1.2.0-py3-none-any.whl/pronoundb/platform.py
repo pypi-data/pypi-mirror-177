4e19a11e76237efec004d2d809333cf50e3d93d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:11 +0000

    Add package pronoundb/1.2.0/pronoundb-1.2.0-py3-none-any.whl

---
+++ packages/pronoundb/pronoundb-1.2.0-py3-none-any.whl/pronoundb/platform.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Platform(Enum):
+    DISCORD = "discord"
+    FACEBOOK = "facebook"
+    GITHUB = "github"
+    MINECRAFT = "minecraft"
+    TWITCH = "twitch"
+    TWITTER = "twitter"
