e9e6b34024a94593a29e252fcce0bf14718ced8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:51 +0000

    Add package kiki-utils/1.0.3/kiki_utils-1.0.3.tar.gz

---
+++ packages/kiki-utils/kiki_utils-1.0.3.tar.gz/kiki_utils-1.0.3/kikiutils/aes.py
@@ -0,0 +1,51 @@
+import json
+
+from binascii import a2b_hex, b2a_hex
+from Cryptodome.Cipher import AES
+from typing import Union
+
+from .check import isdict, islist
+from .string import b2s, s2b
+from .hash import md5
+
+
+class AesCrypt:
+    def __init__(
+        self, iv: Union[bytes, str],
+        key: Union[bytes, str],
+        mode=AES.MODE_CBC
+    ):
+        iv = s2b(iv)
+        key = md5(key, True)
+        self.cipher = AES.new(key, mode, iv=iv)
+
+    def pad(self, text: Union[dict, list, str]):
+        if isdict(text) or islist(text):
+            text = json.dumps(text)
+
+        text = s2b(text)
+
+        while len(text) % 16 != 0:
+            text += b' '
+
+        return text
+
+    def encrypt(self, text: Union[dict, list, str]):
+        text = self.pad(text)
+        ciphertext = self.cipher.encrypt(text)
+        return b2s(b2a_hex(ciphertext))
+
+    def decrypt(self, ciphertext: str):
+        ciphertext = a2b_hex(s2b(ciphertext))
+        text = self.cipher.decrypt(ciphertext)
+
+        try:
+            text = b2s(text).rstrip()
+
+            try:
+                data = json.loads(text)
+                return data
+            except:
+                return text
+        except:
+            return text.rstrip()
---
+++ packages/kiki-utils/kiki_utils-1.0.3.tar.gz/kiki_utils-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='kiki_utils',
+    classifiers=[
+        'License :: Freely Distributable'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    version='1.0.3',
+    description='Utils functions without cv2 and pillow',
+    author='kiki-kanri',
+    author_email='a470666@gmail.com',
+    keywords=['Utils'],
+    install_requires=[
+        'aiofiles',
+        'aiohttp',
+        'brotli',
+        'loguru',
+        'pycryptodomex',
+        'pyopenssl',
+        'python-magic;platform_system=="Linux"',
+        'python-magic-bin;platform_system=="Windows"',
+        'quart',
+        'requests',
+        'validator'
+    ],
+    python_requires=">=3.6"
+)
