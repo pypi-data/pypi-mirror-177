d4978a961ead6a13d5723825f77b8ecd51c21a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:01 +0000

    Add package sharingiscaring/0.1.4/sharingiscaring-0.1.4.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.1.4.tar.gz/sharingiscaring-0.1.4/sharingiscaring/ccdscan.py
@@ -0,0 +1,43 @@
+from .ccdscan_queries._accountByAddress                      import Mixin as _accountByAddress
+from .ccdscan_queries._accounts                              import Mixin as _accounts
+from .ccdscan_queries._bakerByBakerId                        import Mixin as _bakerByBakerId
+from .ccdscan_queries._bakers                                import Mixin as _bakers
+from .ccdscan_queries._blockByBlockHash                      import Mixin as _blockByBlockHash
+from .ccdscan_queries._blocks                                import Mixin as _blocks
+from .ccdscan_queries._passiveDelegation                     import Mixin as _passiveDelegation
+from .ccdscan_queries._paydayStatus                          import Mixin as _paydayStatus
+from .ccdscan_queries._poolRewardMetricsForBakerPool         import Mixin as _poolRewardMetricsForBakerPool
+from .ccdscan_queries._poolRewardMetricsForPassiveDelegation import Mixin as _poolRewardMetricsForPassiveDelegation
+from .ccdscan_queries._rewardMetricsForAccount               import Mixin as _rewardMetricsForAccount
+from .ccdscan_queries._search                                import Mixin as _search
+from .ccdscan_queries._transactionByTransactionHash          import Mixin as _transactionByTransactionHash
+from .ccdscan_queries._transactionMetrics                    import Mixin as _transactionMetrics
+from .ccdscan_queries.ql_support                             import Mixin as ql_support
+from .ccdscan_queries.exchange_account_updates               import Mixin as exchange_accounts  
+
+# from .ccdscan_queries.enums import NODES_REQUEST_LIMIT
+NODES_REQUEST_LIMIT =20
+class CCDScan(
+    _accountByAddress,
+    _accounts,
+    _bakerByBakerId,
+    _bakers,
+    _blockByBlockHash,
+    _blocks,
+    _passiveDelegation,
+    _paydayStatus,
+    _poolRewardMetricsForBakerPool,
+    _poolRewardMetricsForPassiveDelegation,
+    _rewardMetricsForAccount,
+    _search,
+    _transactionByTransactionHash,
+    _transactionMetrics,
+    ql_support,
+    exchange_accounts,
+    ):
+
+    # https://github.com/Concordium/concordium-scan/tree/e95e8b2b191fefcf381ef4b4a1c918dd1f11ae05/frontend/src/queries
+    def __init__(self):
+        self.nodes_request_limit = NODES_REQUEST_LIMIT
+        self.graphql_url = 'https://api-ccdscan.mainnet.concordium.software/graphql/'
+        
