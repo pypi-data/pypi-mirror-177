4e7170574743d08188575ad3ee9d512b803e0c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:24 +0000

    Add package sourceplusplus/0.1.8/sourceplusplus-0.1.8.tar.gz

---
+++ packages/sourceplusplus/sourceplusplus-0.1.8.tar.gz/sourceplusplus-0.1.8/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import setup
+
+setup(name='sourceplusplus',
+      version='0.1.8',
+      description='Source++ Python Probe',
+      url='https://github.com/sourceplusplus/probe-python',
+      author='Source++',
+      author_email='hello@sourceplusplus.com',
+      license='Apache License, Version 2.0',
+      packages=setuptools.find_packages(),
+      install_requires=['vertx-eventbus-client>=1.0.0',
+                        'apache-skywalking>=0.8.0',
+                        'nopdb>=0.2.0',
+                        'pyhumps>=3.7.2',
+                        'PyYAML>=6.0'])
---
+++ packages/sourceplusplus/sourceplusplus-0.1.8.tar.gz/sourceplusplus-0.1.8/sourceplusplus/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.8'
+__name__ = 'Source++'
+agent_name = 'Source++ Python Probe'
