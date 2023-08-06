0a6fb773d999cf1e4fab83b025763070db4f645d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:31 +0000

    Add package django-rest-framework-tricks/0.2.13/django_rest_framework_tricks-0.2.13-py2.py3-none-any.whl

---
+++ packages/django-rest-framework-tricks/django_rest_framework_tricks-0.2.13-py2.py3-none-any.whl/rest_framework_tricks/serializers/__init__.py
@@ -0,0 +1,17 @@
+"""
+Serializers.
+"""
+
+from .nested_proxy import *
+
+__author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
+__copyright__ = "2017-2022 Artur Barseghyan"
+__license__ = "GPL-2.0-only OR LGPL-2.1-or-later"
+__all__ = (
+    "extract_nested_serializers",
+    "HyperlinkedModelSerializer",
+    "is_nested_proxy_field",
+    "ModelSerializer",
+    "NestedProxyFieldIdentifier",
+    "set_instance_values",
+)
