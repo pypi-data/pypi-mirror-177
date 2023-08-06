71c232a9f43673898e7d7f96058d1ffabe8182a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:50 +0000

    Add package bfet/0.1.4/bfet-0.1.4-py2.py3-none-any.whl

---
+++ packages/bfet/bfet-0.1.4-py2.py3-none-any.whl/bfet/__init__.py
@@ -0,0 +1,13 @@
+from bfet.create_data import DataCreator
+from bfet.testing_models import DjangoTestingModel
+
+__author__ = """Lucas Montes"""
+
+__email__ = "lluc23@hotmail.com"
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "DjangoTestingModel",
+    "DataCreator",
+]
