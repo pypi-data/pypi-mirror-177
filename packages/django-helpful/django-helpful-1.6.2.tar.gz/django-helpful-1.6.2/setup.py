438adc61b4e5d56c2030083e2ebe2adf669e48bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:19 +0000

    Add package django-helpful/1.6.2/django-helpful-1.6.2.tar.gz

---
+++ packages/django-helpful/django-helpful-1.6.2.tar.gz/django-helpful-1.6.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+# python setup.py register
+# python setup.py sdist upload
+# python setup.py sdist bdist_wheel
+# twine upload dist/*.whl
+
+# python3 -m pip install --user --upgrade setuptools wheel
+
+# python3 -m pip install --upgrade twine
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository pipy dist/*
+# rm -r build/
+# rm -r dist/
+# rm -r django_helpful.egg-info
+
+
+setup(
+    name="django-helpful",
+    version="1.6.2",
+    description="Django helpful things",
+    long_description=open("README.rst").read(),
+    author="Stanislav Baltrunas",
+    author_email="stanislav@baltrunas.ru",
+    license="BSD",
+    url="https://github.com/Baltrunas/django-helpful",
+    # packages=find_packages(exclude=["tests.*", "tests"]),
+    packages=["helpful",],
+    package_data={"templates": ["*"], "static": ["*"], "locale": ["*"], "docs": ["*"],},
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=2.6, <4",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    zip_safe=False,
+)
