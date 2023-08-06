422054fe41999ae3aac4485f3f6311ac86ae22e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:19 +0000

    Add package mitchiCalpackage/0.0.6/mitchiCalpackage-0.0.6-py3-none-any.whl

---
+++ packages/mitchiCalpackage/mitchiCalpackage-0.0.6-py3-none-any.whl/mitchiCalpackage/mitchiCalPackage_example.py
@@ -0,0 +1,8 @@
+x = 5
+y = 10
+
+print("Adding", x, "and", y, ":", add(x,y))
+print("Subtracting", x, "and", y, ":", subtract(x,y))
+print("Multiplying", x, "and", y, ":", multi(x,y))
+print("Dividing", x, "and", y, ":", divide(x,y))
+print("Finding", x, "to the power of", y, ":", power(x,y))
