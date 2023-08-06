7de450e56eb21ed93f4e790b5223adf6ac2cd41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:30 +0000

    Add package zxl/1.0/zxl-1.0.tar.gz

---
+++ packages/zxl/zxl-1.0.tar.gz/zxl-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='zxl',
+    version='v1.0',
+    description='for love',
+    py_modules=['zxl'],
+    author='upuuuuuu',
+    author_email='2226548059@qq.com',
+    url='https://github.com/upuuuuuu',
+    license='MIT'
+)
