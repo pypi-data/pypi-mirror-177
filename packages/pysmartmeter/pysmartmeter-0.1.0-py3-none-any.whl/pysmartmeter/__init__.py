bd5e8b2384ba9d252fa6f2a0be7bea1361b30303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:28 +0000

    Add package pysmartmeter/0.1.0/pysmartmeter-0.1.0-py3-none-any.whl

---
+++ packages/pysmartmeter/pysmartmeter-0.1.0-py3-none-any.whl/pysmartmeter/__init__.py
@@ -0,0 +1,7 @@
+"""pysmartmeter - Collect data from Hitchi Smartmeter and expose it via MQTT"""
+
+from importlib.metadata import version
+
+
+__version__ = version('pysmartmeter')
+__author__ = 'Jens Diemer <pysmartmeter@jensdiemer.de>'
