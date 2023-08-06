ca64d0110881c8788d43dd1e99e94957ba56d4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:12 +0000

    Add package decorworld-database-model/0.2.8.2/decorworld_database_model-0.2.8.2-py3-none-any.whl

---
+++ packages/decorworld-database-model/decorworld_database_model-0.2.8.2-py3-none-any.whl/decorworld_database_model/tables/address.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from marshmallow import fields
+from marshmallow import Schema
+
+from .base import Base
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    zip_code = Column(String(4), nullable=False)
+    city = Column(String(25), nullable=False)
+    address = Column(String(25), nullable=False)
+    description = Column(String(50), nullable=True)
+
+
+class AddressSchema(Schema):
+
+    model_class = Address
+
+    id = fields.Integer()
+    user_id = fields.Integer(load_only=True)
+    zip_code = fields.String(data_key='zipCode')
+    city = fields.String()
+    address = fields.String()
+    description = fields.String(allow_none=True)
