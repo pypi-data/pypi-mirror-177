013c31b2fd6c6318b8e2478c9ac611df77b8bb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:18 +0000

    Add package General-distributions-vic-xav/0.1.1.2/General_distributions_vic_xav-0.1.1.2.tar.gz

---
+++ packages/General-distributions-vic-xav/General_distributions_vic_xav-0.1.1.2.tar.gz/General_distributions_vic_xav-0.1.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='General_distributions_vic_xav',
+      version='0.1.1.2',
+      description='Gaussian distributions',
+      packages=['General_distributions_vic_xav'],
+      author='Victor Rivera',
+      author_email='victorjavier0716@gmail.com',
+      zip_safe=False)
