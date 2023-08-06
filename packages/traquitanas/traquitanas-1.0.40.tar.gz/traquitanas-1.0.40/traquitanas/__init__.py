f5595752ca4f3914ebb87a9a00dac4ca67eb277c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:37 +0000

    Add package traquitanas/1.0.40/traquitanas-1.0.40.tar.gz

---
+++ packages/traquitanas/traquitanas-1.0.40.tar.gz/traquitanas-1.0.40/traquitanas/__init__.py
@@ -0,0 +1,10 @@
+"""
+_summary_
+
+https://stackoverflow.com/questions/4902974/python-import-subpackage-from-a-package-not-working
+
+"""
+
+from traquitanas.net import downloads, scraping
+
+__all__ = ['downloads', 'scraping']  # Net
