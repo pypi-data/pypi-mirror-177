1247b73c695b4da16cad82651adbb4d4e30b426c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:12 +0000

    Add package cartes/0.7.3/cartes-0.7.3-py3-none-any.whl

---
+++ packages/cartes/cartes-0.7.3-py3-none-any.whl/cartes/__init__.py
@@ -0,0 +1,14 @@
+import warnings
+
+import importlib_metadata
+from tqdm import TqdmExperimentalWarning
+
+# Monkey-patches GeoAxesSubplot for .set_extent()
+# Monkey-patches GeoDataFrame for .extent()
+from .utils import geoaxes  # noqa: F401
+
+__version__ = importlib_metadata.version("cartes")
+
+
+# Silence this warning about autonotebook mode for tqdm
+warnings.simplefilter("ignore", TqdmExperimentalWarning)
