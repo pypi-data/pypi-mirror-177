3235bd6af3044925ada7321918600b912520605e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:06 +0000

    Add package zigpy/0.51.6/zigpy-0.51.6.tar.gz

---
+++ packages/zigpy/zigpy-0.51.6.tar.gz/zigpy-0.51.6/setup.py
@@ -0,0 +1,34 @@
+"""Setup module for zigpy"""
+
+import pathlib
+
+from setuptools import find_packages, setup
+
+import zigpy
+
+REQUIRES = [
+    "aiohttp",
+    "aiosqlite>=0.16.0",
+    "crccheck",
+    "cryptography",
+    "voluptuous",
+    'pyserial-asyncio; platform_system!="Windows"',
+    'pyserial-asyncio!=0.5; platform_system=="Windows"',
+    "yarl",
+]
+
+setup(
+    name="zigpy",
+    version=zigpy.__version__,
+    description="Library implementing a ZigBee stack",
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zigpy/zigpy",
+    author="Russell Cloran",
+    author_email="rcloran@gmail.com",
+    license="GPL-3.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=REQUIRES,
+    python_requires=">=3.7",
+    package_data={"": ["appdb_schemas/schema_v*.sql"]},
+)
