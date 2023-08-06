699b906a4400bdbadc427f36b9f0e1b0a29e3008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:43 +0000

    Add package flake8-logging-format/0.9.0/flake8-logging-format-0.9.0.tar.gz

---
+++ packages/flake8-logging-format/flake8-logging-format-0.9.0.tar.gz/flake8-logging-format-0.9.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+project = "flake8-logging-format"
+version = "0.9.0"
+long_description = open("README.md").read()
+
+setup(
+    name=project,
+    version=version,
+    author="Globality Engineering",
+    author_email="engineering@globality.com",
+    url="https://github.com/globality-corp/flake8-logging-format",
+    license="Apache License 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    keywords="microcosm",
+    install_requires=[
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "PyHamcrest",
+        ],
+        "lint": [
+            "flake8",
+        ]
+    },
+    dependency_links=[
+    ],
+    entry_points={
+        "flake8.extension": [
+            "G = logging_format.api:LoggingFormatValidator",
+        ],
+        "logging.extra.example": [
+            "example = logging_format.whitelist:example_whitelist",
+        ],
+    },
+    classifiers=[
+        "Framework :: Flake8",
+    ],
+)
