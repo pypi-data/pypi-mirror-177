935a3f23e492a887a807525de38af3d532ae9263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:18 +0000

    Add package mufid/3.0/mufid-3.0.tar.gz

---
+++ packages/mufid/mufid-3.0.tar.gz/mufid-3.0/src/mufid/load_json.py
@@ -0,0 +1,25 @@
+from jsonref import load
+from os import getcwd
+from pathlib import Path
+from jsonref import JsonRef
+
+get_base_uri = lambda: Path(getcwd()).absolute().as_uri()
+
+def resolve(dictionary):
+	keys = list(dictionary.keys())
+	for key in keys:
+		print(key)
+		if type(dictionary[key]) == JsonRef:
+			dictionary[key] = dictionary[key].__subject__
+		if type(dictionary[key]) == dict:
+			dictionary[key] = resolve(dictionary[key])
+	return dictionary
+
+def f(path, base_uri=None):
+	base_uri = (
+		get_base_uri() 
+		if base_uri is None 
+		else base_uri
+	)
+	data = load(open(path), base_uri)
+	return resolve(data)
