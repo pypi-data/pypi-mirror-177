5658d5ad38b835fc6574c252c670f02b219cf225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:07 +0000

    Add package SynackAPI/0.4.0/SynackAPI-0.4.0.tar.gz

---
+++ packages/SynackAPI/SynackAPI-0.4.0.tar.gz/SynackAPI-0.4.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.9
+
+import setuptools
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="SynackAPI",
+    version="0.4.0",
+    author="bamhm182",
+    author_email="bamhm182@gmail.com",
+    description="A package to interact with Synack's API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/bamhm182/synackAPI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6",
+    packages=['synack', 'synack.plugins', 'synack.db'],
+    package_data={
+        'synack.db': ['alembic.ini', 'alembic/*', 'alembic/**/*', 'models/*'],
+    },
+    package_dir={'': 'src'},
+    install_requires=[
+        "netaddr==0.8.0",
+        "pathlib2==2.3.5",
+        "psycopg2-binary==2.9.1",
+        "pyaml==21.8.3",
+        "pyotp==2.6.0",
+        "requests==2.25.1",
+        "SQLAlchemy==1.4.31",
+        "alembic==1.7.5",
+        "urllib3==1.26.3",
+    ]
+)
