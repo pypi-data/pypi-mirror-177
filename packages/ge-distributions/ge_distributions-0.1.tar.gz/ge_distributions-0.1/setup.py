2ee2f17adbfb0d8f3b00f8f26aadcf59f14d23b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:02 +0000

    Add package ge-distributions/0.1/ge_distributions-0.1.tar.gz

---
+++ packages/ge-distributions/ge_distributions-0.1.tar.gz/ge_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ge_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ge_distributions'],
+      zip_safe=False)
