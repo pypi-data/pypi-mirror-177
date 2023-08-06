680f1285340b2a690bf31b8c402a5cedac23029c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:16 +0000

    Add package vstools/1.6.0/vstools-1.6.0-py3-none-any.whl

---
+++ packages/vstools/vstools-1.6.0-py3-none-any.whl/vstools/functions/funcs.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from typing import Callable, Concatenate, overload
+
+from ..exceptions import CustomRuntimeError
+from ..types import P, R, T, MissingT, MISSING, KwargsT
+
+__all__ = [
+    'iterate', 'fallback', 'kwargs_fallback'
+]
+
+
+def iterate(
+    base: T, function: Callable[Concatenate[T | R, P], T | R],
+    count: int, *args: P.args, **kwargs: P.kwargs
+) -> T | R:
+    if count <= 0:
+        return base
+
+    result: T | R = base
+
+    for _ in range(count):
+        result = function(result, *args, **kwargs)
+
+    return result
+
+
+fallback_missing = object()
+
+
+@overload
+def fallback(value: T | None, fallback: T) -> T:
+    ...
+
+
+@overload
+def fallback(value: T | None, fallback0: T | None, default: T) -> T:
+    ...
+
+
+@overload
+def fallback(value: T | None, fallback0: T | None, fallback1: T | None, default: T) -> T:
+    ...
+
+
+@overload
+def fallback(value: T | None, *fallbacks: T | None) -> T | MissingT:
+    ...
+
+
+@overload
+def fallback(value: T | None, *fallbacks: T | None, default: T) -> T:
+    ...
+
+
+def fallback(value: T | None, *fallbacks: T | None, default: T = fallback_missing) -> T | MissingT:  # type: ignore
+    if value is not None:
+        return value
+
+    for fallback in fallbacks:
+        if fallback is not None:
+            return fallback
+
+    if default is not fallback_missing:
+        return default
+    elif len(fallbacks) > 3:
+        return MISSING
+
+    raise CustomRuntimeError('You need to specify a default/fallback value!')
+
+
+@overload
+def kwargs_fallback(
+    input_value: T | None, kwargs: tuple[KwargsT, str], fallback: T
+) -> T:
+    ...
+
+
+@overload
+def kwargs_fallback(
+    input_value: T | None, kwargs: tuple[KwargsT, str], fallback0: T | None, default: T
+) -> T:
+    ...
+
+
+@overload
+def kwargs_fallback(
+    input_value: T | None, kwargs: tuple[KwargsT, str], fallback0: T | None, fallback1: T | None,
+    default: T
+) -> T:
+    ...
+
+
+@overload
+def kwargs_fallback(
+    input_value: T | None, kwargs: tuple[KwargsT, str], *fallbacks: T | None
+) -> T | MissingT:
+    ...
+
+
+@overload
+def kwargs_fallback(
+    input_value: T | None, kwargs: tuple[KwargsT, str], *fallbacks: T | None, default: T
+) -> T:
+    ...
+
+
+def kwargs_fallback(  # type: ignore
+    value: T | None, kwargs: tuple[KwargsT, str], *fallbacks: T | None, default: T = fallback_missing  # type: ignore
+) -> T | MissingT:
+    return fallback(value, kwargs[0].get(kwargs[1], None), *fallbacks, default=default)
