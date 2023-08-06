38e1dce602d7170c28308d58bcbe1a3ca7a115ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:16 +0000

    Add package s3pathlib/1.2.1/s3pathlib-1.2.1-py2.py3-none-any.whl

---
+++ packages/s3pathlib/s3pathlib-1.2.1-py2.py3-none-any.whl/s3pathlib/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Objective Oriented Interface for AWS S3, similar to pathlib.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Objective Oriented Interface for AWS S3, similar to pathlib."
+__license__ = "Apache License 2.0"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "sanhehu@amazon.com"
+__github_username__ = "aws-samples"
+
+try:
+    from . import utils
+    from .aws import context
+    from .core import S3Path
+except ImportError:  # pragma: no cover
+    pass
+except:  # pragma: no cover
+    raise
