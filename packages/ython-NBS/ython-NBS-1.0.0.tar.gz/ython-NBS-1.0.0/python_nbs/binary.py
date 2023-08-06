1e65c6c51e54e804f8f7e1f4ca1393b17b3913d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package ython-NBS/1.0.0/ython-NBS-1.0.0.tar.gz

---
+++ packages/ython-NBS/ython-NBS-1.0.0.tar.gz/ython-NBS-1.0.0/python_nbs/binary.py
@@ -0,0 +1,20 @@
+import webbrowser
+
+class _Binary:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class File(io.FileIO, _Binary):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Buffer(io.BytesIO, _Binary):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+    @classmethod
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+    @classmethod
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Wrapper(_Binary):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
