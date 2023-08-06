0e9428a106b905723703d0cbadb9772e3e958288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:36 +0000

    Add package cwe2/2.0.0/cwe2-2.0.0.tar.gz

---
+++ packages/cwe2/cwe2-2.0.0.tar.gz/cwe2-2.0.0/src/cwe2/database.py
@@ -0,0 +1,99 @@
+#
+# Copyright (c) Julian-Nash, Ziad Hany, nexB. Inc. and others. All rights reserved.
+# SPDX-License-Identifier: MIT
+# See https://github.com/nexB/cwe2/blob/main/mit.LICENSE for the license text.
+# See https://github.com/nexB/cwe2 for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+import csv
+import os
+from typing import List
+from typing import Union
+
+from cwe2.categories import CWECategory
+from cwe2.mappings import cwe_src_mapping
+from cwe2.mappings import external_mapping
+from cwe2.weakness import Weakness
+
+
+class Database:
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    software_development_db = open(
+        os.path.join(base_path, cwe_src_mapping["software_development"]["csv_file"])
+    )
+    hardware_design_db = open(
+        os.path.join(base_path, cwe_src_mapping["hardware_design"]["csv_file"])
+    )
+    research_concepts_db = open(
+        os.path.join(base_path, cwe_src_mapping["research_concepts"]["csv_file"])
+    )
+    cwe_top_25_2022_db = open(
+        os.path.join(base_path, external_mapping["CWE_top_25_2022"]["csv_file"])
+    )
+    owasp_top_ten_2021_db = open(
+        os.path.join(base_path, external_mapping["OWASP_top_ten_2021"]["csv_file"])
+    )
+
+    def get(self, cwe_id: Union[int, str], category: str = None) -> Weakness:
+        """Returns a CWE Weakness object"""
+        cwe_obj = None
+        cwe_category_mapper = {
+            CWECategory.SOFTWARE_DEVELOPMENT: self.software_development_db,
+            CWECategory.HARDWARE_DESIGN: self.hardware_design_db,
+            CWECategory.RESEARCH_CONCEPTS: self.research_concepts_db,
+        }
+
+        for cwe_category in (
+            {cwe_category_mapper[category]} if category else cwe_category_mapper.values()
+        ):
+            cwe_category.seek(0)
+            reader = csv.DictReader(cwe_category)
+            for row in reader:
+                if row.get("CWE-ID") == str(cwe_id):
+                    cwe_obj = list(row.values())[0:-1]
+                    break
+
+        if not cwe_obj:
+            raise Exception(f"Invalid CWE ID {cwe_id} - {category or ''}")
+
+        return Weakness(*cwe_obj)
+
+    def get_top_25_cwe(self) -> List[Weakness]:
+        """Returns a list of all CWE Top 25 (2022) Weakness objects"""
+        self.cwe_top_25_2022_db.seek(0)
+        weakness_list = []
+        reader = csv.DictReader(self.cwe_top_25_2022_db)
+        for row in reader:
+            weakness_list.append(Weakness(*list(row.values())[0:-1]))
+        return weakness_list
+
+    def get_owasp_top_ten_2021(self) -> List[Weakness]:
+        """Returns a list of all OWASP Top Ten (2021) Weakness objects"""
+        self.owasp_top_ten_2021_db.seek(0)
+        weakness_list = []
+        reader = csv.DictReader(self.owasp_top_ten_2021_db)
+        for row in reader:
+            weakness_list.append(Weakness(*list(row.values())[0:-1]))
+        return weakness_list
+
+    def is_cwe_top_25(self, cwe_id: Union[int, str]) -> bool:
+        """Returns True if Weakness object in a Top 25 CWE else False"""
+        self.cwe_top_25_2022_db.seek(0)
+        reader = csv.DictReader(self.cwe_top_25_2022_db)
+        for row in reader:
+            if row.get("CWE-ID") == str(cwe_id):
+                return True
+        return False
+
+    def is_owasp_top_ten_2021(self, cwe_id: Union[int, str]) -> bool:
+        """Returns True if Weakness object in a Top OWASP Ten (2021) else False"""
+        self.owasp_top_ten_2021_db.seek(0)
+        reader = csv.DictReader(self.owasp_top_ten_2021_db)
+        for row in reader:
+            if row.get("CWE-ID") == str(cwe_id):
+                return True
+        return False
+
+
+database = Database()
---
+++ packages/cwe2/cwe2-2.0.0.tar.gz/cwe2-2.0.0/tests/test_cwe.py
@@ -0,0 +1,106 @@
+#
+# Copyright (c) Julian-Nash, Ziad Hany, nexB. Inc. and others. All rights reserved.
+# SPDX-License-Identifier: MIT
+# See https://github.com/nexB/cwe2/blob/main/mit.LICENSE for the license text.
+# See https://github.com/nexB/cwe2 for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+from unittest import TestCase
+
+from cwe2.categories import CWECategory
+from cwe2.database import Database
+from cwe2.weakness import Weakness
+
+
+class TestDatabase(TestCase):
+    def setUp(self):
+        self.db = Database()
+
+    def test_get_cwe_by_category(self):
+        cwe = self.db.get(441, CWECategory.HARDWARE_DESIGN)
+        self.assertEqual(cwe.cwe_id, "441")
+        self.assertEqual(cwe.name, "Unintended Proxy or Intermediary ('Confused Deputy')")
+
+    def test_cwe_get(self):
+        cwe = self.db.get(15)
+        self.assertEqual(cwe.cwe_id, "15")
+        self.assertEqual(cwe.name, "External Control of System or Configuration Setting")
+        self.assertEqual(cwe.weakness_abstraction, "Base")
+        self.assertEqual(cwe.status, "Incomplete")
+        self.assertEqual(
+            cwe.description,
+            "One or more system settings or configuration elements can be externally "
+            "controlled by a user.",
+        )
+        self.assertEqual(
+            cwe.extended_description,
+            "Allowing external control of system settings can disrupt service "
+            "or cause an application to behave in unexpected, and potentially "
+            "malicious ways.",
+        )
+
+        self.assertEqual(
+            cwe.related_weaknesses,
+            "::NATURE:ChildOf:CWE ID:642:VIEW "
+            "ID:1000:ORDINAL:Primary::NATURE:ChildOf:CWE ID:610:VIEW "
+            "ID:1000::NATURE:ChildOf:CWE ID:20:VIEW ID:700:ORDINAL:Primary::",
+        )
+        self.assertEqual(cwe.related_attack_patterns, "::13::146::176::203::270::271::69::76::77::")
+
+        self.assertEqual(
+            cwe.potential_mitigations,
+            "::PHASE:Architecture and Design:STRATEGY:Separation of "
+            "Privilege:DESCRIPTION:Compartmentalize the system to have safe "
+            "areas where trust boundaries can be unambiguously drawn. Do not "
+            "allow sensitive data to go outside of the trust boundary and "
+            "always be careful when interfacing with a compartment outside of "
+            "the safe area. Ensure that appropriate compartmentalization is "
+            "built into the system design, and the compartmentalization "
+            "allows for and reinforces privilege separation functionality. "
+            "Architects and designers should rely on the principle of least "
+            "privilege to decide the appropriate time to use privileges and "
+            "the time to drop privileges.::PHASE:Implementation Architecture "
+            "and Design:DESCRIPTION:Because setting manipulation covers a "
+            "diverse set of functions, any attempt at illustrating it will "
+            "inevitably be incomplete. Rather than searching for a tight-knit "
+            "relationship between the functions addressed in the setting "
+            "manipulation category, take a step back and consider the sorts "
+            "of system values that an attacker should not be allowed to "
+            "control.::PHASE:Implementation Architecture and "
+            "Design:DESCRIPTION:In general, do not allow user-provided or "
+            "otherwise untrusted data to control sensitive values. The "
+            "leverage that an attacker gains by controlling these values is "
+            "not always immediately obvious, but do not underestimate the "
+            "creativity of the attacker.::",
+        )
+        self.assertEqual(
+            cwe.taxonomy_mappings,
+            "::TAXONOMY NAME:7 Pernicious Kingdoms:ENTRY NAME:Setting "
+            "Manipulation::TAXONOMY NAME:Software Fault Patterns:ENTRY "
+            "ID:SFP25:ENTRY NAME:Tainted input to variable::",
+        )
+
+    def test_cwe_error(self):
+        with self.assertRaises(Exception):
+            self.db.get(1000000)
+
+    def test_is_top_25_cwe(self):
+        self.assertTrue(self.db.is_cwe_top_25(20))
+        self.assertTrue(self.db.is_cwe_top_25("20"))
+        self.assertFalse(self.db.is_cwe_top_25(0))
+        self.assertFalse(self.db.is_cwe_top_25("0"))
+
+    def test_is_owasp_top_ten_2021(self):
+        self.assertTrue(self.db.is_owasp_top_ten_2021(11))
+        self.assertTrue(self.db.is_owasp_top_ten_2021("11"))
+        self.assertFalse(self.db.is_owasp_top_ten_2021(0))
+        self.assertFalse(self.db.is_cwe_top_25("0"))
+
+    def test_get_top_25_cwe(self):
+        self.assertEqual(len(self.db.get_top_25_cwe()), 25)
+        self.assertTrue(isinstance(self.db.get_top_25_cwe()[0], Weakness))
+
+    def test_get_owasp_top_ten_2021(self):
+        self.assertEqual(len(self.db.get_owasp_top_ten_2021()), 182)
+        self.assertTrue(isinstance(self.db.get_owasp_top_ten_2021()[0], Weakness))
