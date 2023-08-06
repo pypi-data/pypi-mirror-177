77bc13f3b2b7a302e629070b0722e9b70f106f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:12 +0000

    Add package cdds/1.0.1/cdds-1.0.1-py2.py3-none-any.whl

---
+++ packages/cdds/cdds-1.0.1-py2.py3-none-any.whl/cdds/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for Coding Dojo Data Science."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@gmail.com'
+__version__ = '1.0.1'
+
+# from cdds import datasets
+# # from cdds.inspect import check_package_versions
+# from cdds import lp_functions as lp
+# from cdds import inspect
+# from cdds import filetools
+# from cdds import utils
+
+from cdds.inspect import ihelp 
