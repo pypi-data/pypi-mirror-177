40facf89835018187f8fc5fa34bfdedb10bfc893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:15 +0000

    Add package oundofgothic/0.1/oundofgothic-0.1-py3-none-any.whl

---
+++ packages/oundofgothic/oundofgothic-0.1-py3-none-any.whl/soundofgothic/Dialogs/__init__.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+class Find:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class URL:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class File:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
