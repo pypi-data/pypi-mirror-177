1562fc06ceea57a5efeb49ef9ef30a7fa298cdd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:30 +0000

    Add package popcatapiwrapper/0.0.7/PopCatAPIWrapper-0.0.7.tar.gz

---
+++ packages/popcatapiwrapper/PopCatAPIWrapper-0.0.7.tar.gz/PopCatAPIWrapper-0.0.7/PopCatAPIWrapper/errors.py
@@ -0,0 +1,102 @@
+__all__ = [
+    "ColorNotFound",
+    "SteamAppNotFound",
+    "GenericError",
+    "NotValid",
+    "SongNotFound",
+    "FilmNotFound",
+    "ElementNotFound",
+]
+
+
+class GenericError(Exception):
+    """
+    Exception raised during a generic error
+
+    Attributes
+    ----------
+    msg: :class:`str`
+        the error message
+    """
+
+    def __init__(self, msg):
+        self.error = msg
+        super().__init__(f"Encountered a generic error. {self.error}")
+
+
+class NotValid(Exception):
+    """
+    Exception raised when the argument(s) given are invalid.
+    """
+
+    def __init__(self):
+        super().__init__("The argument(s) given are invalid.")
+
+
+class ElementNotFound(Exception):
+    """
+    Exception raised when the element is not found.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        name of the element that was not found
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+        super().__init__(f"Element with the name {self.name} was not found")
+
+
+class SongNotFound(Exception):
+    """
+    Exception raised when the song is not found.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        name of the song that was not found
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+        super().__init__(f"Song with the name {self.name} was not found")
+
+
+class FilmNotFound(Exception):
+    """
+    Exception raised when the film is not found.
+
+    Attributes
+    ----------
+    title: :class:`str`
+        title of the film that was not found
+    """
+
+    def __init__(self, title: str):
+        self.title = title
+        super().__init__(f"A film with the name {self.title} was not found")
+
+
+class SteamAppNotFound(Exception):
+    """
+    Exception raised when the steam application is not found.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        name of the steam application that was not found
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+        super().__init__(f"A steam application with the name {self.name} was not found")
+
+
+class ColorNotFound(Exception):
+    """
+    Exception raised when the color is not found.
+    """
+
+    def __init__(self):
+        super().__init__(f"Color could not be found, make sure you are typing it correctly (refer documentation)")
---
+++ packages/popcatapiwrapper/PopCatAPIWrapper-0.0.7.tar.gz/PopCatAPIWrapper-0.0.7/PopCatAPIWrapper/objects/film.py
@@ -0,0 +1,153 @@
+__all__ = ("Film",)
+
+
+class Film:
+    def __init__(self, res):
+        self.res = res
+
+    @property
+    def ratings(self):
+        """
+        Returns a list of dictionaries with the source and the ratings of the movie
+        """
+        return self.res["ratings"]
+
+    @property
+    def title(self) -> str:
+        """
+        Returns the title of the movie
+        """
+        return self.res["title"]
+
+    @property
+    def year(self) -> int:
+        """
+        Returns the release year of the movie
+        """
+        return self.res["year"]
+
+    @property
+    def rated(self) -> str:
+        """
+        Returns the PG rating of the movie
+        """
+        return self.res["rated"]
+
+    @property
+    def runtime(self) -> str:
+        """
+        Returns the runtime of the movie
+        """
+        return self.res["runtime"]
+
+    @property
+    def genres(self) -> str:
+        """
+        Returns the genres of the movie
+        """
+        return self.res["genres"]
+
+    @property
+    def director(self) -> str:
+        """
+        Returns the director of the movie
+        """
+        return self.res["director"]
+
+    @property
+    def writer(self) -> str:
+        """
+        Returns the writer of the movie
+        """
+        return self.res["writer"]
+
+    @property
+    def actors(self) -> str:
+        """
+        Returns the actors of the movie
+        """
+        return self.res["actors"]
+
+    @property
+    def plot(self) -> str:
+        """
+        Returns the brief plot of the movie
+        """
+        return self.res["plot"]
+
+    @property
+    def languages(self) -> str:
+        """
+        Returns the languages of the movie
+        """
+        return self.res["languages"]
+
+    @property
+    def country(self) -> str:
+        """
+        Returns the country the movie was shot in
+        """
+        return self.res["country"]
+
+    @property
+    def awards(self) -> str:
+        """
+        Returns the awards given to the movie
+        """
+        return self.res["awards"]
+
+    @property
+    def poster(self) -> str:
+        """
+        Returns the URL of the poster of the movie
+        """
+        return self.res["poster"]
+
+    @property
+    def metascore(self) -> str:
+        """
+        Returns the metascore of the movie
+        """
+        return self.res["metascore"]
+
+    @property
+    def votes(self) -> str:
+        """
+        Returns the number of votes received by the movie
+        """
+        return self.res["votes"]
+
+    @property
+    def imdb_id(self) -> str:
+        """
+        Returns the IMDB ID of the movie
+        """
+        return self.res["imdbid"]
+
+    @property
+    def type(self) -> str:
+        """
+        Returns the type of the movie
+        """
+        return self.res["type"]
+
+    @property
+    def box_office(self) -> str:
+        """
+        Returns the box office earnings of the movie
+        """
+        return self.res["boxoffice"]
+
+    @property
+    def is_series(self) -> bool:
+        """
+        Whether the movie is a series
+        """
+        return self.res["series"]
+
+    @property
+    def imdb_url(self) -> str:
+        """
+        Returns the URL of the movie on IMDB
+        """
+        return self.res["imdburl"]
