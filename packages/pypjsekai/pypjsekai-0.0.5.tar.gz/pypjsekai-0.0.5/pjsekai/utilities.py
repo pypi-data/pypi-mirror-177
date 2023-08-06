16f44beca691535a23b50e53e8d9ff19615ab6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:42 +0000

    Add package pypjsekai/0.0.5/pypjsekai-0.0.5.tar.gz

---
+++ packages/pypjsekai/pypjsekai-0.0.5.tar.gz/pypjsekai-0.0.5/pjsekai/utilities.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2022-present Erik Chan <erikchan002@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from typing import Iterator, Optional, List
+from msgpack import packb, unpackb
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+def msgpack(dict: Optional[dict]) -> bytes:
+    return b"" if dict is None else packb(dict) 
+
+def unmsgpack(data: bytes) -> dict:
+    return unpackb(data, strict_map_key=False) if len(data)>0 else None
+
+def encrypt(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    ciphertext: bytes = cipher.encrypt(pad(plaintext, 16))
+    return ciphertext
+
+def decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    plaintext: bytes = unpad(cipher.decrypt(ciphertext), 16)
+    return plaintext
+
+def deobfuscated(obfuscated_chunks: Iterator[List[bytes]]) -> Iterator:
+    return (chunk if chunk_index>0 else bytes([byte[0] if i>=128 or i%8>=5 else byte[0]^0xFF for i,byte in enumerate(chunk[4:])]) for chunk_index, chunk in enumerate(obfuscated_chunks))
+
+def obfuscated(chunks: Iterator[List[bytes]]) -> Iterator:
+    return (chunk if chunk_index>0 else bytes([0x10,0,0,0]+[byte[0] if i>=128 or i%8>=5 else byte[0]^0xFF for i,byte in enumerate(chunk)]) for chunk_index, chunk in enumerate(chunks))
