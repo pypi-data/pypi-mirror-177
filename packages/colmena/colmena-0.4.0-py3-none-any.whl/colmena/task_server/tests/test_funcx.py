2eda1f51138039c7ddd39e45268133386f33f610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:39 +0000

    Add package colmena/0.4.0/colmena-0.4.0-py3-none-any.whl

---
+++ packages/colmena/colmena-0.4.0-py3-none-any.whl/colmena/task_server/tests/test_funcx.py
@@ -0,0 +1,80 @@
+from concurrent.futures import Future
+from typing import Callable
+from time import sleep
+from uuid import uuid4
+
+from pytest_mock import MockFixture
+from pytest import fixture, mark
+
+from colmena.models import Result
+from colmena.queue.python import PipeQueues
+from colmena.task_server.funcx import FuncXTaskServer
+
+
+class FakeClient:
+    """Faked FuncXClient that allows you to register functions"""
+
+    def __init__(self):
+        self.my_funcs = {}
+
+    def register_function(self, new_func, function_name: str, description: str, searchable: bool):
+        uuid = uuid4()
+        self.my_funcs[uuid] = (new_func, function_name)
+        return uuid
+
+
+# TODO (wardlt): This mocked version does not mimic the real versions' multi-threaded nature, which
+#  means it does not suffer the same problems when being copied over to a subprocess
+class FakeExecutor:
+    """Faked FuncXExecutor that generates "futures" but does not communicate with FuncX"""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def submit(self, func: Callable, task: Result, endpoint_id: str):
+        new_future = Future()
+        result = func(task)
+        new_future.set_result(result)
+        return new_future
+
+    def shutdown(self):
+        pass
+
+
+@fixture()
+def mock_funcx(mocker: MockFixture):
+    mocker.patch('colmena.task_server.funcx.FuncXExecutor', FakeExecutor)
+
+
+@mark.timeout(10)
+def test_mocked_server(mock_funcx):
+    # Create the task server with a single, no-op function
+    client = FakeClient()
+    queues = PipeQueues()
+
+    def func(x):
+        if x is None:
+            raise MemoryError()
+        return x
+    fts = FuncXTaskServer({func: 'fake_endp'}, client, queues)
+    fts.start()
+
+    # Submit a task to the queue and see how it works
+    try:
+        # Send a task that will execute properly
+        queues.send_inputs(1, method='func')
+        sleep(1)
+        result = queues.get_result()
+        assert result.success
+        assert result.value == 1
+
+        # Send a task that will throw a memory error
+        queues.send_inputs(None, method='func')
+        sleep(1)
+        result = queues.get_result()
+        assert not result.success
+        assert 'MemoryError' in result.failure_info.exception
+
+    finally:
+        queues.send_kill_signal()
+        fts.join()
