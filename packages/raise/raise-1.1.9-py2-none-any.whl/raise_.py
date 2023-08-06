4c396f7453db31d7c3995f7c02efaefdad5e2f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:37 +0000

    Add package raise/1.1.9/raise-1.1.9-py2-none-any.whl

---
+++ packages/raise/raise-1.1.9-py2-none-any.whl/raise_.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: 0BSD
+# Copyright 2018 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+
+"""Raise exceptions with a function instead of a statement.
+
+Provides a minimal, clean and portable interface for raising exceptions
+with all the advantages of functions over syntax.
+"""
+
+__all__ = ('raise_',)
+__version__ = '1.1.9'
+
+
+def raise_(exception, traceback=None):
+    """Raise an exception, optionally with a custom traceback.
+
+    Arguments:
+        exception: The exception instance or type to raise.
+        traceback (optional): Traceback to raise the exception with.
+    """
+    try:
+        raise exception, None, traceback
+    finally:
+        exception = traceback = None
