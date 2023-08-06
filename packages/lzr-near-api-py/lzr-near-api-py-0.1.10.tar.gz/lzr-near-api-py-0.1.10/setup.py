095f3b90d0d99aae03e445a6edcc9917a37a11cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:49 +0000

    Add package lzr-near-api-py/0.1.10/lzr-near-api-py-0.1.10.tar.gz

---
+++ packages/lzr-near-api-py/lzr-near-api-py-0.1.10.tar.gz/lzr-near-api-py-0.1.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+META_DATA = dict(
+    name="lzr-near-api-py",
+    version="0.1.10",
+    license="MIT",
+
+    author="NEAR Inc",
+
+    url="https://github.com/near/near-api-py",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+
+    install_requires=["requests", "urllib3", "base58", "ed25519"]
+)
+
+if __name__ == "__main__":
+    setup(**META_DATA)
