c2fe7bcfa189ee50d95e0469b77233f6826a4217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:00 +0000

    Add package actinia-api/3.4.0/actinia-api-3.4.0.tar.gz

---
+++ packages/actinia-api/actinia-api-3.4.0.tar.gz/actinia-api-3.4.0/src/actinia_api/__init__.py
@@ -0,0 +1,13 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+API_VERSION = "v3"
+
+# This is the URL prefix that must be used in the tests
+URL_PREFIX = "/api/%s" % API_VERSION
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
