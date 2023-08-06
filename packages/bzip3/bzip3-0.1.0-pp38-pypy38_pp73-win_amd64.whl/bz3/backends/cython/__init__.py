511ed84d44b68550882883ea8a43fbba3f586474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:07 +0000

    Add package bzip3/0.1.0/bzip3-0.1.0-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/bzip3/bzip3-0.1.0-pp38-pypy38_pp73-win_amd64.whl/bz3/backends/cython/__init__.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+from bz3.backends.cython._bz3 import (
+    BZ3Compressor,
+    BZ3Decompressor,
+    bound,
+    compress_file,
+    compress_into,
+    decompress_file,
+    decompress_into,
+    libversion,
+    test_file,
+)
