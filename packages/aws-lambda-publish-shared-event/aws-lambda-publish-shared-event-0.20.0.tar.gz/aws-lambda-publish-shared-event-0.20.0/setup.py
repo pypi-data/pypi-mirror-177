305b78d729b799b284646f24407a7c8d7f518e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:04 +0000

    Add package aws-lambda-publish-shared-event/0.20.0/aws-lambda-publish-shared-event-0.20.0.tar.gz

---
+++ packages/aws-lambda-publish-shared-event/aws-lambda-publish-shared-event-0.20.0.tar.gz/aws-lambda-publish-shared-event-0.20.0/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="aws-lambda-publish-shared-event",
+    version="0.20.0",
+    entry_points={
+        "console_scripts": [
+            "publish-shared-event=aws_lambda_publish_shared_event.__main__:main",
+            "generate-test-event=aws_lambda_publish_shared_event.generate_test_event.__main__:main",
+        ],
+    },
+    author="Michael Brewer",
+    license="MIT",
+    url="https://lambda.101i.de/",
+    description="Cli to publish shareable Lambda test events.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=["aws_lambda_publish_shared_event",  "aws_lambda_publish_shared_event.generate_test_event"],
+    install_requires=[
+        "boto3 >= 1.26.15",
+        "botocore >= 1.29.15",
+        "pick >= 2.2.0",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    project_urls={
+        "GitHub": "https://github.com/michaelbrewer/aws-lambda-events",
+        "Documentation": "https://lambda.101i.de/",
+    },
+)
