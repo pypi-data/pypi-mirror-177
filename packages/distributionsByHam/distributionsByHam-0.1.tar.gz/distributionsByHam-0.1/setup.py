39ac9ee62fbe135c688ab8d4774045f864054a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:02 +0000

    Add package distributionsByHam/0.1/distributionsByHam-0.1.tar.gz

---
+++ packages/distributionsByHam/distributionsByHam-0.1.tar.gz/distributionsByHam-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsByHam',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsByHam'],
+	author_email = 'syedhamza097@gmail.com',
+      zip_safe=False)
