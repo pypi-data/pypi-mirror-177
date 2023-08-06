90f5163b07be816f6d5d90eae339fb578112470d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:20 +0000

    Add package clothes-sdk-ashuk203/1.0.0/clothes_sdk_ashuk203-1.0.0-py3-none-any.whl

---
+++ packages/clothes-sdk-ashuk203/clothes_sdk_ashuk203-1.0.0-py3-none-any.whl/clothes-sdk-ashuk203/apis/paths/tshirts_id.py
@@ -0,0 +1,11 @@
+from clothes-sdk-ashuk203.paths.tshirts_id.get import ApiForget
+from clothes-sdk-ashuk203.paths.tshirts_id.post import ApiForpost
+from clothes-sdk-ashuk203.paths.tshirts_id.delete import ApiFordelete
+
+
+class TshirtsId(
+    ApiForget,
+    ApiForpost,
+    ApiFordelete,
+):
+    pass
