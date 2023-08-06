ca495192ff601f5db5a97c79989b0e110a0d3ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:05 +0000

    Add package eurofiberpythonlib/0.1.0/eurofiberpythonlib-0.1.0.tar.gz

---
+++ packages/eurofiberpythonlib/eurofiberpythonlib-0.1.0.tar.gz/eurofiberpythonlib-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='eurofiberpythonlib',
+    packages=find_packages(include=['eurofiber_functions']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+)
