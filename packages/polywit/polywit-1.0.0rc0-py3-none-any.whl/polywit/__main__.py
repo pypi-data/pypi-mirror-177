59271484bd767f18b0c795c3a19ccfa13dc7b9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package polywit/1.0.0rc0/polywit-1.0.0rc0-py3-none-any.whl

---
+++ packages/polywit/polywit-1.0.0rc0-py3-none-any.whl/polywit/__main__.py
@@ -0,0 +1,98 @@
+"""
+ This file is part of polywit, a poly-language execution-based violation-witness validator
+ https://github.com/polywit/polywit.
+
+ This module deals with the main functionality of the tool
+"""
+
+import os
+import sys
+
+import argparse
+
+from polywit import __version__
+from polywit.java import JavaValidator
+
+
+def dir_path(path):
+    """
+    Checks if a path is a valid directory
+    :param path: Potential directory
+    :return: The original path if valid
+    """
+    if os.path.isdir(path):
+        return path
+    raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+
+
+def create_argument_parser() -> argparse.ArgumentParser:
+    """
+    Creates a parser for the command-line options.
+    @return: An argparse.ArgumentParser instance
+    """
+    parser = argparse.ArgumentParser(
+        description="""
+                   Validate a given program with a witness conforming to the appropriate SV-COMP
+                   exchange format.
+               """,
+    )
+
+    parser.add_argument(
+        "--version", action="version", version=f'polywit: v{__version__}'
+    )
+    subparsers = parser.add_subparsers(
+        metavar='frontend',
+        dest='language',
+        help='Frontend language'
+    )
+    java_sub_parser = subparsers.add_parser(
+        'java',
+        help='Use the java validator'
+    )
+
+    java_sub_parser.add_argument(
+        'benchmark',
+        type=dir_path,
+        help="Path to the benchmark directory"
+    )
+
+    java_sub_parser.add_argument(
+        '--packages',
+        dest='package_paths',
+        type=dir_path,
+        nargs='*',
+        help="Path to the packages used by the benchmark"
+    )
+
+    java_sub_parser.add_argument(
+        '--witness',
+        dest='witness_file',
+        required=True,
+        type=str,
+        action="store",
+        help='Path to the witness file. Must conform to the exchange format'
+    )
+
+    return parser
+
+
+def main():
+    parser = create_argument_parser()
+    config = parser.parse_args(sys.argv[1:])
+    config = vars(config)
+    try:
+        print(f'polywit: v{__version__}')
+        validator = JavaValidator(config)
+
+        validator.preprocess()
+        assumptions = validator.extract_assumptions()
+        outcome = validator.execute_test_harness(assumptions)
+        print(f'polywit: {outcome}')
+
+    except BaseException as err:
+        print(f'polywit: Could not validate witness \n{err}')
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/polywit/polywit-1.0.0rc0-py3-none-any.whl/polywit/base/file_processors.py
@@ -0,0 +1,126 @@
+"""
+ This file is part of polywit, a poly-language execution-based violation-witness validator
+ https://github.com/polywit/polywit.
+
+ This module deals with the base definitions for processing of the witness and compilation units
+"""
+
+from abc import ABC, abstractmethod
+import os
+import networkx as nx
+
+from polywit.types.aliases import Position
+
+
+class Processor(ABC):
+    """
+    An abstract class representing the base functionality for a processor
+    """
+
+    def __init__(self, test_directory):
+        self.test_directory = test_directory
+
+    @abstractmethod
+    def preprocess(self) -> None:
+        """
+        Stub for the preprocess method
+        """
+
+    def write_to_test_directory(self, path, data):
+        """
+        Writes some data to the working directory at some given path offset
+
+        :param path: The offset path from the working directory
+        :param data: The data to be written
+        :return: The new place the data was written to.
+        """
+        # Check if incoming path involves a new subdirectory
+        subdir = os.path.join(self.test_directory, os.path.dirname(path))
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+        new_path = os.path.join(self.test_directory, path)
+        with open(new_path, 'w', encoding='utf-8') as file:
+            file.write(data)
+
+        return new_path
+
+
+class FileProcessor(Processor):
+    """
+    An abstract class representing the base functionality for a file processor
+    """
+
+    def __init__(self, test_directory):
+        super().__init__(test_directory)
+
+    @abstractmethod
+    def extract_position_type_map(self) -> dict[Position, str]:
+        """
+        Stub for the extract nondet mappings method
+        """
+
+
+class WitnessProcessor(Processor):
+    """
+    A class representing the witness processor
+    """
+
+    def __init__(self, test_directory, witness_path):
+        super().__init__(test_directory)
+        self.producer = None
+        self.witness_path = witness_path
+        self.witness = None
+
+    def preprocess(self):
+        if self.witness is None:
+            try:
+                self.witness = nx.read_graphml(self.witness_path)
+            except Exception as exc:
+                raise ValueError(f'Witness file is not formatted correctly. \n {exc}') from exc
+        self.specification = self._get_value_from_witness('specification')
+        # Check witness type is a violation witness
+        witness_type = self._get_value_from_witness('witness-type')
+        if witness_type != 'violation_witness':
+            raise ValueError(f'No support for {witness_type}')
+        # Check witness is linear
+        self._check_witness_linearity()
+
+    def _check_witness_linearity(self):
+        """
+        Checks the witness is linear before building validator
+        """
+        entry_nodes = list(filter(
+            lambda nodes: nodes[1],
+            self.witness.nodes.data('isEntryNode', default=False)
+        ))
+        if len(entry_nodes) != 1:
+            raise ValueError('Witness does not have a single entry node')
+        self.entry_node = entry_nodes[0][0]
+        violation_nodes = list(filter(
+            lambda nodes: nodes[1],
+            self.witness.nodes.data('isViolationNode', default=False)
+        ))
+        if len(entry_nodes) != 1:
+            raise ValueError('Witness does not have a single violation node')
+        self.violation_node = violation_nodes[0][0]
+        if len(list(nx.all_simple_paths(self.witness, source=self.entry_node, target=self.violation_node))) > 1:
+            raise ValueError('Witness has multiple execution paths from source to sink')
+
+    def _get_value_from_witness(self, key):
+        return self.witness.graph[key] if key in self.witness.graph else None
+
+    @abstractmethod
+    def extract_assumptions(self):
+        """
+        Extracts the assumptions from the witness
+        """
+
+    @staticmethod
+    def _get_file_name_from_path(path):
+        """
+        Returns the file name
+        :param path: Path to file
+        :return: Base file name without the extension
+        """
+        base_name = os.path.basename(path)
+        return os.path.splitext(base_name)[0]
