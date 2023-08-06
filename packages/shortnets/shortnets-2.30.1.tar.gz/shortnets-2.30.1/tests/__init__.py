b1ec8c23920f56b9df47fe7423e2b4741a2d94b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:25 +0000

    Add package shortnets/2.30.1/shortnets-2.30.1.tar.gz

---
+++ packages/shortnets/shortnets-2.30.1.tar.gz/shortnets-2.30.1/tests/__init__.py
@@ -0,0 +1,10 @@
+"""shortnet test package initialisation."""
+
+import warnings
+
+from urllib3.exceptions import SNIMissingWarning
+
+# urllib3 sets SNIMissingWarning to only go off once,
+# while this test suite requires it to always fire
+# so that it occurs during test_shortnet.test_https_warnings
+warnings.simplefilter("always", SNIMissingWarning)
