cf5757a9f58cd2c0e3db1e0e6a27f0f3718c947c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:47 +0000

    Add package odoo14-addon-mitxelena/14.0.0.0.6/odoo14-addon-mitxelena-14.0.0.0.6.tar.gz

---
+++ packages/odoo14-addon-mitxelena/odoo14-addon-mitxelena-14.0.0.0.6.tar.gz/odoo14-addon-mitxelena-14.0.0.0.6/odoo/addons/mitxelena/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+  'name': "mitxelena",
+
+  'summary': """
+    Manufacturing customisation for Talleres mitxelena""",
+
+  'author': "Coopdevs Treball SCCL",
+  'website': "https://gitlab.com/coopdevs",
+
+  # Categories can be used to filter modules in modules listing
+  # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+  # for the full list
+  'category': 'manufacturing',
+  'version': '14.0.0.0.6',
+
+  # any module necessary for this one to work correctly
+  'depends': [
+    'base',
+    'sale',
+    'product_supplierinfo_for_customer',
+    'base_revision',
+    'mrp_parallel',
+    
+  ],
+
+  # always loaded
+  'data': [
+    'views/res_partner.xml',
+    'views/product_template.xml',
+    'views/sale_order_line.xml',
+    'views/engspecsheet.xml',
+    'views/sale_order.xml'
+  ]
+}
