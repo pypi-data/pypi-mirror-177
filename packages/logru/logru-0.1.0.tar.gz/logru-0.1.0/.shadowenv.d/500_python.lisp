538cca3527fd6a4a0a34aeac5b3698eb01c62cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:38 +0000

    Add package logru/0.1.0/logru-0.1.0.tar.gz

---
+++ packages/logru/logru-0.1.0.tar.gz/logru-0.1.0/.shadowenv.d/500_python.lisp
@@ -0,0 +1,4 @@
+(provide "python" "3.10.8")
+(env/set "WORKON_HOME" "/Users/william/.pyenv/versions/3.10.8/virtualenvs/logru")
+(env/set "VIRTUAL_ENV" "/Users/william/.pyenv/versions/3.10.8/virtualenvs/logru")
+(env/prepend-to-pathlist "PATH" (path-concat (env/get "WORKON_HOME") "bin"))
