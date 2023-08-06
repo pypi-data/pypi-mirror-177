39da0b94a30e3f8ac658e8b9549b87ce513d5bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:07 +0000

    Add package ahmed-laaziz/0.1/ahmed-laaziz-0.1.tar.gz

---
+++ packages/ahmed-laaziz/ahmed-laaziz-0.1.tar.gz/ahmed-laaziz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ahmed-laaziz',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ahmed-laaziz'],
+      zip_safe=False)
