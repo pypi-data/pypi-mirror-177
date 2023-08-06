958a03fe825831ec3dc1096445eb2978f54ee63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:42 +0000

    Add package some-tool-mbrochh/0.0.3/some_tool_mbrochh-0.0.3-py3-none-any.whl

---
+++ packages/some-tool-mbrochh/some_tool_mbrochh-0.0.3-py3-none-any.whl/some_tool/tests/test_some_tool.py
@@ -0,0 +1,26 @@
+"""Tests for some_tool."""
+import pytest
+
+from some_tool import some_tool
+
+
+def test_some_function_exceptions():
+    """Tests for exceptions of some_function()"""
+    with pytest.raises(some_tool.BadValue):
+        some_tool.some_function("", "asd")
+
+    with pytest.raises(some_tool.BadValue):
+        some_tool.some_function("asd", "")
+
+
+# fmt: off
+@pytest.mark.parametrize("val1,val2,exp", [
+    ("HELLO", "world", "helloWORLD"),
+    ("Hello", "World", "helloWORLD"),
+    ("hello", "world", "helloWORLD"),
+])
+# fmt: on
+def test_some_function(val1: str, val2: str, exp: str):
+    """Tests for some_function()"""
+    res = some_tool.some_function(val1, val2)
+    assert res == exp
