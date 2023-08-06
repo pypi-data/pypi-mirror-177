44a98308d26986779258388c3c20802c9e019b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:19 +0000

    Add package Luci-first-package/0.1.0/Luci-first-package-0.1.0.tar.gz

---
+++ packages/Luci-first-package/Luci-first-package-0.1.0.tar.gz/Luci-first-package-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Luci-first-package",
+    version="0.1.0",
+    author= "Lucian",
+    author_email="stardust0010@yahoo.com",
+    packages=["my_own_package"],
+    package_dir={"":"src\\"},
+    include_package_data=True,
+    description="This is my first package,get pwnd noobs."
+)
