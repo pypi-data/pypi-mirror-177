7058eabde969d9bef3875701a1b5c67936db5cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:04 +0000

    Add package highspy/1.4.0.dev0/highspy-1.4.0.dev0-cp39-cp39-win_amd64.whl

---
+++ packages/highspy/highspy-1.4.0.dev0-cp39-cp39-win_amd64.whl/highspy/__init__.py
@@ -0,0 +1,52 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_2():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'highspy.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        load_order_filepath = os.path.join(libs_dir, '.load-order-highspy-1.4.0.dev0')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-highspy-1.4.0.dev0')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_2()
+del _delvewheel_init_patch_1_1_2
+# end delvewheel patch
+
+from .highs import (
+    ObjSense,
+    MatrixFormat,
+    HessianFormat,
+    SolutionStatus,
+    BasisValidity,
+    HighsModelStatus,
+    HighsBasisStatus,
+    HighsVarType,
+    HighsStatus,
+    HighsLogType,
+    HighsSparseMatrix,
+    HighsLp,
+    HighsHessian,
+    HighsModel,
+    HighsSolution,
+    HighsBasis,
+    HighsInfo,
+    HighsOptions,
+    Highs,
+    kHighsInf,
+    HIGHS_VERSION_MAJOR,
+    HIGHS_VERSION_MINOR,
+    HIGHS_VERSION_PATCH,
+)
