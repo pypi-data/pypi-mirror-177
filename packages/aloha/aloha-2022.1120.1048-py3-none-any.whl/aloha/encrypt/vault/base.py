b9fed527ab003641782374246adaa4f5807b67c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package aloha/2022.1120.1048/aloha-2022.1120.1048-py3-none-any.whl

---
+++ packages/aloha/aloha-2022.1120.1048-py3-none-any.whl/aloha/encrypt/vault/base.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from urllib.parse import quote_plus as urlquote
+
+
+class BaseVault(ABC):
+    @abstractmethod
+    def decrypt_password(self, *args, **kwargs):
+        return kwargs.get('password')
+
+    def get_password(self, *args, **kwargs):
+        url_quote = kwargs.pop('url_quote', True)
+
+        pwd = self.decrypt_password(*args, **kwargs)
+        if pwd is None:
+            return None
+
+        if url_quote:
+            return urlquote(pwd)
+        else:
+            return pwd
+
+
+class DummyVault(BaseVault):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def decrypt_password(self, password):
+        return password
+
+
+def main():
+    vault = DummyVault()
+    ret = vault.get_password(None, url_quote=True)
+    # print(ret)
+    return ret
