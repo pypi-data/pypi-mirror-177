cfefb85571cfa3b5dc58549d5bba9c53be5ece03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:49 +0000

    Add package progress-table/0.0.9/progress-table-0.0.9.tar.gz

---
+++ packages/progress-table/progress-table-0.0.9.tar.gz/progress-table-0.0.9/progress_table/devel.py
@@ -0,0 +1,26 @@
+import random
+import time
+from colorama import Fore
+from progress_table import ProgressTable
+
+progress = ProgressTable(columns=["step", "x", "x squared"])
+progress.add_column("x root", color=Fore.RED)
+
+for step in range(20):
+    progress["step"] = step  # insert step value in the current row
+
+    for _ in progress(range(10)):  # display progress bar
+        time.sleep(0.1)  # simulate artificial work
+
+    x = random.randint(0, 200)
+    progress["x"] = x
+    progress["x squared"] = x ** 2
+    progress["x root"] = x ** 0.5
+    progress.next_row()
+
+progress.close()
+
+# export your data
+data = progress.to_list()
+pandas_df = progress.to_df()
+np_array = progress.to_numpy()
