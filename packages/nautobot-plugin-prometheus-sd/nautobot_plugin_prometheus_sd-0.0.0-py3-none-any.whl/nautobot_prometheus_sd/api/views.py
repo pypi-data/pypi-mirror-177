3fc70efe5a043e5c7015bb724dce8bb8af8c5f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:18 +0000

    Add package nautobot-plugin-prometheus-sd/0.0.0/nautobot_plugin_prometheus_sd-0.0.0-py3-none-any.whl

---
+++ packages/nautobot-plugin-prometheus-sd/nautobot_plugin_prometheus_sd-0.0.0-py3-none-any.whl/nautobot_prometheus_sd/api/views.py
@@ -0,0 +1,78 @@
+from nautobot.ipam.models import IPAddress
+from nautobot.virtualization.models import VirtualMachine
+from nautobot.dcim.models.devices import Device
+
+# The base ViewSet has been renamed, this try-except helps to support
+# Both < 3.2 and the newer 3.2+ Versions:
+# https://github.com/netbox-community/netbox/commit/bbdeae0ed9bcc06fb96ffa2970272e1a3447448c
+try:
+    from nautobot.core.api.viewsets import NetBoxModelViewSet
+except ImportError:
+    from nautobot.extras.api.views import CustomFieldModelViewSet as NetBoxModelViewSet
+
+# Filtersets have been renamed, we support both
+# https://github.com/netbox-community/netbox/commit/1024782b9e0abb48f6da65f8248741227d53dbed#diff-d9224204dab475bbe888868c02235b8ef10f07c9201c45c90804d395dc161c40
+# pylint: disable=ungrouped-imports
+try:
+    from nautobot.ipam.filtersets import IPAddressFilterSet
+    from nautobot.dcim.filtersets import DeviceFilterSet
+    from nautobot.virtualization.filtersets import VirtualMachineFilterSet
+except ImportError:
+    from nautobot.ipam.filters import IPAddressFilterSet
+    from nautobot.dcim.filters import DeviceFilterSet
+    from nautobot.virtualization.filters import VirtualMachineFilterSet
+# pylint: enable=ungrouped-imports
+
+
+from .serializers import (
+    PrometheusIPAddressSerializer,
+    PrometheusDeviceSerializer,
+    PrometheusVirtualMachineSerializer,
+)
+
+
+class VirtualMachineViewSet(
+    NetBoxModelViewSet
+):  # pylint: disable=too-many-ancestors
+    queryset = VirtualMachine.objects.prefetch_related(
+        "cluster__site",
+        "role",
+        "tenant",
+        "platform",
+        "primary_ip4",
+        "primary_ip6",
+        "tags",
+        "services",
+        "contacts",
+
+    )
+    filterset_class = VirtualMachineFilterSet
+    serializer_class = PrometheusVirtualMachineSerializer
+    pagination_class = None
+
+
+class DeviceViewSet(NetBoxModelViewSet):  # pylint: disable=too-many-ancestors
+    queryset = Device.objects.prefetch_related(
+        "device_type__manufacturer",
+        "device_role",
+        "tenant",
+        "platform",
+        "site",
+        "location",
+        "rack",
+        "parent_bay",
+        "virtual_chassis__master",
+        "primary_ip4__nat_outside",
+        "primary_ip6__nat_outside",
+        "tags",
+    )
+    filterset_class = DeviceFilterSet
+    serializer_class = PrometheusDeviceSerializer
+    pagination_class = None
+
+
+class IPAddressViewSet(NetBoxModelViewSet):  # pylint: disable=too-many-ancestors
+    queryset = IPAddress.objects.prefetch_related("tenant", "tags")
+    serializer_class = PrometheusIPAddressSerializer
+    filterset_class = IPAddressFilterSet
+    pagination_class = None
