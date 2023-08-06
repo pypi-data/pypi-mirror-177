1705e34cc368f476f362e000ae5800d07aa5084a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:16 +0000

    Add package live-trading-indicators/0.3.1/live_trading_indicators-0.3.1.tar.gz

---
+++ packages/live-trading-indicators/live_trading_indicators-0.3.1.tar.gz/live_trading_indicators-0.3.1/src/live_trading_indicators/indicators_set/MA.py
@@ -0,0 +1,24 @@
+"""MA(period=, value='close', ma_type='sma')"""
+from ..indicator_data import IndicatorData
+from ..move_average import ma_calculate, MA_Type
+
+
+def get_indicator_out(indicators, symbol, timeframe, out_for_grow, period, value='close', ma_type='sma'):
+
+    ma_type_enum = MA_Type.cast(ma_type)
+
+    ohlcv = indicators.OHLCV.full_data(symbol, timeframe)
+    source_values = ohlcv.data[value]
+
+    out = ma_calculate(source_values, period, ma_type_enum)
+
+    return IndicatorData({
+        'name': 'SMA',
+        'symbol': symbol,
+        'timeframe': timeframe,
+        'time': ohlcv.time,
+        'move_average': out,
+        'allowed_nan': True
+    })
+
+
