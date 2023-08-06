7d46285616278c748d5a0f92022aa2b170012a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:56 +0000

    Add package eth-ape/0.5.6/eth-ape-0.5.6.tar.gz

---
+++ packages/eth-ape/eth-ape-0.5.6.tar.gz/eth-ape-0.5.6/tests/integration/cli/test_accounts.py
@@ -0,0 +1,131 @@
+import json
+
+import pytest
+from eth_account import Account
+
+from tests.integration.cli.utils import assert_failure, run_once
+
+ALIAS = "test"
+PASSWORD = "a"
+PRIVATE_KEY = "0000000000000000000000000000000000000000000000000000000000000001"
+IMPORT_VALID_INPUT = "\n".join([f"0x{PRIVATE_KEY}", PASSWORD, PASSWORD])
+GENERATE_VALID_INPUT = "\n".join(["random entropy", PASSWORD, PASSWORD])
+
+
+@pytest.fixture(autouse=True)
+def temp_keyfile_path(temp_accounts_path):
+    test_keyfile_path = temp_accounts_path / f"{ALIAS}.json"
+
+    if test_keyfile_path.is_file():
+        # Corrupted from a previous test
+        test_keyfile_path.unlink()
+
+    return test_keyfile_path
+
+
+@pytest.fixture
+def temp_keyfile(temp_keyfile_path, keyparams):
+    temp_keyfile_path.write_text(json.dumps(keyparams))
+
+    yield temp_keyfile_path
+
+    if temp_keyfile_path.is_file():
+        temp_keyfile_path.unlink()
+
+
+@pytest.fixture
+def temp_account():
+    return Account.from_key(bytes.fromhex(PRIVATE_KEY))
+
+
+@run_once
+def test_import(ape_cli, runner, temp_account, temp_keyfile_path):
+    assert not temp_keyfile_path.is_file()
+    # Add account from private keys
+    result = runner.invoke(ape_cli, ["accounts", "import", ALIAS], input=IMPORT_VALID_INPUT)
+    assert result.exit_code == 0, result.output
+    assert temp_account.address in result.output
+    assert ALIAS in result.output
+    assert temp_keyfile_path.is_file()
+
+
+@run_once
+def test_import_alias_already_in_use(ape_cli, runner, temp_account):
+    def invoke_import():
+        return runner.invoke(ape_cli, ["accounts", "import", ALIAS], input=IMPORT_VALID_INPUT)
+
+    result = invoke_import()
+    assert result.exit_code == 0, result.output
+    result = invoke_import()
+    assert_failure(result, f"Account with alias '{ALIAS}' already in use")
+
+
+@run_once
+def test_import_account_instantiation_failure(mocker, ape_cli, runner, temp_account):
+    eth_account_from_key_patch = mocker.patch("ape_accounts._cli.EthAccount.from_key")
+    eth_account_from_key_patch.side_effect = Exception("Can't instantiate this account!")
+    result = runner.invoke(ape_cli, ["accounts", "import", ALIAS], input=IMPORT_VALID_INPUT)
+    assert_failure(result, "Key can't be imported: Can't instantiate this account!")
+
+
+@run_once
+def test_generate(ape_cli, runner, temp_keyfile_path):
+    assert not temp_keyfile_path.is_file()
+    # Generate new private key
+    result = runner.invoke(ape_cli, ["accounts", "generate", ALIAS], input=GENERATE_VALID_INPUT)
+    assert result.exit_code == 0, result.output
+    assert ALIAS in result.output
+    assert temp_keyfile_path.is_file()
+
+
+@run_once
+def test_generate_alias_already_in_use(ape_cli, runner, temp_account):
+    def invoke_generate():
+        return runner.invoke(ape_cli, ["accounts", "generate", ALIAS], input=GENERATE_VALID_INPUT)
+
+    result = invoke_generate()
+    assert result.exit_code == 0, result.output
+    result = invoke_generate()
+    assert_failure(result, f"Account with alias '{ALIAS}' already in use")
+
+
+@run_once
+def test_list(ape_cli, runner, temp_keyfile):
+    # Check availability
+    assert temp_keyfile.is_file()
+    result = runner.invoke(ape_cli, ["accounts", "list"], catch_exceptions=False)
+    assert ALIAS in result.output
+
+    # NOTE: the un-checksummed version of this address is found in the temp_keyfile fixture.
+    expected_address = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
+    assert expected_address in result.output
+
+
+@run_once
+def test_list_all(ape_cli, runner, temp_keyfile):
+    # Check availability
+    assert temp_keyfile.is_file()
+    result = runner.invoke(ape_cli, ["accounts", "list", "--all"])
+    assert ALIAS in result.output
+
+
+@run_once
+def test_change_password(ape_cli, runner, temp_keyfile):
+    assert temp_keyfile.is_file()
+    # Delete Account (`N` for "Leave unlocked?")
+    valid_input = [PASSWORD, "N", "b", "b"]
+    result = runner.invoke(
+        ape_cli,
+        ["accounts", "change-password", ALIAS],
+        input="\n".join(valid_input) + "\n",
+    )
+    assert result.exit_code == 0, result.output
+
+
+@run_once
+def test_delete(ape_cli, runner, temp_keyfile):
+    assert temp_keyfile.is_file()
+    # Delete Account
+    result = runner.invoke(ape_cli, ["accounts", "delete", ALIAS], input=f"{PASSWORD}\n")
+    assert result.exit_code == 0, result.output
+    assert not temp_keyfile.is_file()
---
+++ packages/eth-ape/eth-ape-0.5.6.tar.gz/eth-ape-0.5.6/tests/integration/cli/test_compile.py
@@ -0,0 +1,205 @@
+import shutil
+
+import pytest
+
+from ape.contracts import ContractContainer
+
+from .utils import skip_projects, skip_projects_except
+
+skip_non_compilable_projects = skip_projects(
+    "empty-config",
+    "no-config",
+    "script",
+    "only-dependencies",
+    "unregistered-contracts",
+    "test",
+    "geth",
+)
+
+
+@skip_projects(
+    "geth",
+    "multiple-interfaces",
+    "only-dependencies",
+    "test",
+    "unregistered-contracts",
+    "with-dependencies",
+    "with-contracts",
+)
+def test_compile_missing_contracts_dir(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING" in result.output, result.output
+    assert "Nothing to compile" in result.output
+
+
+@skip_projects_except("unregistered-contracts")
+def test_missing_extensions(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "--force"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING: No compilers detected for the following extensions:" in result.output
+    assert ".test" in result.output
+    assert ".foobar" in result.output
+
+
+@skip_projects_except("unregistered-contracts")
+def test_no_compiler_for_extension(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "contracts/Contract.test"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING: No compilers detected for the following extensions: .test" in result.output
+
+
+@skip_non_compilable_projects
+def test_compile(ape_cli, runner, project, clean_cache):
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+    # First time it compiles, it compiles the files with registered compilers successfully.
+    # Files with multiple extensions are currently not supported.
+    all_files = [f for f in project.path.glob("contracts/**/*")]
+    expected_files = [f for f in all_files if f.name.count(".") == 1]
+    unexpected_files = [f for f in all_files if f not in expected_files]
+
+    manifest = project.extract_manifest()
+    for file in expected_files:
+        assert file.name in manifest.sources
+
+    assert all([f.stem in result.output for f in expected_files])
+    assert not any([f.stem in result.output for f in unexpected_files])
+
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    # First time it compiles, it caches
+    for file in project.path.glob("contracts/**/*"):
+        assert file.stem not in result.output
+
+
+@skip_projects_except("multiple-interfaces")
+def test_compile_when_sources_change(ape_cli, runner, project, clean_cache):
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" in result.output
+
+    # Change the contents of a file
+    source_path = project.contracts_folder / "Interface.json"
+    modified_source_text = source_path.read_text().replace("foo", "bar")
+    source_path.unlink()
+    source_path.touch()
+    source_path.write_text(modified_source_text)
+
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" in result.output
+
+    # Verify that the next time, it does not need to recompile (no changes)
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" not in result.output
+
+
+@skip_projects_except("multiple-interfaces")
+def test_compile_when_source_contains_return_characters(ape_cli, runner, project, clean_cache):
+    # NOTE: This tests a bugfix where a source file contained return-characters
+    # and that triggered endless re-compiles because it technically contains extra
+    # bytes than the ones that show up in the text.
+
+    # Change the contents of a file to contain the '\r' character.
+    source_path = project.contracts_folder / "Interface.json"
+    modified_source_text = f"{source_path.read_text()}\r"
+    source_path.unlink()
+    source_path.touch()
+    source_path.write_text(modified_source_text)
+
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" in result.output
+
+    # Verify that the next time, it does not need to recompile (no changes)
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" not in result.output
+
+
+@skip_projects_except("multiple-interfaces")
+def test_can_access_contracts(project, clean_cache):
+    # This test does not use the CLI but still requires a project or run off of.
+    assert project.Interface, "Unable to access contract when needing to compile"
+    assert project.Interface, "Unable to access contract when not needing to compile"
+
+
+@skip_projects_except("multiple-interfaces")
+@pytest.mark.parametrize(
+    "contract_path",
+    ("Interface", "Interface.json", "contracts/Interface", "contracts/Interface.json"),
+)
+def test_compile_specified_contracts(ape_cli, runner, project, contract_path, clean_cache):
+    result = runner.invoke(ape_cli, ["compile", contract_path], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" in result.output
+
+
+@skip_projects_except("multiple-interfaces")
+def test_compile_unknown_extension_does_not_compile(ape_cli, runner, project, clean_cache):
+    result = runner.invoke(
+        ape_cli, ["compile", "Interface.js"], catch_exceptions=False
+    )  # Suffix to existing extension
+    assert result.exit_code == 2, result.output
+    assert "Error: Contract 'Interface.js' not found." in result.output
+
+
+@skip_projects_except()
+def test_compile_contracts(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "--size"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    # Still caches but displays bytecode size
+    for file in project.path.glob("contracts/**/*"):
+        assert file.stem in result.output
+
+
+@skip_projects_except("with-dependencies")
+@pytest.mark.parametrize(
+    "contract_path",
+    (None, "contracts/", "Project", "contracts/Project.json"),
+)
+def test_compile_with_dependency(ape_cli, runner, project, contract_path):
+    cmd = ["compile", "--force"]
+
+    if contract_path:
+        cmd.append(contract_path)
+
+    result = runner.invoke(ape_cli, cmd, catch_exceptions=False)
+    assert result.exit_code == 0
+    for name in (
+        "default",
+        "renamed-contracts-folder",
+        "containing-sub-dependencies",
+        "renamed-complex-contracts-folder",
+        "renamed-contracts-folder-specified-in-config",
+    ):
+        assert name in project.dependencies
+        assert type(project.dependencies[name]["local"]["name"]) == ContractContainer
+
+
+@skip_projects_except("with-dependencies")
+def test_compile_individual_contract_excludes_other_contract(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "Project", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Other" not in result.output
+
+
+@skip_projects_except("with-dependencies")
+def test_compile_non_ape_project_deletes_ape_config_file(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "Project", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "ape-config.yaml" not in [f.name for f in (project.path / "default").iterdir()]
+
+
+@skip_projects_except("only-dependencies")
+def test_compile_only_dependency(ape_cli, runner, project, clean_cache):
+    dependency_cache = project.path / "renamed_contracts_folder" / ".build"
+    if dependency_cache.is_dir():
+        shutil.rmtree(str(dependency_cache))
+
+    result = runner.invoke(ape_cli, ["compile", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'DependencyInProjectOnly.json'" in result.output
