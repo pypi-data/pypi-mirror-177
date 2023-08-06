623927cea0f108ff8d1bc51cd6e59a6df1a27c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:31 +0000

    Add package MiniballCpp/0.2.3/MiniballCpp-0.2.3.tar.gz

---
+++ packages/MiniballCpp/MiniballCpp-0.2.3.tar.gz/MiniballCpp-0.2.3/setup.py
@@ -0,0 +1,34 @@
+__author__ = "Konstantin Weddige"
+from setuptools import setup, Extension
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MiniballCpp",
+    version="0.2.3",
+    description="Smallest Enclosing Balls of Points",
+    long_description=long_description,
+    author="Bernd Gärtner, Konstantin Weddige",
+    url="https://github.com/weddige/miniball",
+    packages=[
+        "miniball",
+    ],
+    package_data={"miniball": ["py.typed"]},
+    ext_modules=[
+        Extension(
+            "miniball.bindings",
+            ["src/miniballmodule.cpp"],
+            include_dirs=["src"],
+            language="c++",
+        ),
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
