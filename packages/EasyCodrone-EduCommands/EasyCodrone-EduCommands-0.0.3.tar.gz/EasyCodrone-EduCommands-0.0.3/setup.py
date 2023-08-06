f4cf871a450a5d4a03ab21b32a51b8e394d43525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:39 +0000

    Add package EasyCodrone-EduCommands/0.0.3/EasyCodrone-EduCommands-0.0.3.tar.gz

---
+++ packages/EasyCodrone-EduCommands/EasyCodrone-EduCommands-0.0.3.tar.gz/EasyCodrone-EduCommands-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='EasyCodrone-EduCommands',
+    version='0.0.3',
+    packages=['easycodronecommands'],
+    url='https://github.com/burgerface627/EasyCodroneEduCommands',
+    license='MIT',
+    author='Alex Birdsall',
+    author_email='burgerface627@outlook.com',
+    description='Creates simple commands to be used with Codrone-edu',
+    install_requires=["codrone-edu"]
+)
