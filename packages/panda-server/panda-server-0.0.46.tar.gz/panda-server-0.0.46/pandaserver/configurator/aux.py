9a9aa31f96a0dfa7af6740a0472c7734d783df28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package panda-server/0.0.46/panda-server-0.0.46.tar.gz

---
+++ packages/panda-server/panda-server-0.0.46.tar.gz/panda-server-0.0.46/pandaserver/configurator/aux.py
@@ -0,0 +1,46 @@
+import requests
+import time
+import os
+
+from pandaserver.config import panda_config
+
+GB = 1024**3
+PROD_INPUT = 'Production Input'
+PROD_OUTPUT = 'Production Output'
+EXPRESS = 'Express'
+FILES = 'files'
+MBPS = 'mbps'
+LATENCY = 'latency'
+PACKETLOSS = 'packetloss'
+DONE = 'done'
+QUEUED = 'queued'
+TOTAL = 'total'
+LATEST = 'latest'
+H1 = '1h'
+H6 = '6h'
+D1 = '1d'
+W1 = '1w'
+TIMESTAMP = 'timestamp'
+
+
+def get_dump(url):
+    """
+    Retrieves a json file from the given URL and loads it into memory
+    """
+    if panda_config.configurator_use_cert:
+        key_file = os.environ['X509_USER_PROXY']
+        cert_file = os.environ['X509_USER_PROXY']
+        ca_certs = os.environ['X509_CERT_DIR']
+        cert = (cert_file, key_file)
+    else:
+        cert = None
+        ca_certs = False
+    for i in range(1, 4):  # 3 retries
+        try:
+            r = requests.get(url, cert=cert, verify=ca_certs)
+            if r.status_code == requests.codes.ok:
+                return r.json()
+        except Exception:
+            time.sleep(1)
+    return None
+
