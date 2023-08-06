747b996829ff34d8e3450c7a156fa76db2b4bbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:23 +0000

    Add package reait/0.0.12/reait-0.0.12.tar.gz

---
+++ packages/reait/reait-0.0.12.tar.gz/reait-0.0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+        name="reait",
+        version="0.0.12",
+        scripts=['reait'],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/RevEng-AI/reait",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent"
+            ],
+        install_requires=[
+                'tqdm', 'argparse', 'requests', 'rich', 'tomli', 'scikit-learn', 'pandas', 'numpy'
+            ]
+        )
+
