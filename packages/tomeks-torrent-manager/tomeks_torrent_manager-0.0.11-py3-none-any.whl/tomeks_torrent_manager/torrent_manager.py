2bafc16acbffd03e3eb7c60688625ce388c95729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:12 +0000

    Add package tomeks-torrent-manager/0.0.11/tomeks_torrent_manager-0.0.11-py3-none-any.whl

---
+++ packages/tomeks-torrent-manager/tomeks_torrent_manager-0.0.11-py3-none-any.whl/tomeks_torrent_manager/torrent_manager.py
@@ -0,0 +1,38 @@
+#!/bin/python
+import sys
+
+from tomeks_torrent_manager.pull_torrent_info.source_website import pull_website_history
+from tomeks_torrent_manager.pull_torrent_info.source_transmission import pull_transmission_data
+from tomeks_torrent_manager.pull_torrent_info.source_rss import pull_rss_feeds
+
+def pull_website():
+    pull_website_history()
+
+def pull_transmission():
+    pull_transmission_data()
+
+def pull_rss():
+    pull_rss_feeds()
+
+if __name__ == "__main__":
+    for function in sys.argv[1:]:
+        match function:
+            case "web":
+                pull_website_history()
+
+            case "trans":
+                pull_transmission_data()
+
+            case "rss":
+                pull_rss_feeds()
+
+    if len(sys.argv[1:]) == 0:
+        pull_website_history()
+        pull_transmission_data()
+        pull_rss_feeds()
+
+            
+
+    # pull_website_history()
+    # pull_transmission_data()
+    # pull_rss_feeds()
