a9611c2e073e658673f080566062c96c97060ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:00 +0000

    Add package http-client-sync/0.1.1/http_client_sync-0.1.1.tar.gz

---
+++ packages/http-client-sync/http_client_sync-0.1.1.tar.gz/http_client_sync-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['http_client_sync']
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'http-client-sync',
+    'version': '0.1.1',
+    'description': 'Http Client for work with API.',
+    'long_description': '# HttpApiGather\nSimple Http client for convenient work with api.',
+    'author': 'Alexandr Kolesnikov',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
