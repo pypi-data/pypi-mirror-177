291b60d2ea66715b0957a865413bbf38232e1956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:36 +0000

    Add package eth-stdlib/0.2.5/eth_stdlib-0.2.5.tar.gz

---
+++ packages/eth-stdlib/eth_stdlib-0.2.5.tar.gz/eth_stdlib-0.2.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['eth', 'eth.codecs', 'eth.codecs.abi', 'eth.codecs.abi.strategies']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{':python_version < "3.9"': ['pysha3>=1.0.0,<2.0.0'],
+ ':python_version >= "3.9"': ['safe-pysha3>=1.0.3,<2.0.0'],
+ 'hypothesis': ['hypothesis>=6.58.0,<7.0.0']}
+
+setup_kwargs = {
+    'name': 'eth-stdlib',
+    'version': '0.2.5',
+    'description': 'Ethereum Standard Library for Python',
+    'long_description': '# The Ethereum Standard Library\n\n[![GitHub](https://img.shields.io/github/license/skellet0r/eth-stdlib)](https://github.com/skellet0r/eth-stdlib/blob/master/COPYING)\n[![Codecov](https://img.shields.io/codecov/c/github/skellet0r/eth-stdlib)](https://app.codecov.io/gh/skellet0r/eth-stdlib)\n[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/skellet0r/eth-stdlib/test?label=test%20suite)](https://github.com/skellet0r/eth-stdlib/actions/workflows/test.yaml)\n[![Read the Docs](https://img.shields.io/readthedocs/eth-stdlib)](https://eth-stdlib.readthedocs.io/en/latest/)\n[![PyPI](https://img.shields.io/pypi/v/eth-stdlib)](https://pypi.org/project/eth-stdlib/)\n\nThe Ethereum Standard Library is a collection of libraries for developers building on the EVM.\n\n## Installation\n\n### Using pip\n\n```bash\n$ pip install eth-stdlib\n```\n\n### Using poetry\n\n```bash\n$ poetry add eth-stdlib\n```\n\n## Development\n\n### Initializing an Environment\n\nTo start developing/contributing to the eth-stdlib code base follow these steps:\n\n1. Install [poetry](https://python-poetry.org/)\n\n   ```bash\n   $ pipx install poetry\n   ```\n\n2. Clone the eth-stdlib repository\n\n   ```bash\n   $ git clone https://github.com/skellet0r/eth-stdlib.git\n   ```\n\n3. Initialize virtual environment\n\n   ```bash\n   $ poetry install --sync\n   ```\n\nAfterwards the development environment will be complete.\n\n### Testing\n\nTo run the test suite, execute the following command:\n\n```bash\n$ poetry run pytest\n```\n\nAfter running the test suite, code coverage results will be displayed in the terminal, as well as exported in html format (in the `htmlcov` directory).\n',
+    'author': 'Edward Amor',
+    'author_email': 'edward.amor3@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/skellet0r/eth-stdlib',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
