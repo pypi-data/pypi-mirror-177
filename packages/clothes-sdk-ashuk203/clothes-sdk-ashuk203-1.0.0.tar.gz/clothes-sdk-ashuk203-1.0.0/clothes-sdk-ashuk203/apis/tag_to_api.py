48e3f92fa32b340052c5dad54fd96b692fd4684d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:20 +0000

    Add package clothes-sdk-ashuk203/1.0.0/clothes-sdk-ashuk203-1.0.0.tar.gz

---
+++ packages/clothes-sdk-ashuk203/clothes-sdk-ashuk203-1.0.0.tar.gz/clothes-sdk-ashuk203-1.0.0/clothes-sdk-ashuk203/apis/tag_to_api.py
@@ -0,0 +1,17 @@
+import typing_extensions
+
+from clothes-sdk-ashuk203.apis.tags import TagValues
+from clothes-sdk-ashuk203.apis.tags.default_api import DefaultApi
+
+TagToApi = typing_extensions.TypedDict(
+    'TagToApi',
+    {
+        TagValues.DEFAULT: DefaultApi,
+    }
+)
+
+tag_to_api = TagToApi(
+    {
+        TagValues.DEFAULT: DefaultApi,
+    }
+)
