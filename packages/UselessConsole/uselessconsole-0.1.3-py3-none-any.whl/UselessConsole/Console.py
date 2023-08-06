69d8e8cbf49a57c82327cbdf85dd9dca3b4c1a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:11 +0000

    Add package UselessConsole/0.1.3/uselessconsole-0.1.3-py3-none-any.whl

---
+++ packages/UselessConsole/uselessconsole-0.1.3-py3-none-any.whl/UselessConsole/Console.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import random
+
+def WriteLine(input):
+    try:
+        print(input + "\n")
+    except:
+        print("CONSOLE ERROR: Couldn't print because we're out of milk, I'm gonna go get more.")
+    
+def Write(input):
+    try:
+        print(input)
+    except:
+        print("CONSOLE ERROR: Couldn't print because 3MI is ddosing WRD right now.")
+
+def ReplaceLine(input):
+    try:
+        sys.stdout.write("\r" + input)
+    except:
+        print("CONSOLE ERROR: Couldn't replace because you didn't have a dad to replace in the first place.")
+
+def emiray():
+    while True:
+        print("emiray ddos status: 0% !!!!")
+        print("cloudflare status: 100% !!!!!!!!")
+        print("wrd status: 102% !!!!!!!!")
+        print("crappy console module: 100% !!!!!!!!")
