c278248aa2177ecc2f6926a22a3e2ee28858e9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:34 +0000

    Add package pyledlanglib/0.1.0/pyledlanglib-0.1.0.tar.gz

---
+++ packages/pyledlanglib/pyledlanglib-0.1.0.tar.gz/pyledlanglib-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pyledlanglib',
+    version='0.1.0',    
+    description='PyLEdLang compiler on top of Python3',
+    url='https://github.com/shuds13/pyexample',
+    author='Alex Zaslavskis',
+    author_email='sahsariga111@gmail.com',
+    license='BSD 2-clause',
+    packages=['pyledlanglib'],
+    install_requires=[''],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
