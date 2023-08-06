f1001e4a2ab48f1c574757488a0d60dc2718c5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:40 +0000

    Add package rubberdoc/0.2.4/rubberdoc-0.2.4.tar.gz

---
+++ packages/rubberdoc/rubberdoc-0.2.4.tar.gz/rubberdoc-0.2.4/rubberdoc/examples/example_from_script.py
@@ -0,0 +1,25 @@
+from rubberdoc.generator import RubberDoc 
+from rubberdoc.config_provider import RubberDocConfig
+from rubberdoc.doc_handler import BaseDocHandler
+
+class MyDocHandler(BaseDocHandler):
+    """Custom DocHandler"""
+    def __init__(self, file_or_path: str, config: RubberDocConfig):
+        super().__init__(file_or_path, config)
+    
+    def wrap_codeblock(self, code: str) -> str:
+        """Override the `wrap_codeblock` method to return nothing"""
+        return ""
+
+    def wrap_docstring(self, docstring: str) -> str:
+        return '**DOCSTRING**  \n' + docstring + '  \n'
+
+default_config = RubberDocConfig(path_to_config=None)
+
+rd = RubberDoc(
+    config=default_config,
+    doc_handler=MyDocHandler)
+
+rd.generate(
+    input_directory=r"path/to/package", 
+    output_directory=r"path/to/documentation")
