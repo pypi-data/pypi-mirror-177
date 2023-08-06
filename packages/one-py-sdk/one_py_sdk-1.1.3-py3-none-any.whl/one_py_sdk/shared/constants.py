c22ecbab4c361c2c9a6d9b0f58ba625448124986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:56 +0000

    Add package one-py-sdk/1.1.3/one_py_sdk-1.1.3-py3-none-any.whl

---
+++ packages/one-py-sdk/one_py_sdk-1.1.3-py3-none-any.whl/one_py_sdk/shared/constants.py
@@ -0,0 +1,1 @@
+Environment = {"feature":"https://api-feature-us.aquaticinformatics.net", "integration":"https://api-integration-us.aquaticinformatics.net", "stage":"https://api-stage-us.aquaticinformatics.net", "production":"https://api-us.aquaticinformatics.net/"}
