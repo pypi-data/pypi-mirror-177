4d9801182c7952d7bf31998832ddbf5e94813c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:06 +0000

    Add package TPython/0.2/TPython-0.2.tar.gz

---
+++ packages/TPython/TPython-0.2.tar.gz/TPython-0.2/main.py
@@ -0,0 +1,77 @@
+import sys
+from os import system, name, get_terminal_size
+from traceback import format_exc
+try:
+    from colorama import init as cinit, Fore
+except:
+    sys.exit('module not found: colorama\npython3 -m pip install colorama')
+
+n = 1
+err = False
+a = False
+namespace = {}
+
+def main():
+    global n, err, a
+    cinit(autoreset=True)
+
+    def ext(crash=False):
+        cl = get_terminal_size().columns
+        m = 'Crashed' if crash else 'Process Completed Successfully'
+        for i in range((cl-len(m))//2):
+            m = f'-{m}-'
+        if len(m) != cl:
+            m += '-'
+        sys.exit(f'{Fore.LIGHTYELLOW_EX}{"-"*cl}\n{m}\n{"-"*cl}' if crash else f'{Fore.LIGHTCYAN_EX}{m}')
+
+    try:
+        def exe(inp):
+            global err, n
+            try:
+                exec(inp, namespace)
+                err = False
+            except Exception:
+                print(f'{Fore.LIGHTRED_EX}{format_exc()}')
+                err = True
+            n += 1
+
+        cl = get_terminal_size().columns
+        m = 'Welcome to TPython'
+        cl -= 18
+        for i in range(cl//2):
+            m = f'-{m}-'
+        print(f'{Fore.LIGHTCYAN_EX}{m}')
+
+        while True:
+            try:
+                inp = input(f'{Fore.LIGHTGREEN_EX}[{Fore.LIGHTRED_EX if err else Fore.RESET}{n}{Fore.LIGHTGREEN_EX}]-{Fore.LIGHTCYAN_EX}> {Fore.RESET}')
+                if not (inp.isspace() or inp == ''):
+                    inp = inp.strip()
+                    if inp == 'exit' or inp == 'quit':
+                        ext()
+                    elif inp == 'clear' or inp == 'cls':
+                        system('cls' if name == 'nt' else 'clear')
+                        err = False
+                    elif inp == 'version':
+                        print(f'{Fore.LIGHTCYAN_EX}0.2')
+                    else:
+                        if inp.endswith(':'):
+                            while True:
+                                ig = input(f'{Fore.LIGHTGREEN_EX}[{Fore.LIGHTYELLOW_EX}{":"*len(str(n))}{Fore.LIGHTGREEN_EX}]-{Fore.LIGHTYELLOW_EX}> {Fore.RESET}')
+                                if ig.strip() == '':
+                                    if not a:
+                                        a = True
+                                    else:
+                                        break
+                                else:
+                                    inp += f'\n\t{ig}'
+                            exe(inp)
+                            a = False
+                        else:
+                            exe(inp)
+            except KeyboardInterrupt:
+                print(f'\n{Fore.LIGHTYELLOW_EX}KeyboardInterrupt')
+                err = True
+    except Exception:
+        print(f'\n{Fore.LIGHTRED_EX}{format_exc()}')
+        ext(True)
