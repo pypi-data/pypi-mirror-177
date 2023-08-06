170a4f55a56043fe840756e7704963374561b017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:30 +0000

    Add package ezdxf/1.0.0rc1/ezdxf-1.0.0rc1.zip

---
+++ packages/ezdxf/ezdxf-1.0.0rc1.zip/ezdxf-1.0.0rc1/integration_tests/test_drawing_qt_backend.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2020-2021, Matthew Broadway
+# License: MIT License
+import pytest
+import sys
+
+pytest.importorskip("PySide6")
+from ezdxf.addons.xqt import QtWidgets as qw
+from ezdxf.addons.drawing.pyqt import PyQtBackend
+
+WIN = sys.platform == "win32"
+PY310 = sys.version_info[:2] == (3, 10)
+_app = None
+
+
+@pytest.fixture
+def backend():
+    global _app
+    _app = qw.QApplication([])
+    scene = qw.QGraphicsScene()
+    return PyQtBackend(scene)
+
+
+# Error Message:
+# RuntimeError: Please destroy the QApplication singleton before creating a new
+# QApplication instance.
+#
+# ezdxf "view" command works as expected!
+# Works if launched in PyCharm, but not if launched by command line
+# "pytest ..." ???
+
+@pytest.mark.skip(
+    reason="Does not work for CPython 3.10 & PySide6",
+)
+def test_get_text_width(backend):
+    assert backend.get_text_line_width(
+        "   abc", 100
+    ) > backend.get_text_line_width("abc", 100)
+    assert backend.get_text_line_width(
+        "  abc ", 100
+    ) == backend.get_text_line_width("  abc", 100)
+    assert backend.get_text_line_width("   ", 100) == 0
+    assert backend.get_text_line_width("  ", 100) == 0
