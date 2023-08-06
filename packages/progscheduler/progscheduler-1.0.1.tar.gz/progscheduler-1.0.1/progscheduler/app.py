184e9ad068f84942b572d9090cbaf9e09f2c0370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:29 +0000

    Add package progscheduler/1.0.1/progscheduler-1.0.1.tar.gz

---
+++ packages/progscheduler/progscheduler-1.0.1.tar.gz/progscheduler-1.0.1/progscheduler/app.py
@@ -0,0 +1,34 @@
+import sys
+from progscheduler.arguments import Arguments
+from progscheduler.jobs import open_program
+from progscheduler.utils.information import show
+from .scheduler import Scheduler
+
+
+def main():
+    arguments = Arguments().configure()
+
+    if arguments[0].configure:
+        sys.exit()
+
+    show('The program will now start running the scheduler. While this is running this window should not be closed because that will stop the '
+         'schedule. If you know that all scheduled jobs are already finished, then it is safe to close this window.')
+
+    scheduler = Scheduler()
+
+    for program in arguments:
+        process_scheduler(scheduler, program)
+
+    scheduler.run()
+
+
+def process_scheduler(scheduler, program_to_schedule):
+    scheduler.set_method_to_schedule(lambda: open_program(program_to_schedule.executable_path.value))
+    scheduler.process(
+        program_to_schedule.days_to_schedule.value,
+        program_to_schedule.time_to_schedule.value
+    )
+
+
+if __name__ == '__main__':
+    main()
