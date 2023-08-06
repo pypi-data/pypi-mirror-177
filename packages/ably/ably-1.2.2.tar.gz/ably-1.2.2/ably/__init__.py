e32b0551799eb265a64e982ac5d1ee5e6ef61678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:19:47 +0000

    Add package ably/1.2.2/ably-1.2.2.tar.gz

---
+++ packages/ably/ably-1.2.2.tar.gz/ably-1.2.2/ably/__init__.py
@@ -0,0 +1,17 @@
+from ably.rest.rest import AblyRest
+from ably.rest.auth import Auth
+from ably.rest.push import Push
+from ably.types.capability import Capability
+from ably.types.channelsubscription import PushChannelSubscription
+from ably.types.device import DeviceDetails
+from ably.types.options import Options
+from ably.util.crypto import CipherParams
+from ably.util.exceptions import AblyException, AblyAuthException, IncompatibleClientIdException
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+api_version = '1.2'
+lib_version = '1.2.2'
---
+++ packages/ably/ably-1.2.2.tar.gz/ably-1.2.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ably', 'ably.http', 'ably.rest', 'ably.transport', 'ably.types', 'ably.util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['h2>=4.0.0,<5.0.0',
+ 'httpx>=0.23.0,<0.24.0',
+ 'methoddispatch>=3.0.2,<4.0.0',
+ 'msgpack>=1.0.0,<2.0.0']
+
+extras_require = \
+{'crypto': ['pycryptodome'], 'oldcrypto': ['pycrypto>=2.6.1,<3.0.0']}
+
+setup_kwargs = {
+    'name': 'ably',
+    'version': '1.2.2',
+    'description': 'Python REST client library SDK for Ably realtime messaging service',
+    'long_description': 'Official Ably Bindings for Python\n==================================\n\nA Python client library for Ably Realtime messaging.\n\n\nSetup\n-----\n\nYou can install this package by using the pip tool and installing:\n\n    pip install ably\n\n\nUsing Ably for Python\n---------------------\n\n- Sign up for Ably at https://ably.com/sign-up\n- Get usage examples at https://github.com/ably/ably-python\n- Visit https://ably.com/docs for a complete API reference and more examples\n',
+    'author': 'Ably',
+    'author_email': 'support@ably.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://ably.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
