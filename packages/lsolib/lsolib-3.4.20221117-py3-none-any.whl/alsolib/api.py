a3412a6201a9ed8eb657b8df61be879934f46592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package lsolib/3.4.20221117/lsolib-3.4.20221117-py3-none-any.whl

---
+++ packages/lsolib/lsolib-3.4.20221117-py3-none-any.whl/alsolib/api.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+class Alsoai():
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
---
+++ packages/lsolib/lsolib-3.4.20221117-py3-none-any.whl/alsolib/xes.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+class LoadWork:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class user:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
