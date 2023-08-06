9269a6ebdd0160cca0453dab9666cdd762160e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:52:04 +0000

    Add package homeservices/0.1.2/homeservices-0.1.2.tar.gz

---
+++ packages/homeservices/homeservices-0.1.2.tar.gz/homeservices-0.1.2/homeservices/wsgi.py
@@ -0,0 +1,15 @@
+from webservice import HomeServices
+from pathlib import Path
+
+def create_app():
+    templates_path = "{}/templates".format(Path().absolute())
+    static_path = "{}/static".format(Path().absolute())
+    wtf_service = HomeServices(template_folder=templates_path, static_folder=static_path)
+    wtf_service.run()
+    # return wtf_service.getApp()
+    # return wtf_service
+
+if __name__ == '__main__':
+    # create_app = create_app()
+    # create_app.run()
+    create_app()
