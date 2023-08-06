626e610ee340c829ff30f436ec07ced22b317219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:21 +0000

    Add package greet-app/0.1.0/greet-app-0.1.0.tar.gz

---
+++ packages/greet-app/greet-app-0.1.0.tar.gz/greet-app-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['greet_app']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['greet-app = greet_app.main:app']}
+
+setup_kwargs = {
+    'name': 'greet-app',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': 'My-app',
+    'author': 'Kamil Woźniak',
+    'author_email': 'jestem.kamil.wozniak@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
