c8835f276e9ee8b4388d905d7e70a4dabd693369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:11 +0000

    Add package pydappdb/0.2/pydappdb-0.2.tar.gz

---
+++ packages/pydappdb/pydappdb-0.2.tar.gz/pydappdb-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = "pydappdb",
+version="0.2",
+description="lets you store data on a decentralized network.",
+author="Abhirath Dubey",
+author_email="abhirathdubey2804@gmail.com",
+packages=["pydappdb"],
+install_requires = ["requests"],
+# requires=["requests", "json"],
+)
