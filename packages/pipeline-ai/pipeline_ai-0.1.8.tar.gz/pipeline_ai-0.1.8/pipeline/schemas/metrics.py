a0a4c4af3bd1223364638681e76dc050bbd7c85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package pipeline-ai/0.1.8/pipeline_ai-0.1.8.tar.gz

---
+++ packages/pipeline-ai/pipeline_ai-0.1.8.tar.gz/pipeline_ai-0.1.8/pipeline/schemas/metrics.py
@@ -0,0 +1,140 @@
+from datetime import datetime, timedelta
+from typing import List
+
+from pydantic import Field
+
+from .base import BaseModel
+
+
+class RunMetric(BaseModel):
+    #: total number of runs executed on the pipeline
+    run_count: int
+    #: total number of failed runs executed on the pipeline
+    failed_run_count: int
+    #: total number of succeeded runs executed on the pipeline
+    succeeded_run_count: int
+    #: total run compute time (typically of completed runs)
+    total_compute_ms: int
+
+
+class RunHardwareMetric(BaseModel):
+    resource_type: str
+    total_percentage: float
+    run_count: int
+    average_runtime: int
+
+
+class ProjectHardwareMetric(BaseModel):
+    project_id: str
+    project_name: str
+    project_usage: List[RunHardwareMetric]
+
+
+class HardwareMetric(BaseModel):
+    start: datetime
+    end: datetime
+    projects: List[ProjectHardwareMetric]
+    account_usage: List[RunHardwareMetric]
+
+
+class RunMetricsBucket(BaseModel):
+    """
+    Type of metadata of a specific bucket.
+
+    start: start timestamp of the time range
+    end: end timestamp of the time range
+    run_count: count of runs part of this bucket
+    success_rate: percentage of successful runs as float between 0 and 1
+    average_runtime: average runtime of all runs in this bucket
+    total_runtime: total runtime of all runs in this bucket
+    """
+
+    start: datetime
+    end: datetime
+    run_count: int
+    success_rate: float
+    average_runtime: int
+    total_runtime: int
+
+
+class RunMetricsGet(BaseModel):
+    """
+    Return type of the metrics endpoint on run related requests.
+
+    start: start timestamp of the time range
+    end: end timestamp of the time range
+    bucket_count: number of buckets time range was divided into
+    metrics_buckets: list of metadata about each bucket
+    overall_bucket: metadata for the whole time range
+    preceding_bucket: metadata for the bucket directly preceding the time range
+    """
+
+    start: datetime
+    end: datetime
+    bucket_count: int
+    metrics_buckets: List[RunMetricsBucket]
+    overall_bucket: RunMetricsBucket
+    preceding_bucket: RunMetricsBucket
+
+
+class PipelineMetricsGet(RunMetricsGet):
+    """Run metrics supplemented with pipeline meta data. Used in particular
+    for list/paginated metrics views where it is important for the client to know which
+    pipeline is which"""
+
+    pipeline_id: str
+    pipeline_name: str
+
+
+class PipelineMetricsGetSummary(RunMetric):
+    """Pipeline run summary metrics supplemented with pipeline meta data.
+    Pipeline meta data is useful for list/paginated metrics views so the client
+    can keep track of which pipeline is which"""
+
+    pipeline_id: str
+    pipeline_name: str
+
+
+class MetricsQuery(BaseModel):
+    """
+    Query parameters for generic metrics requests.
+
+    start: start timestamp of the time range
+    end: end timestamp of the time range
+    """
+
+    start: datetime = Field(
+        default_factory=lambda: datetime.utcnow() - timedelta(hours=24)
+    )
+    end: datetime = Field(default_factory=datetime.utcnow)
+
+
+class RunMetricsQuery(MetricsQuery):
+    """
+    Query parameters for run metrics requests.
+
+    start: start timestamp of the time range
+    end: end timestamp of the time range
+    bucket_count: number of buckets to divide the time range into
+    """
+
+    bucket_count: int = 100
+
+
+class PipelineComputeGet(BaseModel):
+    """Pipeline compute metrics for retrieving the number of completed runs
+    and total compute time of all the runs on a pipeline"""
+
+    start: datetime
+    end: datetime
+    pipeline_id: str
+    pipeline_name: str
+    completed_run_count: int
+    total_compute_ms: int
+
+
+class TotalComputeGet(RunMetric):
+    """Total compute time of all a users' runs"""
+
+    start: datetime
+    end: datetime
