65163d596a93d17c9e307218aa41089b37f8cf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package types-python-nmap/0.7.1.1/types-python-nmap-0.7.1.1.tar.gz

---
+++ packages/types-python-nmap/types-python-nmap-0.7.1.1.tar.gz/types-python-nmap-0.7.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-python-nmap"
+description = "Typing stubs for python-nmap"
+long_description = '''
+## Typing stubs for python-nmap
+
+This is a PEP 561 type stub package for the `python-nmap` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `python-nmap`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/python-nmap. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `f62f402127254d3ff923da52d42fb878286cf09c`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.7.1.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/python-nmap.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['nmap-stubs'],
+      package_data={'nmap-stubs': ['__init__.pyi', 'nmap.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
