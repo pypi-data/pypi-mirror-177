1df09e5ce5841a201abf190920a0c591b32399f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package pyinels-domenix-dev/0.2.4/pyinels-domenix-dev-0.2.4.tar.gz

---
+++ packages/pyinels-domenix-dev/pyinels-domenix-dev-0.2.4.tar.gz/pyinels-domenix-dev-0.2.4/setup.py
@@ -0,0 +1,23 @@
+"""Setup script for pyinels package."""
+from setuptools import setup, find_packages
+from pyinels.const import NAME, VERSION
+
+
+setup(name=NAME,
+      version=VERSION,
+      url='https://github.com/JH-Soft-Technology/pyinels',
+      license='MIT',
+      author='JH Soft Technology',
+      author_email='horalek.jiri@gmail.com',
+      description='Python library for iNels BUS',
+      keywords=['iNels', 'Elko EP', 'Home assistant integration'],
+      long_description_content_type='text/markdown',
+      charset='UTF-8',
+      variant='GFM',
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      classifiers=[
+          "License :: OSI Approved :: MIT License"
+      ],
+      packages=find_packages(),
+      test_suite='unittest')
