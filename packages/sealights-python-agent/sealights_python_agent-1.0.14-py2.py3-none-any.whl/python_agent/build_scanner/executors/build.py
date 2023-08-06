9f61688b049fc350f60558123efba6b8b1564f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:07 +0000

    Add package sealights-python-agent/1.0.14/sealights_python_agent-1.0.14-py2.py3-none-any.whl

---
+++ packages/sealights-python-agent/sealights_python_agent-1.0.14-py2.py3-none-any.whl/python_agent/build_scanner/executors/build.py
@@ -0,0 +1,16 @@
+from python_agent.build_scanner import app as build_scanner
+from python_agent.utils import disableable
+
+
+class Build(object):
+    def __init__(self, config_data):
+        self.config_data = config_data
+        self.workspacepath = self.config_data.additionalParams.get("workspacepath")
+        self.include = self.config_data.additionalParams.get("include") or None
+        self.exclude = self.config_data.additionalParams.get("exclude") or None
+
+    @disableable()
+    def execute(self):
+        build_scanner.main(config_data=self.config_data, workspacepath=self.workspacepath, include=self.include,
+                           exclude=self.exclude)
+
