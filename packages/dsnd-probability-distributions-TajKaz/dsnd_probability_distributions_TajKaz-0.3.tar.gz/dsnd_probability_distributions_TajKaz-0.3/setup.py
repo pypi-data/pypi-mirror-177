418b2b285a37ff0d0ab15f5a29919e18c7ceca95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:29 +0000

    Add package dsnd-probability-distributions-TajKaz/0.3/dsnd_probability_distributions_TajKaz-0.3.tar.gz

---
+++ packages/dsnd-probability-distributions-TajKaz/dsnd_probability_distributions_TajKaz-0.3.tar.gz/dsnd_probability_distributions_TajKaz-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_distributions_TajKaz',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_distributions_TajKaz'],     
+      author = 'Tajudeen Kazeem',
+      author_email = 'tajqas@gmail.com',
+  zip_safe=False),
+
