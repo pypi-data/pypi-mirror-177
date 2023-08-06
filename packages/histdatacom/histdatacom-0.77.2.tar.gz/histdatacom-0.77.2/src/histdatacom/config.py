35359e9614416e904017a95ea734047340212ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:46 +0000

    Add package histdatacom/0.77.2/histdatacom-0.77.2.tar.gz

---
+++ packages/histdatacom/histdatacom-0.77.2.tar.gz/histdatacom-0.77.2/src/histdatacom/config.py
@@ -0,0 +1,15 @@
+from histdatacom.records import Records
+from multiprocessing import Queue
+
+args = None
+
+available_remote_data = dict()
+repo_data_file_exists = False
+filter_pairs = None
+
+queue_manager = None
+
+current_queue = None
+next_queue = None
+influx_chunks_queue = None
+
