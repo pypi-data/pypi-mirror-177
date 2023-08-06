bd12f4022cc7e288ff5afa96884d2358952fec38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:44 +0000

    Add package transifex-python/3.0.3/transifex-python-3.0.3.tar.gz

---
+++ packages/transifex-python/transifex-python-3.0.3.tar.gz/transifex-python-3.0.3/transifex/native/__init__.py
@@ -0,0 +1,38 @@
+from transifex.native.core import TxNative
+
+
+def init(
+    token, languages, secret=None,
+    cds_host=None, missing_policy=None,
+    error_policy=None, cache=None,
+    fetch_all_langs=False,
+):
+    """Initialize the framework.
+
+    :param list languages: A list of language codes for the languages
+        the application is localized into
+    :param str token: the API token to use for connecting to the CDS
+    :param str secret: the additional secret required for pushing translations
+    :param str cds_host: an optional host for the Content Delivery Service,
+        defaults to the host provided by Transifex
+    :param AbstractRenderingPolicy missing_policy: an optional policy to use
+        for returning strings when a translation is missing
+    :param AbstractErrorPolicy error_policy: an optional policy to use
+        for defining how to handle translation rendering errors
+    :param AbstractCache cache: an optional cache
+    :param bool fetch_all_langs: force pull all remote languages
+    """
+    if not tx.initialized:
+        tx.init(
+            languages,
+            token,
+            secret=secret,
+            cds_host=cds_host,
+            missing_policy=missing_policy,
+            error_policy=error_policy,
+            cache=cache,
+            fetch_all_langs=fetch_all_langs,
+        )
+
+
+tx = TxNative()
