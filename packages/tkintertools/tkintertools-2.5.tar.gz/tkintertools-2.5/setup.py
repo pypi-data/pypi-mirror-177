3f50d47e20e1c2007ac903c95d9932e09c7d181d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:14 +0000

    Add package tkintertools/2.5/tkintertools-2.5.tar.gz

---
+++ packages/tkintertools/tkintertools-2.5.tar.gz/tkintertools-2.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='tkintertools',  # 模块对外的名字
+    version='2.5',  # 版本号
+    author='XiaoKang2022',  # 作者
+    author_email='2951256653@qq.com',  # 邮箱
+    description='Auxiliary module of tkinter',  # 描述
+    url='https://gitcode.net/weixin_62651706/tkintertools',
+    py_modules=['tkintertools.__init__'],  # 模块名
+)
