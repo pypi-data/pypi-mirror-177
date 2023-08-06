968727a2b54adce06918a7f3d5ff4e2ef1dc6af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:57 +0000

    Add package transformers4rec/0.1.15/transformers4rec-0.1.15.tar.gz

---
+++ packages/transformers4rec/transformers4rec-0.1.15.tar.gz/transformers4rec-0.1.15/transformers4rec/torch/model/prediction_task.py
@@ -0,0 +1,405 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import logging
+from typing import Dict, Iterable, Optional
+
+import torch
+import torchmetrics as tm
+
+from ..block.base import Block, BuildableBlock, SequentialBlock
+from ..block.mlp import MLPBlock
+from ..ranking_metric import AvgPrecisionAt, NDCGAt, RecallAt
+from ..utils.torch_utils import LambdaModule
+from .base import BlockType, PredictionTask
+
+LOG = logging.getLogger("transformers4rec")
+
+
+class BinaryClassificationPrepareBlock(BuildableBlock):
+    def build(self, input_size) -> SequentialBlock:
+        return SequentialBlock(
+            torch.nn.Linear(input_size[-1], 1, bias=False),
+            torch.nn.Sigmoid(),
+            LambdaModule(lambda x: x.view(-1)),
+            output_size=[
+                None,
+            ],
+        )
+
+
+class BinaryClassificationTask(PredictionTask):
+    DEFAULT_LOSS = torch.nn.BCELoss()
+    DEFAULT_METRICS = (
+        tm.Precision(num_classes=2),
+        tm.Recall(num_classes=2),
+        tm.Accuracy(),
+        # TODO: Fix this: tm.AUC()
+    )
+
+    def __init__(
+        self,
+        target_name: Optional[str] = None,
+        task_name: Optional[str] = None,
+        task_block: Optional[BlockType] = None,
+        loss=DEFAULT_LOSS,
+        metrics=DEFAULT_METRICS,
+        summary_type="first",
+    ):
+        self.target_dim = 1
+        super().__init__(
+            loss=loss,
+            metrics=metrics,
+            target_name=target_name,
+            task_name=task_name,
+            summary_type=summary_type,
+            task_block=task_block,
+            pre=BinaryClassificationPrepareBlock(),
+            forward_to_prediction_fn=lambda x: torch.round(x).int(),
+        )
+
+
+class RegressionPrepareBlock(BuildableBlock):
+    def build(self, input_size) -> SequentialBlock:
+        return SequentialBlock(
+            torch.nn.Linear(input_size[-1], 1),
+            LambdaModule(lambda x: x.view(-1)),
+            output_size=[
+                None,
+            ],
+        )
+
+
+class RegressionTask(PredictionTask):
+    DEFAULT_LOSS = torch.nn.MSELoss()
+    DEFAULT_METRICS = (tm.regression.MeanSquaredError(),)
+
+    def __init__(
+        self,
+        target_name: Optional[str] = None,
+        task_name: Optional[str] = None,
+        task_block: Optional[BlockType] = None,
+        loss=DEFAULT_LOSS,
+        metrics=DEFAULT_METRICS,
+        summary_type="first",
+    ):
+        self.target_dim = 1
+        super().__init__(
+            loss=loss,
+            metrics=metrics,
+            target_name=target_name,
+            task_name=task_name,
+            summary_type=summary_type,
+            task_block=task_block,
+            pre=RegressionPrepareBlock(),
+        )
+
+
+class NextItemPredictionTask(PredictionTask):
+    """This block performs item prediction task for session and sequential-based models.
+    It requires a body containing a masking schema to use for training and target generation.
+    For the supported masking schemes, please refers to:
+    https://nvidia-merlin.github.io/Transformers4Rec/main/model_definition.html#sequence-masking
+
+    Parameters
+    ----------
+    loss: torch.nn.Module
+        Loss function to use. Defaults to NLLLos.
+    metrics: Iterable[torchmetrics.Metric]
+        List of ranking metrics to use for evaluation.
+    task_block:
+        Module to transform input tensor before computing predictions.
+    task_name: str, optional
+        Name of the prediction task, if not provided a name will be automatically constructed based
+        on the target-name & class-name.
+    weight_tying: bool
+        The item id embedding table weights are shared with the prediction network layer.
+    softmax_temperature: float
+        Softmax temperature, used to reduce model overconfidence, so that softmax(logits / T).
+        Value 1.0 reduces to regular softmax.
+    padding_idx: int
+        pad token id.
+    target_dim: int
+        vocabulary size of item ids
+    hf_format: bool
+        Output the dictionary of outputs needed by RecSysTrainer, if set to False,
+        return the predictions tensor.
+    """
+
+    DEFAULT_METRICS = (
+        # default metrics suppose labels are int encoded
+        NDCGAt(top_ks=[10, 20], labels_onehot=True),
+        AvgPrecisionAt(top_ks=[10, 20], labels_onehot=True),
+        RecallAt(top_ks=[10, 20], labels_onehot=True),
+    )
+
+    def __init__(
+        self,
+        loss: torch.nn.Module = torch.nn.NLLLoss(ignore_index=0),
+        metrics: Iterable[tm.Metric] = DEFAULT_METRICS,
+        task_block: Optional[BlockType] = None,
+        task_name: str = "next-item",
+        weight_tying: bool = False,
+        softmax_temperature: float = 1,
+        padding_idx: int = 0,
+        target_dim: int = None,
+        hf_format=False,
+    ):
+        super().__init__(loss=loss, metrics=metrics, task_block=task_block, task_name=task_name)
+        self.softmax_temperature = softmax_temperature
+        self.weight_tying = weight_tying
+        self.padding_idx = padding_idx
+        self.target_dim = target_dim
+        self.hf_format = hf_format
+
+        self.item_embedding_table = None
+        self.masking = None
+
+    def build(self, body, input_size, device=None, inputs=None, task_block=None, pre=None):
+        """Build method, this is called by the `Head`."""
+        if not len(input_size) == 3 or isinstance(input_size, dict):
+            raise ValueError(
+                "NextItemPredictionTask needs a 3-dim vector as input, found:" f"{input_size}"
+            )
+
+        # Retrieve the embedding module to get the name of itemid col and its related table
+        if not inputs:
+            inputs = body.inputs
+        if not getattr(inputs, "item_id", None):
+            raise ValueError(
+                "For Item Prediction task a categorical_module "
+                "including an item_id column is required."
+            )
+        self.embeddings = inputs.categorical_module
+        if not self.target_dim:
+            self.target_dim = self.embeddings.item_embedding_table.num_embeddings
+        if self.weight_tying:
+            self.item_embedding_table = self.embeddings.item_embedding_table
+            item_dim = self.item_embedding_table.weight.shape[1]
+            if input_size[-1] != item_dim and not task_block:
+                LOG.warning(
+                    f"Projecting inputs of NextItemPredictionTask to'{item_dim}' "
+                    f"As weight tying requires the input dimension '{input_size[-1]}' "
+                    f"to be equal to the item-id embedding dimension '{item_dim}'"
+                )
+                # project input tensors to same dimension as item-id embeddings
+                task_block = MLPBlock([item_dim])
+
+        # Retrieve the masking from the input block
+        self.masking = inputs.masking
+        if not self.masking:
+            raise ValueError(
+                "The input block should contain a masking schema for training and evaluation"
+            )
+        self.padding_idx = self.masking.padding_idx
+        pre = NextItemPredictionPrepareBlock(
+            target_dim=self.target_dim,
+            weight_tying=self.weight_tying,
+            item_embedding_table=self.item_embedding_table,
+            softmax_temperature=self.softmax_temperature,
+        )
+        super().build(
+            body, input_size, device=device, inputs=inputs, task_block=task_block, pre=pre
+        )
+
+    def forward(
+        self, inputs: torch.Tensor, ignore_masking=True, training=False, hf_format=None, **kwargs
+    ):
+        if hf_format is not None:
+            self.hf_format = hf_format
+
+        if isinstance(inputs, (tuple, list)):
+            inputs = inputs[0]
+        x = inputs.float()
+
+        if self.task_block:
+            x = self.task_block(x)  # type: ignore
+
+        # Retrieve labels from masking
+        if training or not ignore_masking:
+            labels = self.masking.masked_targets  # type: ignore
+            trg_flat = labels.flatten()
+            non_pad_mask = trg_flat != self.padding_idx
+            labels_all = torch.masked_select(trg_flat, non_pad_mask)
+            # remove padded items, keep only masked positions
+            x = self.remove_pad_3d(x, non_pad_mask)
+        else:
+            # keep only last non-padded position for the 'Prediction step'
+            labels = self.embeddings.item_seq
+            non_pad_mask = labels != self.padding_idx
+            last_item_sessions = non_pad_mask.sum(dim=1) - 1
+            rows_ids = torch.arange(labels.size(0), dtype=torch.long, device=labels.device)
+            labels_all = labels[rows_ids, last_item_sessions]
+            labels_all = labels_all.flatten()
+            x = x[rows_ids, last_item_sessions]
+
+        # Compute predictions probs
+        x = self.pre(x)  # type: ignore
+
+        # prepare outputs for HF trainer
+        if self.hf_format:
+            loss = self.loss(x, labels_all)
+            return {
+                "loss": loss,
+                "labels": labels_all,
+                "predictions": x,
+                # "pred_metadata": {},
+                # "model_outputs": [],
+            }
+            # TODO: Add model_outputs and metadata
+
+        return x
+
+    def remove_pad_3d(self, inp_tensor, non_pad_mask):
+        # inp_tensor: (n_batch x seqlen x emb_dim)
+        inp_tensor = inp_tensor.flatten(end_dim=1)
+        inp_tensor_fl = torch.masked_select(
+            inp_tensor, non_pad_mask.unsqueeze(1).expand_as(inp_tensor)
+        )
+        out_tensor = inp_tensor_fl.view(-1, inp_tensor.size(1))
+        return out_tensor
+
+    def calculate_metrics(  # type: ignore
+        self, predictions, targets, mode="val", forward=True, **kwargs
+    ) -> Dict[str, torch.Tensor]:
+        if isinstance(targets, dict) and self.target_name:
+            targets = targets[self.target_name]
+
+        outputs = {}
+        if forward:
+            predictions = self(predictions, ignore_masking=False)
+            if self.hf_format:
+                targets = predictions["labels"]
+                predictions = predictions["predictions"]
+        predictions = self.forward_to_prediction_fn(predictions)
+
+        for metric in self.metrics:
+            outputs[self.metric_name(metric)] = metric(predictions, targets)
+
+        return outputs
+
+    def compute_metrics(self):
+        metrics = {
+            self.metric_name(metric): metric.compute()
+            for metric in self.metrics
+            if getattr(metric, "top_ks", None)
+        }
+        # Explode metrics for each cut-off
+        # TODO make result generic:
+        # To accept a mix of ranking metrics and others not requiring top_ks ?
+        topks = {self.metric_name(metric): metric.top_ks for metric in self.metrics}
+        results = {}
+        for name, metric in metrics.items():
+            for measure, k in zip(metric, topks[name]):
+                results[f"{name}_{k}"] = measure
+        return results
+
+
+class NextItemPredictionPrepareBlock(BuildableBlock):
+    def __init__(
+        self,
+        target_dim: int,
+        weight_tying: bool = False,
+        item_embedding_table: Optional[torch.nn.Module] = None,
+        softmax_temperature: float = 0,
+    ):
+        super().__init__()
+        self.target_dim = target_dim
+        self.weight_tying = weight_tying
+        self.item_embedding_table = item_embedding_table
+        self.softmax_temperature = softmax_temperature
+
+    def build(self, input_size) -> Block:
+        return Block(
+            _NextItemPredictionTask(
+                input_size,
+                self.target_dim,
+                self.weight_tying,
+                self.item_embedding_table,
+                self.softmax_temperature,
+            ),
+            [-1, self.target_dim],
+        )
+
+
+class _NextItemPredictionTask(torch.nn.Module):
+    """Predict the interacted item-id probabilities.
+
+    - During inference, the task consists of predicting the next item.
+    - During training, the class supports the following Language modeling tasks:
+        Causal LM, Masked LM, Permutation LM and Replacement Token Detection
+
+    Parameters:
+    -----------
+        input_size: int
+            Input size of this module.
+        target_dim: int
+            Dimension of the target.
+        weight_tying: bool
+            The item id embedding table weights are shared with the prediction network layer.
+        item_embedding_table: torch.nn.Module
+            Module that's used to store the embedding table for the item.
+        softmax_temperature: float
+            Softmax temperature, used to reduce model overconfidence, so that softmax(logits / T).
+            Value 1.0 reduces to regular softmax.
+    """
+
+    def __init__(
+        self,
+        input_size: int,
+        target_dim: int,
+        weight_tying: bool = False,
+        item_embedding_table: Optional[torch.nn.Module] = None,
+        softmax_temperature: float = 0,
+    ):
+        super().__init__()
+        self.input_size = input_size
+        self.target_dim = target_dim
+        self.weight_tying = weight_tying
+        self.item_embedding_table = item_embedding_table
+        self.softmax_temperature = softmax_temperature
+        self.log_softmax = torch.nn.LogSoftmax(dim=-1)
+
+        if self.weight_tying:
+            self.output_layer_bias = torch.nn.Parameter(torch.Tensor(self.target_dim))
+            torch.nn.init.zeros_(self.output_layer_bias)
+        else:
+            self.output_layer = torch.nn.Linear(
+                self.input_size[-1], self.target_dim  # type: ignore
+            )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        if self.weight_tying:
+            logits = torch.nn.functional.linear(
+                inputs,
+                weight=self.item_embedding_table.weight,  # type: ignore
+                bias=self.output_layer_bias,
+            )
+        else:
+            logits = self.output_layer(inputs)
+
+        if self.softmax_temperature:
+            # Softmax temperature to reduce model overconfidence
+            # and better calibrate probs and accuracy
+            logits = torch.div(logits, self.softmax_temperature)
+
+        predictions = self.log_softmax(logits)
+
+        return predictions
+
+    def _get_name(self) -> str:
+        return "NextItemPredictionTask"
---
+++ packages/transformers4rec/transformers4rec-0.1.15.tar.gz/transformers4rec-0.1.15/transformers4rec/torch/utils/torch_utils.py
@@ -0,0 +1,300 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import abc
+from dataclasses import dataclass
+from typing import Dict, Optional, Union
+
+import torch
+
+from merlin_standard_lib import Schema
+from merlin_standard_lib.utils.proto_utils import has_field
+
+from ...config.schema import SchemaMixin
+from ..typing import TabularData
+
+
+class OutputSizeMixin(SchemaMixin, abc.ABC):
+    def build(self, input_size, schema=None, **kwargs):
+        self.check_schema(schema=schema)
+
+        self.input_size = input_size
+        if schema and not getattr(self, "schema", None):
+            self.schema = schema
+
+        return self
+
+    def output_size(self, input_size=None):
+        input_size = input_size or getattr(self, "input_size", None)
+        if not input_size:
+            # TODO: log warning here
+            return None
+
+        return self.forward_output_size(input_size)
+
+    def forward_output_size(self, input_size):
+        raise NotImplementedError()
+
+    def __rrshift__(self, other):
+        from ..block.base import right_shift_block
+
+        return right_shift_block(self, other)
+
+
+class LossMixin:
+    """Mixin to use for a `torch.Module` that can calculate a loss."""
+
+    def compute_loss(
+        self,
+        inputs: Union[torch.Tensor, TabularData],
+        targets: Union[torch.Tensor, TabularData],
+        compute_metrics: bool = True,
+        **kwargs,
+    ) -> torch.Tensor:
+        """Compute the loss on a batch of data.
+
+        Parameters
+        ----------
+        inputs: Union[torch.Tensor, TabularData]
+            TODO
+        targets: Union[torch.Tensor, TabularData]
+            TODO
+        compute_metrics: bool, default=True
+            Boolean indicating whether or not to update the state of the metrics
+            (if they are defined).
+        """
+        raise NotImplementedError()
+
+
+class MetricsMixin:
+    """Mixin to use for a `torch.Module` that can calculate metrics."""
+
+    def calculate_metrics(
+        self,
+        inputs: Union[torch.Tensor, TabularData],
+        targets: Union[torch.Tensor, TabularData],
+        mode: str = "val",
+        forward=True,
+        **kwargs,
+    ) -> Dict[str, torch.Tensor]:
+        """Calculate metrics on a batch of data, each metric is stateful and this updates the state.
+
+        The state of each metric can be retrieved by calling the `compute_metrics` method.
+
+        Parameters
+        ----------
+        inputs: Union[torch.Tensor, TabularData]
+            TODO
+        targets: Union[torch.Tensor, TabularData]
+            TODO
+        forward: bool, default True
+
+        mode: str, default="val"
+
+        """
+        raise NotImplementedError()
+
+    def compute_metrics(self, mode: str = None) -> Dict[str, Union[float, torch.Tensor]]:
+        """Returns the current state of each metric.
+
+        The state is typically updated each batch by calling the `calculate_metrics` method.
+
+        Parameters
+        ----------
+        mode: str, default="val"
+
+        Returns
+        -------
+        Dict[str, Union[float, torch.Tensor]]
+        """
+        raise NotImplementedError()
+
+    def reset_metrics(self):
+        """Reset all metrics."""
+        raise NotImplementedError()
+
+
+def requires_schema(module):
+    module.REQUIRES_SCHEMA = True
+
+    return module
+
+
+def check_gpu(module):
+    try:
+        return next(module.parameters()).is_cuda
+    except StopIteration:
+        return False
+
+
+def get_output_sizes_from_schema(schema: Schema, batch_size=-1, max_sequence_length=None):
+    sizes = {}
+    for feature in schema.feature:
+        name = feature.name
+        # Sequential or multi-hot feature
+        if has_field(feature, "value_count"):
+            sizes[name] = torch.Size(
+                [
+                    batch_size,
+                    max_sequence_length if max_sequence_length else feature.value_count.max,
+                ]
+            )
+        elif has_field(feature, "shape"):
+            sizes[name] = torch.Size([batch_size] + [d.size for d in feature.shape.dim])
+        else:
+            sizes[name] = torch.Size([batch_size])
+
+    return sizes
+
+
+def calculate_batch_size_from_input_size(input_size):
+    if isinstance(input_size, dict):
+        input_size = [i for i in input_size.values() if isinstance(i, torch.Size)][0]
+
+    return input_size[0]
+
+
+def check_inputs(ks, scores, labels):
+    if len(ks.shape) > 1:
+        raise ValueError("ks should be a 1-dimensional tensor")
+
+    if len(scores.shape) != 2:
+        raise ValueError("scores must be a 2-dimensional tensor")
+
+    if len(labels.shape) != 2:
+        raise ValueError("labels must be a 2-dimensional tensor")
+
+    if scores.shape != labels.shape:
+        raise ValueError("scores and labels must be the same shape")
+
+    return (
+        ks.to(dtype=torch.int32, device=scores.device),
+        scores,
+        labels,  # .to(dtype=torch.float32, device=scores.device),
+    )
+
+
+def extract_topk(ks, scores, labels):
+    max_k = int(max(ks))
+    topk_scores, topk_indices = torch.topk(scores, max_k)
+    topk_labels = torch.gather(labels, 1, topk_indices)
+    return topk_scores, topk_indices, topk_labels
+
+
+def create_output_placeholder(scores, ks):
+    return torch.zeros(scores.shape[0], len(ks)).to(device=scores.device, dtype=torch.float32)
+
+
+def tranform_label_to_onehot(labels, vocab_size):
+    return one_hot_1d(labels.reshape(-1).to(torch.int64), vocab_size, dtype=torch.float32).detach()
+
+
+def one_hot_1d(
+    labels: torch.Tensor,
+    num_classes: int,
+    device: Optional[torch.device] = None,
+    dtype: Optional[torch.dtype] = torch.float32,
+) -> torch.Tensor:
+    r"""Coverts a 1d label tensor to one-hot representation
+
+    Args:
+        labels (torch.Tensor) : tensor with labels of shape :math:`(N, H, W)`,
+                                where N is batch size. Each value is an integer
+                                representing correct classification.
+        num_classes (int): number of classes in labels.
+        device (Optional[torch.device]): the desired device of returned tensor.
+         Default: if None, uses the current device for the default tensor type
+         (see torch.set_default_tensor_type()). device will be the CPU for CPU
+         tensor types and the current CUDA device for CUDA tensor types.
+        dtype (Optional[torch.dtype]): the desired data type of returned
+         tensor. Default: torch.float32
+
+    Returns:
+        torch.Tensor: the labels in one hot tensor.
+
+    Examples::
+        >>> labels = torch.LongTensor([0, 1, 2, 0])
+        >>> one_hot_1d(labels, num_classes=3)
+        tensor([[1., 0., 0.],
+                [0., 1., 0.],
+                [0., 0., 1.],
+                [1., 0., 0.],
+               ])
+    """
+    if not torch.is_tensor(labels):
+        raise TypeError("Input labels type is not a torch.Tensor. Got {}".format(type(labels)))
+    if not len(labels.shape) == 1:
+        raise ValueError("Expected tensor should have 1 dim. Got: {}".format(labels.shape))
+    if not labels.dtype == torch.int64:
+        raise ValueError(
+            "labels must be of the same dtype torch.int64. Got: {}".format(labels.dtype)
+        )
+    if num_classes < 1:
+        raise ValueError(
+            "The number of classes must be bigger than one." " Got: {}".format(num_classes)
+        )
+    if device is None:
+        device = labels.device
+    labels_size = labels.shape[0]
+    one_hot = torch.zeros(labels_size, num_classes, device=device, dtype=dtype)
+    return one_hot.scatter_(1, labels.unsqueeze(-1), 1.0)
+
+
+class LambdaModule(torch.nn.Module):
+    def __init__(self, lambda_fn):
+        super().__init__()
+        import types
+
+        assert isinstance(lambda_fn, types.LambdaType)
+        self.lambda_fn = lambda_fn
+
+    def forward(self, x):
+        return self.lambda_fn(x)
+
+
+@dataclass
+class MappingTransformerMasking:
+    from transformers4rec.torch.masking import (
+        CausalLanguageModeling,
+        MaskedLanguageModeling,
+        PermutationLanguageModeling,
+        ReplacementLanguageModeling,
+    )
+
+    DEFAULT_MASKING = [
+        CausalLanguageModeling,
+        MaskedLanguageModeling,
+        ReplacementLanguageModeling,
+        PermutationLanguageModeling,
+    ]
+
+    BertConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    ConvBertConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    DebertaConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    DistilBertConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    GPT2Config = [CausalLanguageModeling]
+    LongformerConfig = [CausalLanguageModeling, MaskedLanguageModeling, ReplacementLanguageModeling]
+    MegatronBertConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    MPNetConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    RobertaConfig = [MaskedLanguageModeling, ReplacementLanguageModeling]
+    RoFormerConfig = [CausalLanguageModeling, MaskedLanguageModeling, ReplacementLanguageModeling]
+    TransfoXLConfig = [CausalLanguageModeling]
+    XLNetConfig = [
+        CausalLanguageModeling,
+        MaskedLanguageModeling,
+        ReplacementLanguageModeling,
+        PermutationLanguageModeling,
+    ]
