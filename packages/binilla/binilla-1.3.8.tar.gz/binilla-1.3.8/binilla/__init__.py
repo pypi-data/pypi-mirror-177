6922be838f7747098ea3e254b6d4b263ff767fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:19 +0000

    Add package binilla/1.3.8/binilla-1.3.8.tar.gz

---
+++ packages/binilla/binilla-1.3.8.tar.gz/binilla-1.3.8/binilla/__init__.py
@@ -0,0 +1,16 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Sigmmma"
+#           YYYY.MM.DD
+__date__ = "2022.11.20"
+__version__ = (1, 3, 8)
+__website__ = "https://github.com/Sigmmma/binilla"
+__all__ = (
+    'defs', 'widgets', 'windows',
+    'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',
+    )
+
+from binilla import constants
+
+constants.inject()
