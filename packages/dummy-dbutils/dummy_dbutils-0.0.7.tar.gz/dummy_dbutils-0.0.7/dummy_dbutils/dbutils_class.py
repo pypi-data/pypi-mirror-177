4678fe100dd599a9bc7d7f2f8d9ac4c3750fc716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:07 +0000

    Add package dummy-dbutils/0.0.7/dummy_dbutils-0.0.7.tar.gz

---
+++ packages/dummy-dbutils/dummy_dbutils-0.0.7.tar.gz/dummy_dbutils-0.0.7/dummy_dbutils/dbutils_class.py
@@ -0,0 +1,45 @@
+class DBUtilsSecrets:
+
+    def get(self, **kwargs):
+        print("calling DBUtilsSecrets.get func from dummy dbutils package, and no operation performed.")
+
+class DBUtilsWidgets:
+
+    def removeAll(self):
+        print("calling DBUtilsWidgets.removeAll func from dummy dbutils package, and no operation performed.")
+
+    def text(self, *args):
+        print("calling DBUtilsWidgets.text func from dummy dbutils package, and no operation performed.")
+
+    def get(self, *args):
+        print("calling DBUtilsWidgets.get func from dummy dbutils package, and no operation performed.")
+
+
+class DButilsFunctions:
+
+    def ls(self, dir: str):
+        print("calling DButilsFunctions.ls func from dummy dbutils package, and no operation performed.")
+
+    def mkdirs(self, dir: str):
+        print("calling DButilsFunctions.mkdirs func from dummy dbutils package, and no operation performed.")
+
+    def mv(self, src: str, tgt: str):
+        print("calling DButilsFunctions.mv func from dummy dbutils package, and no operation performed.")
+
+class DBUtils:
+
+    def __init__(self):
+        self.fs = DButilsFunctions()
+        self.widgets = DBUtilsWidgets()
+        self.secrets = DBUtilsSecrets()
+
+dbutils = DBUtils()
+
+# dbutils.fs.ls("dir")
+# dbutils.fs.mkdirs("dir")
+# dbutils.fs.mv("src", "tgt")
+
+scope_name = "abd"
+key = "acd"
+dbutils.secrets.get(scope = f"{scope_name}", key = f"{key}")
+
