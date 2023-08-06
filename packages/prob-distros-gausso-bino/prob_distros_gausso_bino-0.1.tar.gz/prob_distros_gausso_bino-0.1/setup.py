626065e916b6c53db5a68b0bcbf20b9866abcb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:49 +0000

    Add package prob-distros-gausso-bino/0.1/prob_distros_gausso_bino-0.1.tar.gz

---
+++ packages/prob-distros-gausso-bino/prob_distros_gausso_bino-0.1.tar.gz/prob_distros_gausso_bino-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distros_gausso_bino',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['prob_distros_gausso_bino'],
+      author = 'Aneeket Deo',
+      author_email = 'aneeketdeo@gmail.com',
+      zip_safe=False)
