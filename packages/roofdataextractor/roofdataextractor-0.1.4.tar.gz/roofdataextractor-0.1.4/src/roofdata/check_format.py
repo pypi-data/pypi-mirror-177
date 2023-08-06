a819f9d84bf2a924735ca6360833be47f02219c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:41 +0000

    Add package roofdataextractor/0.1.4/roofdataextractor-0.1.4.tar.gz

---
+++ packages/roofdataextractor/roofdataextractor-0.1.4.tar.gz/roofdataextractor-0.1.4/src/roofdata/check_format.py
@@ -0,0 +1,14 @@
+import defusedxml.ElementTree as ET
+
+def cf(file):
+    # check if the file is correctly formatted
+    try:
+        tree = ET.parse(file)
+        tree.getroot() == "DATA_EXPORT"
+        tree.getroot()[2].tag == "ADDRESS"
+        # check if third child's child is named "ROOF"
+        return True
+    except ET.ParseError:
+        # if the file is not well-formed, print that is not well-formed
+        print(file + " is not well-formed")
+        return False
