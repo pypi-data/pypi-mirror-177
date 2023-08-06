ed3e8ee7976429fc069c778233fb8b5032cc068f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:07 +0000

    Add package ter2/1.1/ter2-1.1.tar.gz

---
+++ packages/ter2/ter2-1.1.tar.gz/ter2-1.1/iter2/iterators/iterator2.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+class InvalidIteratorClass(object):
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+class Iterator2:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+        return '{class_name}<{it_id}>'.format(
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+            class_name=self.__class__.__name__,
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
