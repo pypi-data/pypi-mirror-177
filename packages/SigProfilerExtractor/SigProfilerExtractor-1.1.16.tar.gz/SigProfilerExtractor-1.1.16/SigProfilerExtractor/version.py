2cba679f0d01521541cf3adb25e8aedc37c480a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:20 +0000

    Add package SigProfilerExtractor/1.1.16/SigProfilerExtractor-1.1.16.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.16.tar.gz/SigProfilerExtractor-1.1.16/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.16'
+version = '1.1.16'
+Update = 'Remove decomposition plots from SPE. Expand Travis tests.'
+    
+    
