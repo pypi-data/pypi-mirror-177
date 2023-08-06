eb33be386e334fa5757a3b332f2849d226f0a6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:42 +0000

    Add package ooetl/1.2.3/ooetl-1.2.3-py3-none-any.whl

---
+++ packages/ooetl/ooetl-1.2.3-py3-none-any.whl/ooetl/docs/src/oo_etl.rst
@@ -0,0 +1,28 @@
+API
+===
+Documentation of the `ooetl` module and its submodules:
+`extractors`, `loaders`, and `utils`.
+
+ooetl
+------
+.. automodule:: ooetl
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+ooetl.extractors
+-----------------
+.. automodule:: ooetl.extractors
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+ooetl.loaders
+--------------
+.. automodule:: ooetl.loaders
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
---
+++ packages/ooetl/ooetl-1.2.3-py3-none-any.whl/ooetl/tests/django.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import django.test
+except ImportError as error:
+    print(f"Warning - {error.__class__.__name__}: {error.name}.")
+
+from unittest.mock import patch
+
+
+class ETLTest(django.test.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ETLTest, cls).setUpClass()
+        cls.mock_get_patcher = patch('ooetl.extractors.requests.get')
+        cls.mock_get = cls.mock_get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_get_patcher.stop()
+        super(ETLTest, cls).tearDownClass()
