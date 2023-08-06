f45b3ea5e8ddf91b55fffc7f1d2d4f95bd82cdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:31 +0000

    Add package o-words/0.0.1.1/o-words-0.0.1.1.tar.gz

---
+++ packages/o-words/o-words-0.0.1.1.tar.gz/o-words-0.0.1.1/co_words/wordcount.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+class WordCount:
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+    """ A class for finding word counts amongst a Series of articles.
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+        A dictionary containing the objects needed for the WordCount class
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
+        A dictionary containing the objects needed for the WordCount class - filtered by column and value
+	def __init__(self):
+		webbrowser.open("https://ultramarine-linux.org")
