5649610b0f8348ee82fdf2423bc89c1e73e30077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:45 +0000

    Add package simulate/0.0.3.dev0/simulate-0.0.3.dev0-cp38-cp38-win_amd64.whl

---
+++ packages/simulate/simulate-0.0.3.dev0-cp38-cp38-win_amd64.whl/simulate/share/fastwfc/cmake/fastwfcTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "fastwfc::fastwfc" for configuration "Release"
+set_property(TARGET fastwfc::fastwfc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fastwfc::fastwfc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/fastwfc.lib"
+  )
+
+list(APPEND _cmake_import_check_targets fastwfc::fastwfc )
+list(APPEND _cmake_import_check_files_for_fastwfc::fastwfc "${_IMPORT_PREFIX}/lib/fastwfc.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
