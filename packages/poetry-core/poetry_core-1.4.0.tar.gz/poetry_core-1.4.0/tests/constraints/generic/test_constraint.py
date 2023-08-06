aee6e0993bdbbcc351aacc02bc8ae741fd65242a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:16 +0000

    Add package poetry-core/1.4.0/poetry_core-1.4.0.tar.gz

---
+++ packages/poetry-core/poetry_core-1.4.0.tar.gz/poetry_core-1.4.0/tests/constraints/generic/test_constraint.py
@@ -0,0 +1,185 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from poetry.core.constraints.generic import AnyConstraint
+from poetry.core.constraints.generic import Constraint
+from poetry.core.constraints.generic import EmptyConstraint
+from poetry.core.constraints.generic import MultiConstraint
+from poetry.core.constraints.generic import UnionConstraint
+
+
+if TYPE_CHECKING:
+    from poetry.core.constraints.generic import BaseConstraint
+
+
+def test_allows() -> None:
+    c = Constraint("win32")
+
+    assert c.allows(Constraint("win32"))
+    assert not c.allows(Constraint("linux"))
+
+    c = Constraint("win32", "!=")
+
+    assert not c.allows(Constraint("win32"))
+    assert c.allows(Constraint("linux"))
+
+
+def test_allows_any() -> None:
+    c = Constraint("win32")
+
+    assert c.allows_any(Constraint("win32"))
+    assert not c.allows_any(Constraint("linux"))
+    assert c.allows_any(UnionConstraint(Constraint("win32"), Constraint("linux")))
+    assert c.allows_any(Constraint("linux", "!="))
+
+    c = Constraint("win32", "!=")
+
+    assert not c.allows_any(Constraint("win32"))
+    assert c.allows_any(Constraint("linux"))
+    assert c.allows_any(UnionConstraint(Constraint("win32"), Constraint("linux")))
+    assert c.allows_any(Constraint("linux", "!="))
+
+
+def test_allows_all() -> None:
+    c = Constraint("win32")
+
+    assert c.allows_all(Constraint("win32"))
+    assert not c.allows_all(Constraint("linux"))
+    assert not c.allows_all(Constraint("linux", "!="))
+    assert not c.allows_all(UnionConstraint(Constraint("win32"), Constraint("linux")))
+
+
+@pytest.mark.parametrize(
+    ("constraint1", "constraint2", "expected"),
+    [
+        (
+            Constraint("win32"),
+            Constraint("win32"),
+            Constraint("win32"),
+        ),
+        (
+            Constraint("win32"),
+            Constraint("linux"),
+            EmptyConstraint(),
+        ),
+        (
+            Constraint("win32"),
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+            Constraint("win32"),
+        ),
+        (
+            Constraint("win32"),
+            UnionConstraint(Constraint("linux"), Constraint("linux2")),
+            EmptyConstraint(),
+        ),
+        (
+            Constraint("win32"),
+            Constraint("linux", "!="),
+            Constraint("win32"),
+        ),
+        (
+            Constraint("win32", "!="),
+            Constraint("linux"),
+            Constraint("linux"),
+        ),
+        (
+            Constraint("win32", "!="),
+            Constraint("linux", "!="),
+            MultiConstraint(Constraint("win32", "!="), Constraint("linux", "!=")),
+        ),
+        (
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+            UnionConstraint(Constraint("win32"), Constraint("darwin")),
+            Constraint("win32"),
+        ),
+        (
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+            MultiConstraint(Constraint("win32", "!="), Constraint("darwin", "!=")),
+            Constraint("linux"),
+        ),
+    ],
+)
+def test_intersect(
+    constraint1: BaseConstraint,
+    constraint2: BaseConstraint,
+    expected: BaseConstraint,
+) -> None:
+    intersection = constraint1.intersect(constraint2)
+    assert intersection == expected
+
+
+@pytest.mark.parametrize(
+    ("constraint1", "constraint2", "expected"),
+    [
+        (
+            Constraint("win32"),
+            Constraint("win32"),
+            Constraint("win32"),
+        ),
+        (
+            Constraint("win32"),
+            Constraint("linux"),
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+        ),
+        (
+            Constraint("win32"),
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+            UnionConstraint(Constraint("win32"), Constraint("linux")),
+        ),
+        (
+            Constraint("win32"),
+            UnionConstraint(Constraint("linux"), Constraint("linux2")),
+            UnionConstraint(
+                Constraint("win32"), Constraint("linux"), Constraint("linux2")
+            ),
+        ),
+        (
+            Constraint("win32"),
+            Constraint("linux", "!="),
+            Constraint("linux", "!="),
+        ),
+        (
+            Constraint("win32", "!="),
+            Constraint("linux"),
+            Constraint("win32", "!="),
+        ),
+        (
+            Constraint("win32", "!="),
+            Constraint("linux", "!="),
+            AnyConstraint(),
+        ),
+    ],
+)
+def test_union(
+    constraint1: BaseConstraint,
+    constraint2: BaseConstraint,
+    expected: BaseConstraint,
+) -> None:
+    union = constraint1.union(constraint2)
+    assert union == expected
+
+
+def test_difference() -> None:
+    c = Constraint("win32")
+
+    assert c.difference(Constraint("win32")).is_empty()
+    assert c.difference(Constraint("linux")) == c
+
+
+@pytest.mark.parametrize(
+    "constraint",
+    [
+        EmptyConstraint(),
+        AnyConstraint(),
+        Constraint("win32"),
+        UnionConstraint(Constraint("win32"), Constraint("linux")),
+        MultiConstraint(Constraint("win32", "!="), Constraint("linux", "!=")),
+    ],
+)
+def test_constraints_are_hashable(constraint: BaseConstraint) -> None:
+    # We're just testing that constraints are hashable, there's nothing much to say
+    # about the result.
+    hash(constraint)
---
+++ packages/poetry-core/poetry_core-1.4.0.tar.gz/poetry_core-1.4.0/tests/json/test_poetry_schema.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from poetry.core.json import validate_object
+
+
+@pytest.fixture
+def base_object() -> dict[str, Any]:
+    return {
+        "name": "myapp",
+        "version": "1.0.0",
+        "description": "Some description.",
+        "authors": ["Your Name <you@example.com>"],
+        "dependencies": {"python": "^3.6"},
+        "dev-dependencies": {},
+    }
+
+
+@pytest.fixture
+def multi_url_object() -> dict[str, Any]:
+    return {
+        "name": "myapp",
+        "version": "1.0.0",
+        "description": "Some description.",
+        "authors": ["Your Name <you@example.com>"],
+        "dependencies": {
+            "python": [
+                {
+                    "url": "https://download.pytorch.org/whl/cpu/torch-1.4.0%2Bcpu-cp37-cp37m-linux_x86_64.whl",
+                    "platform": "linux",
+                },
+                {"path": "../foo", "platform": "darwin"},
+            ]
+        },
+        "dev-dependencies": {},
+    }
+
+
+def test_path_dependencies(base_object: dict[str, Any]) -> None:
+    base_object["dependencies"].update({"foo": {"path": "../foo"}})
+    base_object["dev-dependencies"].update({"foo": {"path": "../foo"}})
+
+    assert len(validate_object(base_object, "poetry-schema")) == 0
+
+
+def test_multi_url_dependencies(multi_url_object: dict[str, Any]) -> None:
+    assert len(validate_object(multi_url_object, "poetry-schema")) == 0
+
+
+def test_multiline_description(base_object: dict[str, Any]) -> None:
+    bad_description = "Some multi-\nline string"
+    base_object["description"] = bad_description
+
+    errors = validate_object(base_object, "poetry-schema")
+    assert len(errors) == 1
+    assert errors[0] == f"[description] {bad_description!r} does not match '^[^\\n]*$'"
