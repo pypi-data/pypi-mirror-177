066e0f3dddf8e3d86a363ae7479a426d6e5f0349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package cat-win/1.0.15/cat_win-1.0.15-py3-none-any.whl

---
+++ packages/cat-win/cat_win-1.0.15-py3-none-any.whl/cat_win/util/Holder.py
@@ -0,0 +1,58 @@
+from cat_win.util.ArgConstants import *
+
+
+class Holder():
+    files = []
+    args = []
+    args_id = []
+    temp_file = None
+    reversed = False
+    maxlineLength = 0
+    lineSum = 0
+    fileLineMaxLength = 0
+    fileMaxLength = 0
+    clipBoard = ""
+
+    def setFiles(self, files: list) -> None:
+        self.files = files
+
+    def setArgs(self, args: list) -> None:
+        self.args = args
+        self.args_id = [x[0] for x in self.args]
+        self.reversed = ARGS_REVERSE in self.args_id
+
+    def setTempFile(self, file: str) -> None:
+        self.temp_file = file
+        
+    def getAppliedFiles(self) -> list:
+        return ["<STDIN>" if f == self.temp_file else f for f in self.files]
+    
+    def __count_generator__(self, reader) -> bytes:
+        b = reader(1024 * 1024)
+        while b:
+            yield b
+            b = reader(1024 * 1024)
+
+    def __getFileLinesSum__(self, file) -> int:
+        with open(file, 'rb') as fp:
+            c_generator = self.__count_generator__(fp.raw.read)
+            count = sum(buffer.count(b'\n') for buffer in c_generator)
+            return count + 1
+
+    def __calcFilesLineSum__(self) -> None:
+        self.lineSum = max([self.__getFileLinesSum__(file) for file in self.files])
+
+    def __calcFileLineMaxLength__(self) -> None:
+        self.fileLineMaxLength = len(str(self.lineSum))
+
+    def __calcFileMaxLength__(self) -> None:
+        self.fileMaxLength = len(str(len(self.files)))
+
+    def __calcMaxLineLength__(self) -> None:
+        self.maxlineLength = len(str(max([max([len(line)-1 for line in open(file, 'rb').readlines()]) for file in self.files])))
+
+    def generateValues(self) -> None:
+        self.__calcFilesLineSum__()
+        self.__calcFileLineMaxLength__()
+        self.__calcFileMaxLength__()
+        self.__calcMaxLineLength__()
