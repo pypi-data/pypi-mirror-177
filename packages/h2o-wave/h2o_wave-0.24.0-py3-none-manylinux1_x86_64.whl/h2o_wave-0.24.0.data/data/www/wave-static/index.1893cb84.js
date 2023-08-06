26fcd93c8ce4c7863da261e4ae47ac22adadf902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:17 +0000

    Add package h2o-wave/0.24.0/h2o_wave-0.24.0-py3-none-manylinux1_x86_64.whl

---
+++ packages/h2o-wave/h2o_wave-0.24.0-py3-none-manylinux1_x86_64.whl/h2o_wave-0.24.0.data/data/www/wave-static/index.1893cb84.js
@@ -0,0 +1,1 @@
+import{L as a,M as e}from"./LocalizationProvider.2439edbe.js";import"./vendor.30d900c5.js";import"./useThemeProps.01d06021.js";export{a as LocalizationProvider,e as MuiPickersAdapterContext};
