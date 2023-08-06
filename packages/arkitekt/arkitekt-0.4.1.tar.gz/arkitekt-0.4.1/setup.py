e773578518d01780459f386adcf1dc8c3f24eca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:37 +0000

    Add package arkitekt/0.4.1/arkitekt-0.4.1.tar.gz

---
+++ packages/arkitekt/arkitekt-0.4.1.tar.gz/arkitekt-0.4.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arkitekt',
+ 'arkitekt.apps',
+ 'arkitekt.cli',
+ 'arkitekt.cli.dev',
+ 'arkitekt.cli.prod',
+ 'arkitekt.qt']
+
+package_data = \
+{'': ['*'], 'arkitekt.qt': ['assets/dark/*', 'assets/light/*']}
+
+install_requires = \
+['fakts>=0.3.1,<0.4.0',
+ 'herre>=0.3.1,<0.4.0',
+ 'mikro>=0.3.1,<0.4.0',
+ 'rekuest>=0.1.1,<0.2.0',
+ 'unlok>=0.1.3']
+
+entry_points = \
+{'console_scripts': ['arkitekt = arkitekt.cli.main:entrypoint']}
+
+setup_kwargs = {
+    'name': 'arkitekt',
+    'version': '0.4.1',
+    'description': 'client for the arkitekt platform',
+    'long_description': 'None',
+    'author': 'jhnnsrs',
+    'author_email': 'jhnnsrs@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
