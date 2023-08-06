620763c1a62488a81afe2531ed80f47c8a144eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:44 +0000

    Add package dastyletools/0.0.2/dastyletools-0.0.2-py3-none-any.whl

---
+++ packages/dastyletools/dastyletools-0.0.2-py3-none-any.whl/dastyletools/tools.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    """
+    两数相加
+    """
+    return a + b
+
+
+def sub(a, b):
+    """
+    两数相减
+    """
+    return a - b
+
+
+def mul(a, b):
+    """
+    两数相乘
+    """
+    return a * b
+
+
+def div(a, b):
+    """
+    两数相除
+    """
+    return a / b
