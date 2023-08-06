febcfd82394556d5e4c4e7e6e65c245fbbae9a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:03 +0000

    Add package balance-domain/0.1.28/balance_domain-0.1.28-py3-none-any.whl

---
+++ packages/balance-domain/balance_domain-0.1.28-py3-none-any.whl/balance_domain/models/user_models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from balance_domain.database.settings import Base
+
+
+class User(Base):
+    __tablename__ = "TB_USER"
+
+    id = Column(Integer, primary_key=True, index=True)
+    surname = Column(String(255))
+    fullname = Column(String(255))
+    email = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
+    is_active = Column(Boolean, default=True)
+
+    bank = relationship("TB_BANK", back_populates="User")
+
+    def to_json(self):
+        return vars(self)
+
+
+class Bank(Base):
+    __tablename__ = "TB_BANK"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String(800))
+    user_id = Column(Integer, ForeignKey("TB_USER.id"))
+    is_active = Column(Boolean, default=True)
+
+    user = relationship("TB_USER", back_populates="Bank")
+
+    def to_json(self):
+        return vars(self)
