1754eb2802b8224e41185f49a5344aa43db7780e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:41 +0000

    Add package roofdataextractor/0.1.4/roofdataextractor-0.1.4-py3-none-any.whl

---
+++ packages/roofdataextractor/roofdataextractor-0.1.4-py3-none-any.whl/roofdata/xml_to_json.py
@@ -0,0 +1,10 @@
+from xmljson import badgerfish as bf
+from defusedxml.ElementTree import fromstring
+import json
+import roofdata.check_format as check_format
+
+def xtj(file):
+    if check_format.cf(file):
+        with open(file, "r") as input:
+            jsonOut = bf.data(fromstring(input.read()))
+        return json.loads(json.dumps(jsonOut))
