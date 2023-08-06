309208453163fcb118315aa7d3b044e2f5f5154b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package gradio/3.10.1/gradio-3.10.1.tar.gz

---
+++ packages/gradio/gradio-3.10.1.tar.gz/gradio-3.10.1/gradio/templates/cdn/assets/index.c9272e65.js
@@ -0,0 +1,2 @@
+import{S as s,i as e,s as o}from"./index.20ccd338.js";class a extends s{constructor(t){super(),e(this,t,null,null,o,{})}}var r=a;const i=["static"];export{r as Component,i as modes};
+//# sourceMappingURL=index.c9272e65.js.map
---
+++ packages/gradio/gradio-3.10.1.tar.gz/gradio-3.10.1/gradio/templates/frontend/assets/color.b26158cb.js
@@ -0,0 +1,2 @@
+import{af as o}from"./index.5ef90923.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color.b26158cb.js.map
