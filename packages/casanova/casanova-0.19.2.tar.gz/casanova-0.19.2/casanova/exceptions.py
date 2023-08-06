959f807bb5fa9879f7abbe2a98f6f1ffa87f596b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:51:04 +0000

    Add package casanova/0.19.2/casanova-0.19.2.tar.gz

---
+++ packages/casanova/casanova-0.19.2.tar.gz/casanova-0.19.2/casanova/exceptions.py
@@ -0,0 +1,40 @@
+# =============================================================================
+# Casanova Exceptions
+# =============================================================================
+#
+
+
+class CasanovaError(Exception):
+    pass
+
+
+class ColumnNumberMismatchError(CasanovaError):
+    pass
+
+
+class EmptyFileError(CasanovaError):
+    pass
+
+
+class NoHeadersError(CasanovaError):
+    pass
+
+
+class MissingColumnError(CasanovaError):
+    pass
+
+
+class InvalidFileError(CasanovaError):
+    pass
+
+
+class NotResumableError(CasanovaError):
+    pass
+
+
+class ResumeError(CasanovaError):
+    pass
+
+
+class CorruptedIndexColumn(CasanovaError):
+    pass
