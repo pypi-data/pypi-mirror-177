4a03e86be700411408405e07c6ced4b680fe42a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:58 +0000

    Add package vinclab-distri/0.1/vinclab_distri-0.1.tar.gz

---
+++ packages/vinclab-distri/vinclab_distri-0.1.tar.gz/vinclab_distri-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vinclab_distri',
+      version='0.1',
+      description='Gaussian and Binomail distributions',
+      packages=['vinclab_distri'],
+      autor = 'Vincent Salas',
+      author_email = 'contact@vincentsalas.fr',
+      zip_safe=False)
