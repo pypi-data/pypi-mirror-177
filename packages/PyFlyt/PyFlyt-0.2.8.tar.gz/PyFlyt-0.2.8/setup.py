b11e5215d830a5dd4e7cc7961481857a94ace5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:14 +0000

    Add package PyFlyt/0.2.8/PyFlyt-0.2.8.tar.gz

---
+++ packages/PyFlyt/PyFlyt-0.2.8.tar.gz/PyFlyt-0.2.8/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="PyFlyt",
+    version="0.2.8",
+    author="jjshoots",
+    author_email="taijunjet@hotmail.com",
+    description="Freestyle Quadcopter Flight in Pybullet with Gym and (soon) PettingZoo APIs",
+    url="https://github.com/jjshoots/PyFlyt",
+    long_description="# [Docs](https://github.com/jjshoots/PyFlyt/blob/master/readme.md)",
+    long_description_content_type="text/markdown",
+    keywords=[
+        "Reinforcement Learning",
+        "UAVs",
+        "drones",
+        "Quadcopter",
+        "AI",
+        "Gym",
+        "PettingZoo",
+    ],
+    python_requires=">=3.8, <3.11",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "matplotlib",
+        "gymnasium",
+        "numpy",
+        "pybullet",
+        "cflib",
+        "cfclient",
+        "scipy",
+    ],
+)
