36b15b974656eba0b1233e9ac48c5f4c236a730d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:36 +0000

    Add package eth-stdlib/0.2.5/eth_stdlib-0.2.5-py3-none-any.whl

---
+++ packages/eth-stdlib/eth_stdlib-0.2.5-py3-none-any.whl/eth/codecs/utils.py
@@ -0,0 +1,43 @@
+import string
+from typing import Union
+
+from eth.hash import keccak256
+
+
+def checksum_encode(addr: Union[str, bytes]) -> str:
+    """Checksum encode an address.
+
+    See `EIP-55 <https://eips.ethereum.org/EIPS/eip-55>`_.
+
+    Parameters:
+        addr: The address to checksum encode.
+
+    Returns:
+        The checksum encoded address.
+
+    Raises:
+        TypeError: If ``addr`` argument is not an instance of ``str`` or ``bytes``.
+        ValueError: If ``addr`` contains non-hexadecimal characters or is not the proper length.
+    """
+    if isinstance(addr, bytes):
+        hexval = addr.hex()
+    elif isinstance(addr, str):
+        if addr[:2] in ("0x", "0X"):  # pragma: no branch
+            addr = addr[2:]
+        hexval = addr.lower()
+    else:
+        raise TypeError(
+            f"Invalid argument type, expected 'str' or 'bytes' got: {type(addr).__qualname__}"
+        )
+
+    if len(hexval) != 40:
+        raise ValueError("Invalid value length")
+    elif not set(hexval) < set(string.hexdigits):
+        raise ValueError("Invalid hexadecimal characters")
+
+    buffer = ""
+    digest = keccak256(hexval.encode()).hex()
+    for char, nibble in zip(hexval, digest):
+        buffer += char.upper() if int(nibble, 16) > 7 else char
+
+    return "0x" + buffer
