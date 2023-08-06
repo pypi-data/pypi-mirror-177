efb72e5e0ec50071593f83ba70720b933db0b220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:55 +0000

    Add package gdshoplib/0.5.1/gdshoplib-0.5.1.tar.gz

---
+++ packages/gdshoplib/gdshoplib-0.5.1.tar.gz/gdshoplib-0.5.1/gdshoplib/packages/__init__.py
@@ -0,0 +1,14 @@
+from gdshoplib.packages.files.converter import (BaseConverter, ImageConverter,
+                                                VideoConverter)
+from gdshoplib.packages.files.disk import Disk
+from gdshoplib.packages.files.marker import Marker
+from gdshoplib.packages.lang import transliterate
+
+__all__ = (
+    "ImageConverter",
+    "VideoConverter",
+    "BaseConverter",
+    "transliterate",
+    "Marker",
+    "Disk",
+)
---
+++ packages/gdshoplib/gdshoplib-0.5.1.tar.gz/gdshoplib-0.5.1/gdshoplib/services/ula/manager.py
@@ -0,0 +1,7 @@
+from gdshoplib.core.manager.basemanager import BaseManager
+from gdshoplib.services.vk.settings import Settings
+
+
+class UlaManager(BaseManager):
+    KEY = "ULA"
+    SETTINGS = Settings
