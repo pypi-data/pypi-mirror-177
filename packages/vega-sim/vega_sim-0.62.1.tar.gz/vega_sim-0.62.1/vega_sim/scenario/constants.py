91c8a4fd97f55b7368abf19f65d5aab754afb1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:47 +0000

    Add package vega-sim/0.62.1/vega_sim-0.62.1.tar.gz

---
+++ packages/vega-sim/vega_sim-0.62.1.tar.gz/vega_sim-0.62.1/vega_sim/scenario/constants.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Network(Enum):
+    NULLCHAIN = "nullchain"
+    STAGNET1 = "stagnet1"
+    STAGNET2 = "vegawallet-stagnet2"
+    STAGNET3 = "stagnet3"
+    FAIRGROUND = "fairground"
