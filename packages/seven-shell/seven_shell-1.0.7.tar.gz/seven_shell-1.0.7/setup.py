f15ccfc9e1e8a22d0f5baf4f0318e175ef2d34c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:43 +0000

    Add package seven-shell/1.0.7/seven_shell-1.0.7.tar.gz

---
+++ packages/seven-shell/seven_shell-1.0.7.tar.gz/seven_shell-1.0.7/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: ChenXiaolei
+:Date: 2020-04-22 21:25:59
+:LastEditTime: 2022-11-18 14:44:27
+:LastEditors: Kangwenbin
+:Description: 
+"""
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="seven_shell",
+    version="1.0.7",
+    author="seven",
+    author_email="tech@gao7.com",
+    description="seven shell",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="http://gitlab.tdtech.gao7.com/python/pypi_packages/seven_shell",
+    packages=find_packages(),
+    install_requires=[
+        "seven-framework >=1.1.10"
+    ],
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires='~=3.4',
+)
