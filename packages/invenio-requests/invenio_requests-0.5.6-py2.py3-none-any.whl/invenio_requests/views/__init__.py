a55ac0a58e6f08168e0ad2bfffd845bfb1f88bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:38 +0000

    Add package invenio-requests/0.5.6/invenio_requests-0.5.6-py2.py3-none-any.whl

---
+++ packages/invenio-requests/invenio_requests-0.5.6-py2.py3-none-any.whl/invenio_requests/views/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio-Requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Request views module."""
+
+from .api import create_request_events_bp, create_requests_bp
+from .ui import create_ui_blueprint
+
+__all__ = (
+    "create_ui_blueprint",
+    "create_requests_bp",
+    "create_request_events_bp",
+)
