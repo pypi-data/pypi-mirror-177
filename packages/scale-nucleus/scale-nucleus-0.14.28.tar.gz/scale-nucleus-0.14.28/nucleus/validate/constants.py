bbd1c99962db11e2d613846fa8f6081088948f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:34 +0000

    Add package scale-nucleus/0.14.28/scale-nucleus-0.14.28.tar.gz

---
+++ packages/scale-nucleus/scale-nucleus-0.14.28.tar.gz/scale-nucleus-0.14.28/nucleus/validate/constants.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+EVAL_FUNCTION_KEY = "eval_fn"
+EVALUATION_ID_KEY = "evaluation_id"
+EVAL_FUNCTION_ID_KEY = "eval_function_id"
+ID_KEY = "id"
+PASS_KEY = "pass"
+RESULT_KEY = "result"
+THRESHOLD_COMPARISON_KEY = "threshold_comparison"
+THRESHOLD_KEY = "threshold"
+SCENARIO_TEST_ID_KEY = "scenario_test_id"
+SCENARIO_TEST_NAME_KEY = "scenario_test_name"
+SCENARIO_TEST_METRICS_KEY = "scenario_test_metrics"
+
+
+class ThresholdComparison(str, Enum):
+    """Comparator between the result and the threshold."""
+
+    GREATER_THAN = "greater_than"
+    GREATER_THAN_EQUAL_TO = "greater_than_equal_to"
+    LESS_THAN = "less_than"
+    LESS_THAN_EQUAL_TO = "less_than_equal_to"
+
+
+class EntityLevel(str, Enum):
+    """Level for evaluation functions and unit tests."""
+
+    ITEM = "item"
+    SCENE = "scene"
