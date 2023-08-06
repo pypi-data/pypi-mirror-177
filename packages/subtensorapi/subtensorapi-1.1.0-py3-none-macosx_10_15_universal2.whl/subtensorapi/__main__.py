b654f1c058c4fe43a63a31114cacf614c93415ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:49 +0000

    Add package subtensorapi/1.1.0/subtensorapi-1.1.0-py3-none-macosx_10_15_universal2.whl

---
+++ packages/subtensorapi/subtensorapi-1.1.0-py3-none-macosx_10_15_universal2.whl/subtensorapi/__main__.py
@@ -0,0 +1,53 @@
+import argparse
+import sys
+from typing import List, Optional
+
+from . import cli, __version__
+
+
+def main(args: Optional[List[str]] = sys.argv[1:]) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", action="version", version=__version__)
+
+    cmd_parsers = parser.add_subparsers(dest="command")
+
+    sync_and_save = cmd_parsers.add_parser("sync_and_save")
+    cli.add_args_sync_and_save(sync_and_save)
+
+    blockAtRegistration_for_all_and_save = cmd_parsers.add_parser(
+        "blockAtReg_and_save"
+    )
+    cli.add_args_blockAtRegistration_for_all_and_save(
+        blockAtRegistration_for_all_and_save
+    )
+
+    sync_and_save_historical = cmd_parsers.add_parser(
+        "sync_and_save_historical"
+    )
+    cli.add_args_sync_and_save_historical(
+        sync_and_save_historical
+    )
+
+    sync_and_save_historical_difficulty = cmd_parsers.add_parser(
+        "sync_and_save_historical_difficulty"
+    )
+    cli.add_args_sync_and_save_historical_difficulty(
+        sync_and_save_historical_difficulty
+    )
+
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.command == "sync_and_save":
+        cli.sync_and_save(parsed_args)
+    elif parsed_args.command == "blockAtReg_and_save":
+        cli.blockAtRegistration_for_all_and_save(parsed_args)
+    elif parsed_args.command == "sync_and_save_historical":
+        cli.sync_and_save_historical(parsed_args)
+    elif parsed_args.command == "sync_and_save_historical_difficulty":
+        cli.sync_and_save_historical_difficulty(parsed_args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
