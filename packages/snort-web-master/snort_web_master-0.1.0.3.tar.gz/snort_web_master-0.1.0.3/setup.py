7e4116849e990e8c2da58cfef12c9892cf3b7b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:51 +0000

    Add package snort-web-master/0.1.0.3/snort_web_master-0.1.0.3.tar.gz

---
+++ packages/snort-web-master/snort_web_master-0.1.0.3.tar.gz/snort_web_master-0.1.0.3/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='snort_web_master',
+    version='0.1.0.3',
+    license='MoCorp',
+    author="meir dahan",
+    author_email='1dahanmeir1@gmail.com',
+    packages=find_packages('snort_web_master'),
+    package_dir={'': 'snort_web_master'},
+    url='https://github.com/mosheovadi1/snort-web-master',
+    keywords='snort3 django',
+    include_package_data=False,
+    install_requires=[
+          'django',
+'django-auth-ldap==1.7.0',
+'django-object-actions','dpkt',
+
+      ],
+
+)
