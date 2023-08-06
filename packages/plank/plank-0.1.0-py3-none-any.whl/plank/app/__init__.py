82295319a13b00d7075a7796bd8c057e79d52ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:52 +0000

    Add package plank/0.1.0/plank-0.1.0-py3-none-any.whl

---
+++ packages/plank/plank-0.1.0-py3-none-any.whl/plank/app/__init__.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Optional, List, Any, Dict, Type, Union, TYPE_CHECKING
+
+from plank import logger
+from plank.context import Context
+from plank.configuration import Configuration
+from plank.serving.interface import ServiceManagerable
+from plank.serving.service import Service
+
+if TYPE_CHECKING:
+    from plank.plugin import Plugin
+
+_Application__singleton_key = "__sigleton"
+
+
+class Application(ServiceManagerable):
+    class Delegate:
+        def application_will_launch(self, app: Application, launch_options: Dict[str, Any]): pass
+
+        def application_did_launch(self, app: Application): pass
+
+        def application_using_plugin_type(self, app: Application, prefix: str) -> Type[Plugin]:
+            from plank.plugin.module import ModulePlugin
+            return ModulePlugin
+
+        def application_did_discover_plugins(self, app: Application, plugins: List[Plugin]): pass
+
+        def application_should_install_plugin(self, app: Application, plugin: Plugin) -> bool: return True
+
+        def application_did_install_plugin(self, app: Application, plugin: Plugin): pass
+
+        def application_should_load_plugin(self, app: Application, plugin: Plugin) -> bool: return True
+
+        def application_did_load_plugin(self, app: Application, plugin: Plugin): pass
+
+    @classmethod
+    def set_default_delegate_type(cls, delegate_type: Type[Application.Delegate]):
+        assert issubclass(delegate_type,
+                          Application.Delegate), f"{delegate_type} should be inherited from Application.Delegate."
+        cls.Delegate = delegate_type
+
+    @property
+    def name(self) -> str:
+        return self.__configuration.app.name
+
+    @property
+    def version(self) -> str:
+        return self.configuration.app.version
+
+    @property
+    def build_version(self) -> str:
+        return self.configuration.app.build_version
+
+    @property
+    def workspace(self) -> Path:
+        return self.configuration.path.workspace
+
+    @property
+    def debug(self) -> bool:
+        return self.configuration.app.debug
+
+    @property
+    def configuration(self) -> Configuration:
+        return self.__configuration
+
+    @property
+    def delegate(self) -> Application.Delegate:
+        return self.__delegate
+
+    @property
+    def plugins(self) -> List[Plugin]:
+        return self.__installed_plugins or []
+        # plugin_type = self.delegate.application_using_plugin_type(app=self)
+        # return plugin_type.installed()
+
+    @property
+    def loaded(self):
+        return self.__loaded
+
+    @classmethod
+    def main(cls) -> Application:
+        if not hasattr(cls, _Application__singleton_key):
+            raise RuntimeError(
+                f"Application had no an instance. please get instance by Application(delegate=...) first.")
+        return getattr(cls, _Application__singleton_key)
+
+    def __init__(self, delegate: Application.Delegate, configuration: Configuration) -> None:
+        self.__delegate = delegate
+        self.__configuration = configuration
+        self.__loaded = False
+        self.__installed_plugins = []
+
+    def as_main(self):
+        setattr(Application, _Application__singleton_key, self)
+
+    def _load_plugin(self):
+        plugin_prefixes = self.configuration.plugin.prefix
+        plugins = []
+        for prefix in plugin_prefixes:
+            plugin_type = self.delegate.application_using_plugin_type(self, prefix=prefix)
+            if hasattr(plugin_type, "discover"):
+                plugins += (plugin_type.discover(plugin_prefix=prefix) or [])
+
+        if len(plugins) > 0:
+            self.delegate.application_did_discover_plugins(app=self, plugins=plugins)
+        for plugin in plugins:
+            try:
+                if self.delegate.application_should_install_plugin(app=self, plugin=plugin):
+                    plugin_type.install(plugin=plugin)
+                    self.__installed_plugins.append(plugin)
+                    self.delegate.application_did_install_plugin(app=self, plugin=plugin)
+                    # load plugin if installed, otherwise passing.
+                    if self.delegate.application_should_load_plugin(app=self, plugin=plugin):
+                        plugin.load()
+                        self.delegate.application_did_load_plugin(app=self, plugin=plugin)
+            except Exception as e:
+                logger.error(f"The error happened on loading plugin: {plugin.name}.")
+                raise e
+
+    def unload(self):
+        for plugin in self.plugins:
+            plugin.unload()
+        self.__loaded = False
+
+    def launch(self, **options):
+        if self.__loaded: return
+
+        # configuration = self.__programs.get(program)
+        # assert configuration is not None, f"{program} not in programs({self.__programs.keys()})"
+        self.__configuration.set_default()
+        self.__delegate.application_will_launch(self, options)
+        standard_context = Context.standard()
+        standard_context.update(options)
+        standard_context.update(self.configuration.context)
+        self.as_main()
+        self.__delegate.application_did_launch(self)
+
+        self._load_plugin()
+        for plugin in self.plugins:
+            plugin.delegate.application_did_launch(plugin=plugin, launch_options=options)
+
+        self.__loaded = True
+
+    def _server_did_startup(self, server):
+        for service in Service.registered():
+            actions = service.get_actions()
+            print("actions:", actions)
+            server.add_actions(*(actions.values()))
+
+    def _server_did_shutdown(self, server):
+        pass
---
+++ packages/plank/plank-0.1.0-py3-none-any.whl/plank/server/__init__.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+from collections import namedtuple
+from pathlib import Path
+from typing import Optional, Dict, Any, TYPE_CHECKING
+
+from plank.app import Application
+from plank.context import Context
+from plank.configuration import Configuration
+
+if TYPE_CHECKING:
+    from plank.server.action import Action
+
+
+class BindAddress(namedtuple("BindAddress", ("host", "port"))):
+    def description(self) -> str:
+        key = f"{self.host}"
+        if self.port is not None:
+            return f"{key}:{self.port}"
+        return key
+
+
+class Server:
+    class Delegate(Application.Delegate):
+        def server_did_startup(self, server: Server): pass
+
+        def server_did_shutdown(self, server: Server): pass
+
+    @property
+    def delegate(self) -> Server.Delegate:
+        return self.__delegate
+
+    @property
+    def path_prefix(self) -> str:
+        return self.__path_prefix
+
+    @property
+    def bind_address(self) -> BindAddress:
+        return self.__bind_address
+
+    @property
+    def application(self) -> Application:
+        return self.__application
+
+    @property
+    def actions(self) -> Dict[str, Action]:
+        return self.__registered_backends
+
+    def __init__(self, application: Application, delegate: Optional[Server.Delegate] = None,
+                 path_prefix: Optional[str] = None):
+        self.__bind_address = None
+        self.__registered_backends = {}
+        self.__application = application
+        self.__path_prefix = path_prefix
+
+        if delegate is None and isinstance(application.delegate, Server.Delegate):
+            self.__delegate = application.delegate
+        else:
+            self.__delegate = delegate or Server.Delegate()
+
+        standard_context = Context.standard()
+        vars = []
+        vars.append(("path_prefix", path_prefix or ""))
+        standard_context.update(vars)
+
+    def add_action(self, backend: Action):
+        self.__registered_backends[backend.routing_path()] = backend
+
+    def add_actions(self, *backends: Action):
+        for backend in backends:
+            self.add_action(backend=backend)
+
+    def remove_action(self, path: str) -> Action:
+        return self.__registered_backends.pop(path)
+
+    def get_action(self, key: str) -> Optional[Action]:
+        return self.__registered_backends.get(key)
+
+    def launch(self, **options):
+        self.application.launch(**options)
+
+    def listen(self, address: BindAddress):
+        self.__bind_address = address
+
+    def __safe_call_delegate_method(self, method_name: str) -> Any:
+        if hasattr(self.delegate, method_name):
+            method = getattr(self.delegate, method_name)
+            return method(self)
+
+    def did_startup(self):
+        self.application._server_did_startup(server=self)
+        self.__safe_call_delegate_method(method_name="server_did_startup")
+
+    def did_shutdown(self):
+        self.__application._server_did_shutdown(server=self)
+        self.__safe_call_delegate_method(method_name="server_did_shutdown")
