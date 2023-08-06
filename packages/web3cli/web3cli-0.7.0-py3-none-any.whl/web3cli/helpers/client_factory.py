5e03a2e9e590932d9f18cd341dbca75e5b56fcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:00 +0000

    Add package web3cli/0.7.0/web3cli-0.7.0-py3-none-any.whl

---
+++ packages/web3cli/web3cli-0.7.0-py3-none-any.whl/web3cli/helpers/client_factory.py
@@ -0,0 +1,24 @@
+from cement import App
+from web3cli.core.helpers.client_factory import make_base_client, make_base_wallet
+from web3client.base_client import BaseClient
+from web3cli.helpers import args
+from web3cli.core.exceptions import SignerNotFound
+
+
+def make_client(app: App, log: bool = False) -> BaseClient:
+    """Client suitable to read the blockchain"""
+    if log:
+        app.log.info(f"Using chain '{app.chain}'")
+    return make_base_client(app.chain)
+
+
+def make_wallet(app: App, log: bool = False) -> BaseClient:
+    """Client suitable to read and write to the blockchain"""
+    if not app.signer:
+        raise SignerNotFound(
+            "Could not find a signer: create one with `web3 signer add`"
+        )
+    client = make_base_wallet(app.chain, app.signer, app.app_key)
+    if log:
+        app.log.info(f"Using signer '{app.signer}'")
+    return client
