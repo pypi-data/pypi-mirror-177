7313ee65d958b4ee5df6d98f4c012ea1c1d7123e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:57 +0000

    Add package PyPlumIO/0.2.23/PyPlumIO-0.2.23.tar.gz

---
+++ packages/PyPlumIO/PyPlumIO-0.2.23.tar.gz/PyPlumIO-0.2.23/pyplumio/helpers/product_info.py
@@ -0,0 +1,29 @@
+"""Contains product info dataclasses."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class ProductInfo:
+    """Represents product info provided by UID response."""
+
+    type: int = 0
+    product: int = 0
+    uid: Optional[str] = None
+    logo: int = 0
+    image: int = 0
+    model: Optional[str] = None
+
+
+@dataclass
+class ConnectedModules:
+    """Represents firmware version info."""
+
+    module_a: Optional[str] = None
+    module_b: Optional[str] = None
+    module_c: Optional[str] = None
+    module_lambda: Optional[str] = None
+    module_ecoster: Optional[str] = None
+    module_panel: Optional[str] = None
