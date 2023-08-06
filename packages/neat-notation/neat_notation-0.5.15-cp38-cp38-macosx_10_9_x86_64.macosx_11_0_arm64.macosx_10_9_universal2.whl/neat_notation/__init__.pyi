625340346b74b5174787d27fc113740520db785a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package neat-notation/0.5.15/neat_notation-0.5.15-cp38-cp38-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/neat-notation/neat_notation-0.5.15-cp38-cp38-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/neat_notation/__init__.pyi
@@ -0,0 +1,7 @@
+def load(file_path:str) -> list | dict:
+	"""
+	Used to deserialize a .neat file to a python datastructure.
+
+	 - file_path:str
+	     The relative or full path to the file you wish to deserialize.
+	"""
